411e4e5998dcd6b732a291c4d648213f529ddcac
Raphael Villas <raphael.villas@gmail.com>
2014-03-28 00:12:04 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mariology::Application.config.secret_key_base = '1976869ae672962f9ed1a7564daa545223d7472f434658bd8b7aba4a1b4f1050a76067f7698f0b97540b43e90ee8a37a483a3894eec66b0478204fc414141ce8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mariology::Application.config.session_store :cookie_store, key: '_mariology_session'
