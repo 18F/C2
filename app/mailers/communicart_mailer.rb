7f31a0e2df5de3a9f737b2a2c31dcd77c61ffd72
Raphael Villas <raphael.villas@gmail.com>
2014-04-03 12:23:48 -0500

    Updated template to accept the client post

--- app/mailers/communicart_mailer.rb
+++ app/mailers/communicart_mailer.rb
@@ -2,12 +2,14 @@ class CommunicartMailer < ActionMailer::Base
   default from: ENV['NOTIFICATION_FROM_EMAIL']
 
   def cart_notification_email(analysis)
+
+    @json_post = analysis
+
     @url = ENV['NOTIFICATION_URL']
     mail(to: analysis['attention'], subject: "You have received a Communicart notification")
   end
 
   def approval_reply_received_email(analysis)
-    binding.pry
     to_address = ENV['NOTIFICATION_TO_ADDRESS'] ? ENV['NOTIFICATION_TO_ADDRESS'] : 'read.robert@gmail.com'
     @url = ENV['NOTIFICATION_URL']
     mail(to: to_address, subject: "You have received a Communicart notification")
