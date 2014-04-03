f5db646f52af23b52bb30c01f5f0516a3142c803
Raphael Villas <raphael.villas@gmail.com>
2014-04-03 11:34:47 -0500

    Removed binding.pry

--- app/mailers/communicart_mailer.rb
+++ app/mailers/communicart_mailer.rb
@@ -2,7 +2,6 @@ class CommunicartMailer < ActionMailer::Base
   default from: ENV['NOTIFICATION_FROM_EMAIL']
 
   def cart_notification_email(analysis)
-    binding.pry
     @url = ENV['NOTIFICATION_URL']
     mail(to: analysis['attention'], subject: "You have received a Communicart notification")
   end
