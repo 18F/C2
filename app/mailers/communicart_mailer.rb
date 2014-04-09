c963bad9947b408ed595daf0794338997a549de9
Raphael Villas <raphael.villas@gmail.com>
2014-04-09 09:54:03 -0500

    Revert "support approvalGroup Syntax"
    This reverts commit abf512d870517fa8201109966c9d57e0470d0f46.

--- app/mailers/communicart_mailer.rb
+++ app/mailers/communicart_mailer.rb
@@ -4,16 +4,7 @@
   def cart_notification_email(analysis)
     @json_post = analysis
     @url = ENV['NOTIFICATION_URL']
-    if !analysis['email'].blank?
-# This case assumes there is a single email approval
-      mail(to: analysis['email'], subject: "Please approve Cart Number: "+analysis["cartNumber"])
-    elsif !analysis['approvalGroup'].blank?
-      logger.debug "Not Implemented -- got an approvalGroup but that is not mapped to emails yet."
-    else
-      logger.debug "ApprovalGroup and email both empty, can't process this SendCart Request!"
-# Here we want to loock up the approvalGroup from the model somehow, that is for Raphy, then we will
-# send to every member of the approval Group!
-    end
+    mail(to: analysis['attention'], subject: "Please approve Cart Number: "+analysis["cartNumber"])
   end
 
   def approval_reply_received_email(analysis)
