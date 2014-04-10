93c433e09ecca4c23ddb8b487d90b39b97710008
Raphael Villas <raphael.villas@gmail.com>
2014-04-10 09:12:31 -0500

    Initial add of CartItem model

---
+++ spec/factories/cart_items.rb
@@ -0,0 +1,15 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :cart_item do
+    vendor "MyString"
+    description "MyText"
+    url "MyString"
+    notes "MyText"
+    quantity 1
+    details "MyText"
+    part_number "MyString"
+    price 1.5
+    cart_id 123456
+  end
+end
