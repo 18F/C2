dde329e7477122c45cadb88aad7278b8ab3ad2c1
Raphael Villas <raphael.villas@gmail.com>
2014-04-14 07:33:03 -0500

    Refactor: Moved totalPrice code into a helper and added a spec

--- app/controllers/communicarts_controller.rb
+++ app/controllers/communicarts_controller.rb
@@ -2,32 +2,25 @@ class CommunicartsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def send_cart
-    jparams = params
-    Cart.initialize_cart_with_items(jparams)
-    
-    approval_group_name = jparams['approvalGroup']
+    Cart.initialize_cart_with_items(params)
 
-    # Here we compute the prices of that jparams
-    # We add it back into the jparams
-    # jparams.merge(totalPriceValue: 5.55)
-    sum = jparams["cartItems"].reduce(0) do |sum,value|
-      sum + (value["qty"].gsub(/[^\d\.]/, '').to_f *  value["price"].gsub(/[^\d\.]/, '').to_f)
-    end
-    jparams = jparams.merge({ totalPriceValue: sum })
+    approval_group_name = params['approvalGroup']
+
+    params['totalPrice'] = total_price_from_params(params['cartItems'])
 
     if !approval_group_name.blank?
       approval_group = ApprovalGroup.find_by name:approval_group_name
       approval_group.approvers.each do | approver |
-        CommunicartMailer.cart_notification_email(approver.email_address,jparams).deliver
+        CommunicartMailer.cart_notification_email(approver.email_address,params).deliver
       end
     else
-      CommunicartMailer.cart_notification_email(jparams["email"],jparams).deliver
+      CommunicartMailer.cart_notification_email(params["email"],params).deliver
     end
     render json: { message: "This was a success"}, status: 200
   end
 
   def approval_reply_received
-    CommunicartMailer.approval_reply_received_email(jparams).deliver
+    CommunicartMailer.approval_reply_received_email(params).deliver
     render json: { message: "approval_reply_received"}, status: 200
   end
 end
