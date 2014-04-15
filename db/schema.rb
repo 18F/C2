479ce8a3f8342182cac2431abd0d36b76afce4c1
Raphael Villas <raphael.villas@gmail.com>
2014-04-14 20:50:14 -0500

    Added 'status' to Approver

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140415011506) do
+ActiveRecord::Schema.define(version: 20140415014856) do
 
   create_table "approval_groups", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140415011506) do
     t.integer  "approval_group_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status"
   end
 
   create_table "cart_items", force: true do |t|
