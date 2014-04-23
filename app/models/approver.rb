85637c656a1775e73f7cf487198e3bc6c70d23b4
Raphael Villas <raphael.villas@gmail.com>
2014-04-22 22:01:02 -0500

    Added Approver validations

--- app/models/approver.rb
+++ app/models/approver.rb
@@ -1,3 +1,6 @@
 class Approver  < ActiveRecord::Base
   belongs_to :approval_group
+  validates_presence_of :email_address
+  validates_uniqueness_of :email_address, scope: :approval_group_id
+
 end
