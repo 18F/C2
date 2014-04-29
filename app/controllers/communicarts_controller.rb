97f491959d2d704ab7ad6871632a85a78c6ccbd6
Robert L. Read <read.robert@gmail.com>
2014-04-29 18:45:42 -0400

    Now creating attachments

--- app/controllers/communicarts_controller.rb
+++ app/controllers/communicarts_controller.rb
@@ -2,10 +2,14 @@ class CommunicartsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def send_cart
-    Cart.initialize_cart_with_items(params)
+    cart = Cart.initialize_cart_with_items(params)
+
+    # Note: There surely should be a better way to fill this in since we just
+    # create the object above, but I don't really know how to do that...
+    cart = Cart.find_by(external_id: (params['cartNumber'].to_i))
+    cart.decorate
 
     approval_group_name = params['approvalGroup']
-
 
     sum = params['cartItems'].reduce(0) do |sum,value|
       sum + (value["qty"].gsub(/[^\d\.]/, '').to_f *  value["price"].gsub(/[^\d\.]/, '').to_f)
@@ -16,10 +20,10 @@ class CommunicartsController < ApplicationController
       approval_group = ApprovalGroup.find_by(name: approval_group_name)
 
       approval_group.approvers.each do | approver |
-        CommunicartMailer.cart_notification_email(approver.email_address,params).deliver
+        CommunicartMailer.cart_notification_email(approver.email_address,params,cart).deliver
       end
     else
-      CommunicartMailer.cart_notification_email(params["email"],params).deliver
+      CommunicartMailer.cart_notification_email(params["email"],params,cart).deliver
     end
     render json: { message: "This was a success"}, status: 200
   end
@@ -33,7 +37,7 @@ class CommunicartsController < ApplicationController
     cart.update_approval_status
 
     cart_report = EmailStatusReport.new(cart)
-    CommunicartMailer.approval_reply_received_email(params, cart_report).deliver
+    CommunicartMailer.approval_reply_received_email(params, cart_report, cart).deliver
     render json: { message: "approval_reply_received"}, status: 200
   end
 
