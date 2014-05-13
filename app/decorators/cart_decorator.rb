de0e1e530f76200f09e074f6f37ff9e5d8f880f5
Robert L. Read <read.robert@gmail.com>
2014-05-12 22:47:12 -0400

    fixing price

--- app/decorators/cart_decorator.rb
+++ app/decorators/cart_decorator.rb
@@ -2,7 +2,7 @@ class CartDecorator < Draper::Decorator
   delegate_all
 
   def total_price
-    object.cart_items.map(&:price).inject { |sum, price| sum + price }
+    object.cart_items.reduce(0) do |sum,citem| sum + (citem.quantity * citem.price) end
   end
 
 end
--- spec/decorators/cart_decorator_spec.rb
+++ spec/decorators/cart_decorator_spec.rb
@@ -10,11 +10,11 @@ describe CartDecorator do
               external_id: '10203040'
               ).decorate
 
-    cart.cart_items << FactoryGirl.create(:cart_item, description: "Item 1", price: 1.00)
-    cart.cart_items << FactoryGirl.create(:cart_item, description: "Item 2", price: 2.25)
-    cart.cart_items << FactoryGirl.create(:cart_item, description: "Item 3", price: 3.50)
+    cart.cart_items << FactoryGirl.create(:cart_item, description: "Item 1", price: 1.00, quantity: 2)
+    cart.cart_items << FactoryGirl.create(:cart_item, description: "Item 2", price: 2.25, quantity: 3)
+    cart.cart_items << FactoryGirl.create(:cart_item, description: "Item 3", price: 3.50, quantity: 4)
 
-    expect(cart.total_price).to eq 6.75
+    expect(cart.total_price).to eq (2 * 1.0 + 3 * 2.25 + 4 * 3.5)
 
     end
   end
