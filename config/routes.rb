991fdae59250d610c3240c58f18f80c19c46c0d5
Robert L. Read <read.robert@gmail.com>
2014-07-05 17:41:27 -0400

    rudiments of new endpoint working for informal creation of cart from email

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 C2::Application.routes.draw do
   post 'send_cart' => 'communicarts#send_cart'
+  post 'create_informal_cart' => 'communicarts#create_informal_cart'
   post 'approval_reply_received' => 'communicarts#approval_reply_received'
   match 'approval_response', to: 'communicarts#approval_response', via: [:get, :put]
   root :to => 'home#index'
