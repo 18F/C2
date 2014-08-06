25806f20137780f9b81f14988f4d2bef08570b8f
Raphy Villas <raphael.villas@gmail.com>
2014-08-06 16:48:01 -0500

    Remove old methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   before_filter :setup_session_user
-  before_filter :reset_session, :only => [:start]
-  # before_filter :merge_params_to_session
   before_filter :setup_mygov_client
   before_filter :setup_mygov_access_token
 
@@ -20,7 +18,7 @@ class HomeController < ApplicationController
     redirect_to root_url
   end
 
-  private
+private
 
   def setup_session_user
     session[:user] = {} if session[:user].nil?
@@ -34,28 +32,4 @@ class HomeController < ApplicationController
     @mygov_access_token = OAuth2::AccessToken.new(@mygov_client, session[:token]) if session
   end
 
-  def merge_params_to_session
-    session.deep_merge!(params)
-  end
-
-
-  # >>>>>>>>>>>>>>>.
-  # FOR REFERENCE:
-  # >>>>>>>>>>>>>>>
-  def store_form_data
-    form_number = 'ss-5' if session[:reasons][:married].present?
-    form_number = '79960' if session[:reasons][:court_order].present?
-    body = {}
-    body.merge!(:form_number => form_number)
-    body.merge!(:data => session[:user])
-    @mygov_access_token.post("/api/forms", :body => body)
-  end
-
-  def create_tasks
-    task_items = []
-    task_items << {:name => 'Get a new Social Security card', :url => 'http://www.socialsecurity.gov/online/ss-5.pdf'} if session[:reasons][:married].present?
-    task_items << {:name => 'Renew your passport', :url => 'http://www.state.gov/documents/organization/79960.pdf'} if session[:reasons][:court_order].present?
-    tasks_response = @mygov_access_token.post("/api/tasks", :params => {:task => {:name => 'Change your name', :task_items_attributes => task_items}})
-    JSON.parse(tasks_response.body)
-  end
 end
