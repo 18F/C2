c65b0db380d7efbc809b3776a994b8f1f3216555
Raphy Villas <raphael.villas@gmail.com>
2014-08-08 16:51:24 -0500

    Initial authentication redirect working

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,12 @@
 class CartsController < ApplicationController
+  before_filter :auth_user
+
   def show
     cart = Cart.find params[:id]
     @cart = cart.decorate
+  end
+
+  def auth_user
+    redirect_to root_url if session[:user].empty?
   end
 end
