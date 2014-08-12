3b759cd26ef681219e7c31990697524900fc8be4
Robert L. Read <read.robert@gmail.com>
2014-08-12 18:48:04 -0400

    Clarifications as Raphy suggested

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,6 +1,6 @@
 require 'settingslogic'
 
 class Settings < Settingslogic
-  source "#{Rails.root}/config/application.yml"
-  namespace Rails.env
+  source "#{Rails.root}/config/c2_mario_constants.yml"
+  namespace "constants"
 end
