2f37191fe96d1749a910b9044438ecfb4d13c789
Raphy Villas <raphael.villas@gmail.com>
2014-08-12 13:21:48 -0500

    Tidying things up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  helper_method :user_signed_in?
+
+private
+  def current_user
+    begin
+      @current_user ||= User.find_by(email_address: session[:user]['email']) if session[:user]
+    rescue Exception => e
+      nil
+    end
+  end
+
+  def user_signed_in?
+    return true if current_user
+  end
+
+  def authenticate_user!
+    if !current_user
+      session[:return_to] = request.fullpath
+      redirect_to root_url, :alert => 'You need to sign in for access to this page.'
+    end
+  end
+
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,9 @@
 class CartsController < ApplicationController
-  before_filter :auth_user
+  before_filter :authenticate_user!
 
   def show
     cart = Cart.find params[:id]
     @cart = cart.decorate
   end
 
-  def auth_user
-    redirect_to root_url if session[:user].empty?
-  end
 end
