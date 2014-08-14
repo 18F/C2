0a74c23715fdc5c88084bb2179ac83531d872e1e
Robert L. Read <read.robert@gmail.com>
2014-08-14 12:36:12 -0400

    extending factorygirl

--- spec/factories/cart_items.rb
+++ spec/factories/cart_items.rb
@@ -10,4 +10,15 @@ FactoryGirl.define do
     price 1.5
     cart_id 1357911
   end
+  factory :cart_item_with_trait do
+    vendor "Test Vendor2"
+    description "This is another test cart item"
+    url "http://some.product.url/12345"
+    notes "This is a note for a test cart item"
+    quantity 100
+    details "Details for a test cart item"
+    part_number "1A2B3C4D"
+    price 1.5
+    cart_id 1357911
+  end
 end
--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -25,5 +25,20 @@ FactoryGirl.define do
       end
     end
 
+    factory :cart_with_approvals_and_items do
+      after :create do |cart|
+        approver1 = FactoryGirl.create(:user, email_address: 'approver3@some-dot-gov.gov', first_name: 'Liono', last_name: 'Approver1')
+        approver2 = FactoryGirl.create(:user, email_address: 'approver4@some-dot-gov.gov', first_name: 'Liono', last_name: 'Approver2')
+        requester = FactoryGirl.create(:user, email_address: 'requester2@some-dot-gov.gov', first_name: 'Liono', last_name: 'Requester')
+
+        cart.approvals << FactoryGirl.create(:approval, role: 'approver', user_id: approver1.id)
+        cart.approvals << FactoryGirl.create(:approval, role: 'approver', user_id: approver2.id)
+        cart.approvals << FactoryGirl.create(:approval, role: 'requester', user_id: requester.id)
+        cart.cart_items << FactoryGirl.create(:cart_item, cart_id: cart.id)
+        cart.cart_items << FactoryGirl.create(:cart_item, cart_id: cart.id,vendor: "Spud Vendor")
+        cart.save
+      end
+    end
+
   end
 end
