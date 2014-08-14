6708b2a2fae358a76f9c0795d1293eb1eb0daeb6
Raphy Villas <raphael.villas@gmail.com>
2014-08-14 17:08:45 -0500

    Removed deploy files and added an example

--- config/deploy/development.rb
+++
@@ -1,7 +0,0 @@
-server '54.214.142.37', :app, :web, :primary => true
-set :domain, '54.214.142.37'
-
-role :app, domain
-role :web, domain
-role :db,  domain, :primary => true
-
--- config/deploy/production.rb
+++
@@ -1,6 +0,0 @@
-server '54.185.133.124', :app, :web, :primary => true
-set :domain, '54.185.133.124'
-
-role :app, domain
-role :web, domain
-role :db,  domain, :primary => true
