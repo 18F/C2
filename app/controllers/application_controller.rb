e882e51bea70c3682f30c71147bd29a72d9c0db2
Raphy Villas <raphael.villas@gmail.com>
2014-08-21 12:52:13 -0500

    Minor cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user
-  helper_method :user_signed_in?
+  helper_method :current_user, :user_signed_in?
 
 private
   def current_user
