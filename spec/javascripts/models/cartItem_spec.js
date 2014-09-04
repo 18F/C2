6ff1c7333e8485b60c82f21879f752c30732ba6a
sashax <alexander.magee@gsa.gov>
2014-09-04 11:47:36 -0700

    Finish reformatting. Add test.

--- spec/javascripts/models/cartItem_spec.js
+++ spec/javascripts/models/cartItem_spec.js
@@ -7,10 +7,17 @@ describe('CartItem', function() {
     expect(cartItem.get('imageUrl')).toEqual('');
     expect(cartItem.get('quantity')).toEqual(0);
     expect(cartItem.get('vendor')).toEqual('');
+    expect(cartItem.subtotal()).toEqual('0.00');
   });
 
   it('should not be valid', function(){
     var cartItem = new CartItem();
       expect(cartItem.isValid()).toBeFalsy();
+  });
+
+  it('should have subtotal of 33', function() {
+      var cartItem = new CartItem();
+      cartItem.set({price: 11.00, quantity: 3});
+      expect(cartItem.subtotal()).toEqual('33.00');
   });
 });
