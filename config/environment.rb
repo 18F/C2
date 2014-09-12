d1c1e557cf6e7228d03496e0bf6c7088da02eea4
Raphy Villas <raphael.villas@gmail.com>
2014-09-12 15:27:39 -0500

    Moved initiate cart code from #send_cart to a new command, InitiateCartApproval

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+#Load all commands
 require "#{Rails.root}/app/commands/base"
 require "#{Rails.root}/app/commands/approval/update_from_approval_response.rb"
+require "#{Rails.root}/app/commands/approval/initiate_cart_approval.rb"
 
 # Initialize the Rails application.
 C2::Application.initialize!
