08edcb52fabe7a8cccee6dc9018ba27abef71367
Raphy Villas <raphael.villas@gmail.com>
2014-10-01 09:43:15 -0500

    [#78718526] Alignment and color of bullets working

--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -14,7 +14,7 @@ FactoryGirl.define do
 
     factory :cart_with_approvals do
       after :create do |cart|
-        
+
         approver1 = FactoryGirl.create(:user, email_address: 'approver1@some-dot-gov.gov', first_name: 'Liono', last_name: 'Approver1')
         approver2 = FactoryGirl.create(:user, email_address: 'approver2@some-dot-gov.gov', first_name: 'Liono', last_name: 'Approver2')
         requester = FactoryGirl.create(:user, email_address: 'requester@some-dot-gov.gov', first_name: 'Liono', last_name: 'Requester')
@@ -37,8 +37,13 @@ FactoryGirl.define do
         cart.approvals << FactoryGirl.create(:approval, role: 'requester', user_id: requester.id)
         cart.cart_items << FactoryGirl.create(:cart_item, cart_id: cart.id)
         cart.cart_items << FactoryGirl.create(:cart_item, cart_id: cart.id,vendor: "Spud Vendor")
-        cart.cart_items[0].cart_item_traits << FactoryGirl.create(:cart_item_trait,name: 'socio',value: "wso")
-        cart.cart_items[0].cart_item_traits << FactoryGirl.create(:cart_item_trait,name: 'features',value: "spud")
+        cart.cart_items[0].cart_item_traits << FactoryGirl.create(:cart_item_trait,name: 'socio',value: "W")
+        cart.cart_items[0].cart_item_traits << FactoryGirl.create(:cart_item_trait,name: 'socio',value: "S")
+        cart.cart_items[0].cart_item_traits << FactoryGirl.create(:cart_item_trait,name: 'socio',value: "O")
+        cart.cart_items[0].cart_item_traits << FactoryGirl.create(:cart_item_trait,name: 'features',value: "discount")
+        cart.cart_items[0].cart_item_traits << FactoryGirl.create(:cart_item_trait,name: 'features',value: "feature2")
+        cart.cart_items[0].cart_item_traits << FactoryGirl.create(:cart_item_trait,name: 'green',value: 'blah')
+
         cart.save
       end
     end
