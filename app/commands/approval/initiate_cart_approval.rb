67a7e99cb76c77a4c85b26e0e5936b39aac8a64e
Aidan Feldman <aidan.feldman@gsa.gov>
2014-10-24 13:38:58 -0400

    remove unnecessary rescues
    were interfering with debugging because they don't give the proper stack
    trace

--- app/commands/approval/initiate_cart_approval.rb
+++ app/commands/approval/initiate_cart_approval.rb
@@ -2,26 +2,21 @@ module Commands
   module Approval
     class InitiateCartApproval < Commands::Base
       def perform(params)
-
-        begin
-          cart = Cart.initialize_cart_with_items(params).reload.decorate
-          if params['approvalGroup'] && !params['approvalGroup'].blank?
-            cart.process_approvals_from_approval_group unless cart.approvals.any?
-          else
-            cart.process_approvals_without_approval_group(params)
-          end
-
-          cart.import_cart_properties(params['properties'])
-          cart.import_cart_items(params['cartItems'])
-          cart.import_initial_comments(params['initiationComment']) unless params['initiationComment'].blank?
-          cart.deliver_approval_emails
-          c2 = cart.instance_variable_get(:@object)
-          c3 = cart.object
-          return c3
-        rescue => error
-          raise "Something went wrong initiating cart approval: #{error}"
+        cart = Cart.initialize_cart_with_items(params).reload.decorate
+        if params['approvalGroup'] && !params['approvalGroup'].blank?
+          cart.process_approvals_from_approval_group unless cart.approvals.any?
+        else
+          cart.process_approvals_without_approval_group(params)
         end
 
+        cart.import_cart_properties(params['properties'])
+        cart.import_cart_items(params['cartItems'])
+        cart.import_initial_comments(params['initiationComment']) unless params['initiationComment'].blank?
+        cart.deliver_approval_emails
+        c2 = cart.instance_variable_get(:@object)
+        c3 = cart.object
+
+        c3
       end
     end
   end
