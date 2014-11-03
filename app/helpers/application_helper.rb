2b639a856f4f5bd30ea5ae230c13624d5fb399bd
Aidan Feldman <aidan.feldman@gsa.gov>
2014-11-03 14:31:45 -0500

    simplify select options helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,6 @@
 module ApplicationHelper
   # Variation of http://git.io/ugBzaQ
-  def enum_option_pairs(record, enum)
-    reader = enum.to_s.pluralize
-    record = record.class unless record.respond_to?(reader)
-    options = record.send(reader)
+  def humanized_options_for_select(options)
     options = options.map do |key, val|
       [key.humanize, val]
     end
