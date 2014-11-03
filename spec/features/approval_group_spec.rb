3cee785f3e65ca89f0bc2236a73f6e7acf2f8c3d
Aidan Feldman <aidan.feldman@gsa.gov>
2014-11-03 12:42:49 -0500

    add form option for selecting linear flow

---
+++ spec/features/approval_group_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "approval groups" do
+  it "saves the linear flow option" do
+    login_with_oauth
+    visit '/approval_groups/new'
+
+    fill_in 'Name', with: 'MyAwesomeApprovalGroup'
+    page.select 'Linear', from: 'Approval flow'
+    fill_in 'Requester', with: 'test-requester-1@some-dot-gov.gov'
+    fill_in 'Approver 1', with: 'test-approver-1@some-dot-gov.gov'
+    fill_in 'Approver 2', with: 'test-approver-2@some-dot-gov.gov'
+
+    expect {
+      click_button 'Create Approval Group'
+    }.to change { ApprovalGroup.count }.by(1)
+
+    group = ApprovalGroup.last
+    expect(group.flow).to eq(:linear)
+  end
+end
