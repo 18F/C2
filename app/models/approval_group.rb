63717c3eeb4342c5fa4b692c0bb539e54ee60759
Aidan Feldman <aidan.feldman@gsa.gov>
2014-11-03 14:24:07 -0500

    validate presence of ApprovalGroup#flow

--- app/models/approval_group.rb
+++ app/models/approval_group.rb
@@ -2,10 +2,12 @@ class ApprovalGroup < ActiveRecord::Base
   enum flow: [:parallel, :linear]
 
   belongs_to :cart
-  validates_presence_of :name
-  validates_uniqueness_of :name
   has_many :user_roles
   has_many :users, through: :user_roles
+
+  validates :name, presence: true, uniqueness: true
+  validates :flow, presence: true
+
 
   def requester_id
     if ur = user_roles.find { |r| r.role == "requester"}
