bbcf9347aa0e1e116aa72cdcb8a5d17bdaf7bc47
Raphy Villas <raphael.villas@gmail.com>
2014-11-04 14:13:20 -0600

    Added a sample integration script

--- spec/factories/apps.rb
+++ spec/factories/apps.rb
@@ -3,7 +3,8 @@
 FactoryGirl.define do
   factory :app do
     name "The Amazing Test Application"
-    slug "the-amazing-test-application"
+    access_id "1a2b3c4d5e"
+    secret_key "4COFtgYMGDyrrY+aZhoXOMHLxUBDV/stc+AEmV5++4EmmqwakXSTKft0ma3/o58njoaiqjkf4WTMYG3//8LpIA"
 
   end
 end
