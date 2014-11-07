0a271c61c9a5fd296030638bbc8c935ed408e6c8
Aidan Feldman <aidan.feldman@gsa.gov>
2014-11-06 20:35:09 -0500

    unify approval logic

--- app/commands/approval/update_from_approval_response.rb
+++ app/commands/approval/update_from_approval_response.rb
@@ -1,22 +1,11 @@
 module Commands
   module Approval
     class UpdateFromApprovalResponse < Commands::Base
-      def perform(params)
-        target_cart = Cart.find_by(id: params[:cart_id].to_i)
-        approval = target_cart.approvals.where(user_id: params[:user_id]).first
-        action = params[:approver_action]
-
-        approval.update_attributes(status: mapped_attributes(action))
+      def perform(approval, new_status)
+        approval.update_attributes(status: new_status)
         approval.cart.update_approval_status
         CommunicartMailer.approval_reply_received_email(approval).deliver
       end
-
-      private
-
-      def mapped_attributes action
-        Cart::APPROVAL_ATTRIBUTES_MAP[action.to_sym]
-      end
-
     end
   end
 end
