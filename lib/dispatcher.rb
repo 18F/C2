43d3cc1d3a77c98e02fa3fcbe18a15c9281beda4
Aidan Feldman <aidan.feldman@gsa.gov>
2014-11-13 16:10:18 -0500

    convert dispatcher to a class
    makes subclass methods easier to work with

--- lib/dispatcher.rb
+++ lib/dispatcher.rb
@@ -1,5 +1,13 @@
-module Dispatcher
-  def get_dispatcher(approval_group)
+class Dispatcher
+  def deliver_new_cart_emails(cart)
+    raise "Must be implemented by subclass"
+  end
+
+  def deliver_approval_email(approval)
+    raise "Must be implemented by subclass"
+  end
+
+  def self.get_dispatcher(approval_group)
     # TODO switch based on flow
     # case approval_group.flow
     # when 'parallel'
@@ -8,19 +16,16 @@ module Dispatcher
     #   LinearDispatcher
     # end
 
-    ParallelDispatcher
+    ParallelDispatcher.new
   end
-  module_function :get_dispatcher
 
-  def deliver_new_cart_emails(cart)
+  def self.deliver_new_cart_emails(cart)
     dispatcher = self.get_dispatcher(cart.approval_group)
     dispatcher.deliver_new_cart_emails(cart)
   end
-  module_function :deliver_new_cart_emails
 
-  def deliver_approval_email(approval)
+  def self.deliver_approval_email(approval)
     dispatcher = self.get_dispatcher(approval.approval_group)
     dispatcher.deliver_approval_email(approval)
   end
-  module_function :deliver_approval_email
 end
--- spec/lib/parallel_dispatcher_spec.rb
+++ spec/lib/parallel_dispatcher_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 describe ParallelDispatcher do
   let(:cart) { FactoryGirl.create(:cart_with_approvals) }
+  let(:dispatcher) { ParallelDispatcher.new }
   let(:deliveries) { ActionMailer::Base.deliveries }
   let(:delivery_emails) { deliveries.map {|email| email.to[0] }.sort }
 
   describe '#deliver_new_cart_emails' do
     it "sends emails to all approvers" do
-      ParallelDispatcher.deliver_new_cart_emails(cart)
+      dispatcher.deliver_new_cart_emails(cart)
       expect(delivery_emails).to eq([
         'approver1@some-dot-gov.gov',
         'approver2@some-dot-gov.gov'
@@ -19,19 +20,19 @@ describe ParallelDispatcher do
       allow(ApiToken).to receive_message_chain(:where, :where, :last).and_return(api_token)
       expect(ApiToken).to receive(:create!).exactly(2).times
 
-      ParallelDispatcher.deliver_new_cart_emails(cart)
+      dispatcher.deliver_new_cart_emails(cart)
     end
 
     it 'sends a cart notification email to observers' do
       cart.approvals << FactoryGirl.create(:approval_with_user, role: 'observer')
       expect(CommunicartMailer).to receive_message_chain(:cart_observer_email, :deliver)
-      ParallelDispatcher.deliver_new_cart_emails(cart)
+      dispatcher.deliver_new_cart_emails(cart)
     end
   end
 
   describe '#deliver_approval_email' do
     it "sends to the requester" do
-      ParallelDispatcher.deliver_approval_email(cart.approvals.first)
+      dispatcher.deliver_approval_email(cart.approvals.first)
       expect(delivery_emails).to eq(['requester@some-dot-gov.gov'])
     end
   end
