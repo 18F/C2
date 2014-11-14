fa0f32dfb51b769aed7f14078246d46d91660780
Aidan Feldman <aidan.feldman@gsa.gov>
2014-11-14 18:13:55 -0500

    remove scopes from config
    there shouldn't be any need to change them independent from code change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   MYGOV_CLIENT_ID = ENV['MYGOV_CLIENT_ID']
   MYGOV_SECRET_ID = ENV['MYGOV_SECRET_ID']
   MYGOV_HOME = ENV['MYGOV_HOME']
-  SCOPES = ENV['MYGOV_SCOPES']
+  SCOPES = 'profile.email'
   provider :myusa, MYGOV_CLIENT_ID, MYGOV_SECRET_ID, :scope => SCOPES, :client_options => {:site => MYGOV_HOME, :token_url => "/oauth/authorize"}
 end
