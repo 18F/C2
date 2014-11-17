1d533baf1091fcde5a7b2c05fba9e34091e40752
Aidan Feldman <aidan.feldman@gsa.gov>
2014-11-17 14:27:01 -0500

    fix InitiateCartApproval specs

--- app/commands/approval/initiate_cart_approval.rb
+++ app/commands/approval/initiate_cart_approval.rb
@@ -1,7 +1,15 @@
 module Commands
   module Approval
     class InitiateCartApproval < Commands::Base
-      def perform(params)
+      def import_details(cart, params)
+        cart.import_cart_properties(params['properties'])
+        cart.import_cart_items(params['cartItems'])
+        unless params['initiationComment'].blank?
+          cart.import_initial_comments(params['initiationComment'])
+        end
+      end
+
+      def setup_cart(params)
         cart = Cart.initialize_cart_with_items(params).reload.decorate
         if params['approvalGroup'].present?
           unless cart.approvals.any?
@@ -11,11 +19,13 @@ module Commands
           cart.process_approvals_without_approval_group(params)
         end
 
-        cart.import_cart_properties(params['properties'])
-        cart.import_cart_items(params['cartItems'])
-        unless params['initiationComment'].blank?
-          cart.import_initial_comments(params['initiationComment'])
-        end
+        self.import_details(cart, params)
+
+        cart
+      end
+
+      def perform(params)
+        cart = self.setup_cart(params)
 
         dispatcher = ParallelDispatcher.new
         dispatcher.deliver_new_cart_emails(cart)
