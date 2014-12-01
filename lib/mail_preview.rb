f13aefe18d89f6f03ca1518c78a09fcf58cd0918
Aidan Feldman <aidan.feldman@gsa.gov>
2014-12-01 17:57:26 -0500

    add mail previews for other mailer methods

--- lib/mail_preview.rb
+++ lib/mail_preview.rb
@@ -1,5 +1,36 @@
 class MailPreview < MailView
+  def cart_notification_email
+    CommunicartMailer.cart_notification_email(email, approval)
+  end
+
+  def cart_observer_email
+    CommunicartMailer.cart_observer_email(email, cart)
+  end
+
   def approval_reply_received_email
-    CommunicartMailer.approval_reply_received_email(Approval.last)
+    CommunicartMailer.approval_reply_received_email(approval)
+  end
+
+  def comment_added_email
+    CommunicartMailer.comment_added_email(comment, email)
+  end
+
+
+  private
+
+  def email
+    'recipient@example.com'
+  end
+
+  def approval
+    Approval.last
+  end
+
+  def cart
+    Cart.last
+  end
+
+  def comment
+    Comment.last
   end
 end
