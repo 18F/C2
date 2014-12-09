7328b4e400db6a07844530f5bed9a0b226de4473
Raphy Villas <raphael.villas@gmail.com>
2014-12-09 17:22:34 -0500

    Revert "inline styles in mail_view"
    This reverts commit 9bf6770d5eeacc4312b84f105a49850c93da48c3.

--- lib/mail_preview.rb
+++ lib/mail_preview.rb
@@ -1,22 +1,18 @@
 class MailPreview < MailView
   def cart_notification_email
-    mail = CommunicartMailer.cart_notification_email(email, pending_approval)
-    inline_styles(mail)
+    CommunicartMailer.cart_notification_email(email, pending_approval)
   end
 
   def cart_observer_email
-    mail = CommunicartMailer.cart_observer_email(email, cart)
-    inline_styles(mail)
+    CommunicartMailer.cart_observer_email(email, cart)
   end
 
   def approval_reply_received_email
-    mail = CommunicartMailer.approval_reply_received_email(received_approval)
-    inline_styles(mail)
+    CommunicartMailer.approval_reply_received_email(received_approval)
   end
 
   def comment_added_email
-    mail = CommunicartMailer.comment_added_email(comment, email)
-    inline_styles(mail)
+    CommunicartMailer.comment_added_email(comment, email)
   end
 
 
@@ -40,16 +36,5 @@ class MailPreview < MailView
 
   def comment
     Comment.where(commentable_type: 'Cart').last
-  end
-
-
-  # https://github.com/Mange/roadie-rails/blob/v1.0.3/lib/roadie/rails/mailer.rb#L6
-
-  def inline_styles(mail)
-    Roadie::Rails::MailInliner.new(mail, roadie_options).execute
-  end
-
-  def roadie_options
-    ::Rails.application.config.roadie
   end
 end
