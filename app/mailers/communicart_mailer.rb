ba351ab0f2ea8173e21cda3ddac7add7b61f570a
Aidan Feldman <aidan.feldman@gsa.gov>
2014-12-16 17:29:08 -0500

    refactor mailer code to make testing easier

--- app/mailers/communicart_mailer.rb
+++ app/mailers/communicart_mailer.rb
@@ -5,35 +5,14 @@ class CommunicartMailer < ActionMailer::Base
 
   def cart_notification_email(email, approval)
     cart = approval.cart
-    @url = ENV['NOTIFICATION_URL']
-    @cart = cart.decorate
     @approval = approval
-    @token = ApiToken.where(user_id: @approval.user_id).where(cart_id: @cart.id).last
-    @prefix_template = @cart.prefix_template_name
+    @token = ApiToken.where(user_id: @approval.user_id).where(cart_id: cart.id).last
 
-    set_attachments(cart)
-
-    approval_format = Settings.email_title_for_approval_request_format
-    mail(
-         to: email,
-         subject: approval_format % [ cart.requester.full_name,cart.external_id],
-         from: ENV['NOTIFICATION_FROM_EMAIL']
-         )
+    send_cart_email(email, cart)
   end
 
   def cart_observer_email(email, cart)
-    @url = ENV['NOTIFICATION_URL']
-    @cart = cart.decorate
-    @prefix_template = @cart.prefix_template_name
-
-    set_attachments(cart)
-
-    approval_format = Settings.email_title_for_approval_request_format
-    mail(
-         to: email,
-         subject: approval_format % [ cart.requester.full_name,cart.external_id],
-         from: ENV['NOTIFICATION_FROM_EMAIL']
-         )
+    send_cart_email(email, cart)
   end
 
   def approval_reply_received_email(approval)
@@ -50,7 +29,7 @@ class CommunicartMailer < ActionMailer::Base
     mail(
          to: to_address,
          subject: "User #{approval.user.email_address} has #{approval.status} cart ##{cart.external_id}",
-         from: ENV['NOTIFICATION_FROM_EMAIL']
+         from: from_email
          )
   end
 
@@ -65,7 +44,7 @@ class CommunicartMailer < ActionMailer::Base
     mail(
          to: to_email,
          subject: "A comment has been added to cart item '#{@cart_item.description}'",
-         from: ENV['NOTIFICATION_FROM_EMAIL']
+         from: from_email
          )
   end
 
@@ -78,5 +57,25 @@ class CommunicartMailer < ActionMailer::Base
       attachments['Communicart' + cart.name + '.comments.csv'] = Exporter::Comments.new(cart).to_csv
       attachments['Communicart' + cart.name + '.approvals.csv'] = Exporter::Approvals.new(cart).to_csv
     end
+  end
+
+  # for easier stubbing in tests
+  def from_email
+    ENV['NOTIFICATION_FROM_EMAIL']
+  end
+
+  def send_cart_email(email, cart)
+    @url = ENV['NOTIFICATION_URL']
+    @cart = cart.decorate
+    @prefix_template = @cart.prefix_template_name
+
+    set_attachments(cart)
+
+    approval_format = Settings.email_title_for_approval_request_format
+    mail(
+      to: email,
+      subject: approval_format % [ cart.requester.full_name,cart.external_id],
+      from: from_email
+    )
   end
 end
