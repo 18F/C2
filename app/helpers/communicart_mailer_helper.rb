ab9c57de235fafd32974d0ac1b9735f73d36b11f
Raphy Villas <raphael.villas@gmail.com>
2014-12-17 18:21:02 -0600

    Handle first and last icons

--- app/helpers/communicart_mailer_helper.rb
+++ app/helpers/communicart_mailer_helper.rb
@@ -2,4 +2,10 @@ module CommunicartMailerHelper
   def status_icon_tag(status)
     image_tag("icon-#{status}.png", class: "status-icon #{status}")
   end
+
+  def order_class(index, count)
+    return "class=last" if index == count - 1
+    return "class=first" if index == 0
+    return ""
+  end
 end
