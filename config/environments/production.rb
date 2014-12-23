1109513c82c56e2cb1e555423b2a338ddac6c23d
Aidan Feldman <aidan.feldman@gsa.gov>
2014-12-23 18:02:51 -0500

    include production.rb

---
+++ config/environments/production.rb
@@ -0,0 +1,28 @@
+C2::Application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  config.serve_static_assets = true
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = true
+  config.assets.digest = true
+  config.assets.version = '1.0'
+  config.log_level = :info
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               ENV.fetch('SMTP_DOMAIN'),
+    user_name:            ENV.fetch('SMTP_USERNAME'),
+    password:             ENV.fetch('SMTP_PASSWORD'),
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV.fetch('HOST_URL') }
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.log_formatter = ::Logger::Formatter.new
+end
