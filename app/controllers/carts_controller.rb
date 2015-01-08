29de5800b265a552e9894292e3fab627b0b3a22b
Raphy Villas <raphael.villas@gmail.com>
2015-01-08 15:52:07 -0600

    [#85744742] Refactoring on carts#index

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,16 +11,17 @@ class CartsController < ApplicationController
   def index
     # TODO: handle role on a cart-by-cart basis
     # (including action buttons in approver/open case)
-    @role = 'requester'
-    @carts = current_user.carts.where(approvals: {role: @role})
-    if @carts.empty?
-      @role = 'approver'
-      @carts = current_user.carts.where(approvals: {role: @role})
-    end
+    @carts = current_user.carts.where(approvals: { role: requester_or_approver })
   end
 
   def archive
     @role = params[:role] || 'requester'
     @closed_cart_full_list = current_user.carts.where(approvals: {role: @role}).closed
+  end
+
+  def requester_or_approver
+    ['requester','approver'].each do |role|
+      break role if current_user.carts.where(approvals: { role: role }).any?
+    end
   end
 end
