4fb03de39db7bd1f8531c34701fa20eadf8c13b6
Raphy Villas <raphael.villas@gmail.com>
2015-02-12 16:27:20 -0600

    [#87736992] Updated configs for rack_session_access

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,8 @@ CodeClimate::TestReporter.start
 require 'simplecov'
 SimpleCov.start 'rails'
 
+require 'rack_session_access/capybara'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
