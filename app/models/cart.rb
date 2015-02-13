30df018ef58b05a400c0b8aa4bb232d5d3c433e2
Aidan Feldman <aidan.feldman@gsa.gov>
2015-02-12 22:15:02 -0500

    fetch users from Cart via SQL

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -45,8 +45,7 @@ class Cart < ActiveRecord::Base
   end
 
   def approvers
-    # TODO do through SQL
-    self.approver_approvals.map(&:user)
+    self.approval_users.merge(self.approver_approvals)
   end
 
   def awaiting_approvals
@@ -54,8 +53,7 @@ class Cart < ActiveRecord::Base
   end
 
   def awaiting_approvers
-    # TODO do through SQL
-    self.awaiting_approvals.map(&:user)
+    self.approval_users.merge(self.awaiting_approvals)
   end
 
   def ordered_approvals
@@ -85,7 +83,7 @@ class Cart < ActiveRecord::Base
   end
 
   def requester
-    approvals.where(role: 'requester').first.try(:user)
+    self.approval_users.where(role: 'requester').first
   end
 
   def observers
