2effc89ff174665abe1294f182375e8a99e7caa3
Aidan Feldman <aidan.feldman@gsa.gov>
2015-03-02 13:59:22 -0500

    fix proposal migration to not rely on models

--- db/migrate/20141214220050_create_proposals.rb
+++ db/migrate/20141214220050_create_proposals.rb
@@ -1,4 +1,16 @@
 class CreateProposals < ActiveRecord::Migration
+  ## http://makandracards.com/makandra/15575-how-to-write-complex-migrations-in-rails ##
+  class TempCart < ActiveRecord::Base
+    self.table_name = 'carts'
+    belongs_to :temp_proposal, foreign_key: 'proposal_id'
+  end
+
+  class TempProposal < ActiveRecord::Base
+    self.table_name = 'proposals'
+    has_many :temp_carts
+  end
+  ######################################################################################
+
   def change
     create_table :proposals do |t|
       t.string :status
@@ -8,11 +20,12 @@ class CreateProposals < ActiveRecord::Migration
     add_reference :carts, :proposal
 
     reversible do |dir|
-      Cart.reset_column_information
+      TempCart.reset_column_information
+      TempProposal.reset_column_information
 
       dir.up do
-        Cart.find_each do |cart|
-          cart.create_proposal!(
+        TempCart.find_each do |cart|
+          cart.create_temp_proposal!(
             status: cart.status,
             flow: cart.flow,
             created_at: cart.created_at,
@@ -22,8 +35,8 @@ class CreateProposals < ActiveRecord::Migration
       end
 
       dir.down do
-        Proposal.find_each do |proposal|
-          proposal.cart.update_attributes!(
+        TempProposal.find_each do |proposal|
+          proposal.temp_cart.update_attributes!(
             status: proposal.status,
             flow: proposal.flow
           )
