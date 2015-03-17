cbbac1029871b0924081140676d5fcb78aa59076
CM Lubinski <christopher.lubinski@gsa.gov>
2015-03-17 16:21:12 +0000

    Missed these specs

--- spec/models/ncr/work_order_spec.rb
+++ spec/models/ncr/work_order_spec.rb
@@ -2,10 +2,10 @@ describe Ncr::WorkOrder do
   describe 'fields_for_display' do
     it "shows BA61 fields" do
       wo = Ncr::WorkOrder.new(
-        amount: 1000, description: "Some description", expense_type: "BA61",
-        vendor: "Some Vend", not_to_exceed: false, emergency: true,
+        amount: 1000, expense_type: "BA61", vendor: "Some Vend", 
+        not_to_exceed: false, emergency: true, rwa_number: "RWWAAA #",
         building_number: Ncr::WorkOrder::BUILDING_NUMBERS[0],
-        rwa_number: "RWWAAA #", office: Ncr::WorkOrder::OFFICES[0])
+        office: Ncr::WorkOrder::OFFICES[0])
       fields = wo.fields_for_display
       expect(fields).to include(["Amount", 1000])
       expect(fields).to include(["Expense type", "BA61"])
@@ -19,10 +19,10 @@ describe Ncr::WorkOrder do
     end
     it "shows BA80 fields" do
       wo = Ncr::WorkOrder.new(
-        amount: 1000, description: "Some description", expense_type: "BA80",
-        vendor: "Some Vend", not_to_exceed: false, emergency: true,
+        amount: 1000, expense_type: "BA80", vendor: "Some Vend", 
+        not_to_exceed: false, emergency: true, rwa_number: "RWWAAA #",
         building_number: Ncr::WorkOrder::BUILDING_NUMBERS[0],
-        rwa_number: "RWWAAA #", office: Ncr::WorkOrder::OFFICES[0])
+        office: Ncr::WorkOrder::OFFICES[0])
       fields = wo.fields_for_display
       expect(fields).to include(["Amount", 1000])
       expect(fields).to include(["Expense type", "BA80"])
