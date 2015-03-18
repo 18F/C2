28bdd33905e246a1332c49ebbfac7c52e0f67ff3
CM Lubinski <christopher.lubinski@gsa.gov>
2015-03-18 20:40:25 +0000

    Fixup specs

--- spec/models/ncr/work_order_spec.rb
+++ spec/models/ncr/work_order_spec.rb
@@ -4,35 +4,33 @@ describe Ncr::WorkOrder do
       wo = Ncr::WorkOrder.new(
         amount: 1000, expense_type: "BA61", vendor: "Some Vend", 
         not_to_exceed: false, emergency: true, rwa_number: "RWWAAA #",
-        building_number: Ncr::WorkOrder::BUILDING_NUMBERS[0],
-        office: Ncr::WorkOrder::OFFICES[0])
+        building_number: Ncr::BUILDING_NUMBERS[0],
+        office: Ncr::OFFICES[0])
       fields = wo.fields_for_display
       expect(fields).to include(["Amount", 1000])
       expect(fields).to include(["Expense type", "BA61"])
       expect(fields).to include(["Vendor", "Some Vend"])
       expect(fields).to include(["Not to exceed", false])
       expect(fields).to include(["Emergency", true])
-      expect(fields).to include(["Building number",
-                                 Ncr::WorkOrder::BUILDING_NUMBERS[0]])
+      expect(fields).to include(["Building number", Ncr::BUILDING_NUMBERS[0]])
       expect(fields).not_to include(["RWA Number", "RWWAAA #"])
-      expect(fields).to include(["Office", Ncr::WorkOrder::OFFICES[0]])
+      expect(fields).to include(["Office", Ncr::OFFICES[0]])
     end
     it "shows BA80 fields" do
       wo = Ncr::WorkOrder.new(
         amount: 1000, expense_type: "BA80", vendor: "Some Vend", 
         not_to_exceed: false, emergency: true, rwa_number: "RWWAAA #",
-        building_number: Ncr::WorkOrder::BUILDING_NUMBERS[0],
-        office: Ncr::WorkOrder::OFFICES[0])
+        building_number: Ncr::BUILDING_NUMBERS[0],
+        office: Ncr::OFFICES[0])
       fields = wo.fields_for_display
       expect(fields).to include(["Amount", 1000])
       expect(fields).to include(["Expense type", "BA80"])
       expect(fields).to include(["Vendor", "Some Vend"])
       expect(fields).to include(["Not to exceed", false])
       expect(fields).not_to include(["Emergency", true])
-      expect(fields).to include(["Building number",
-                                 Ncr::WorkOrder::BUILDING_NUMBERS[0]])
+      expect(fields).to include(["Building number", Ncr::BUILDING_NUMBERS[0]])
       expect(fields).to include(["RWA Number", "RWWAAA #"])
-      expect(fields).to include(["Office", Ncr::WorkOrder::OFFICES[0]])
+      expect(fields).to include(["Office", Ncr::OFFICES[0]])
     end
   end
 end
