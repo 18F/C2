4534b6c214e821aa6388b91a419c1ea03b87b9fa
Aidan Feldman <aidan.feldman@gsa.gov>
2015-03-19 00:06:58 -0400

    fix for api token expiration checking

--- spec/lib/parallel_dispatcher_spec.rb
+++ spec/lib/parallel_dispatcher_spec.rb
@@ -18,7 +18,8 @@ describe ParallelDispatcher do
         dispatcher.deliver_new_cart_emails(cart)
 
         cart.approver_approvals.each do |approval|
-          expect(approval.api_token.expires_at).to eq(7.days.from_now)
+          # https://github.com/travisjeffery/timecop/issues/100
+          expect(approval.api_token.expires_at).to eq(7.days.from_now.to_time)
         end
       end
     end
