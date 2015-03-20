1ef366dbcf1db416f00b42385519c137c9209a8c
CM Lubinski <christopher.lubinski@gsa.gov>
2015-03-20 18:56:05 +0000

    DB schema updated, too

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319174617) do
+ActiveRecord::Schema.define(version: 20150319212424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
