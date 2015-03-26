4d9d44dcb5761beb1b3d14d00a3ebcc54a12cd3c
Aidan Feldman <aidan.feldman@gsa.gov>
2015-03-26 12:08:48 -0400

    split up Cart#restart specs

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -140,30 +140,34 @@ describe Cart do
   end
 
   describe '#restart' do
-    # TODO simplify this test
     it 'resets approval states when rejected' do
       cart = FactoryGirl.create(:cart_with_approvals)
-      Dispatcher.deliver_new_cart_emails(cart)
-      expect(cart.api_tokens.length).to eq(2)
 
       cart.approvals.first.approve!
       cart.approvals.last.reject!
       cart.reload
-
-      expect(cart.approvals.approved.size).to eq(1)
-      expect(cart.approvals.rejected.size).to eq(1)
       expect(cart.rejected?).to eq(true)
 
       cart.restart!
 
       expect(cart.pending?).to eq(true)
-      expect(cart.api_tokens.unscoped.expired.length).to eq(2)
-      expect(cart.api_tokens.unexpired.length).to eq(2)
       expect(cart.approvals.length).to eq(2)
       expect(cart.approvals[0].pending?).to eq(true)
       expect(cart.approvals[1].pending?).to eq(true)
     end
+
+    it "creates new API tokens" do
+      cart = FactoryGirl.create(:cart_with_approvals)
+      Dispatcher.deliver_new_cart_emails(cart)
+      expect(cart.api_tokens.length).to eq(2)
+
+      cart.restart!
+
+      expect(cart.api_tokens.unscoped.expired.length).to eq(2)
+      expect(cart.api_tokens.unexpired.length).to eq(2)
+    end
   end
+
   describe '#total_price' do
     context 'the client origin is 18f' do
       it 'gets price from two fields' do
@@ -173,6 +177,7 @@ describe Cart do
         expect(cart.total_price).to eq(18.50*20)
       end
     end
+
     it 'returns 0 otherwise' do
       expect(cart.total_price).to eq(0.0)
     end
