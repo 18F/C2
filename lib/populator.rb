0d6f13486af1ae5a892ac8dd8778942971205d92
Aidan Feldman <aidan.feldman@gsa.gov>
2015-03-26 12:35:52 -0400

    fancier singleton pattern for Populator

--- lib/populator.rb
+++ lib/populator.rb
@@ -1,17 +1,19 @@
 module Populator
-  class << self
-    def create_carts_with_approvals
-      10.times do |i|
-        cart = FactoryGirl.create(:cart)
+  # singleton
+  # https://practicingruby.com/articles/ruby-and-the-singleton-pattern-dont-get-along
+  extend self
 
-        approval1 = cart.add_approver("approver#{i}a@example.com")
-        approval1.create_api_token!
+  def create_carts_with_approvals
+    10.times do |i|
+      cart = FactoryGirl.create(:cart)
 
-        cart.add_approver("approver#{i}b@example.com")
-        cart.add_observer("observer#{i}a@example.com")
-        cart.add_observer("observer#{i}b@example.com")
-        cart.add_requester("requester#{i}@example.com")
-      end
+      approval1 = cart.add_approver("approver#{i}a@example.com")
+      approval1.create_api_token!
+
+      cart.add_approver("approver#{i}b@example.com")
+      cart.add_observer("observer#{i}a@example.com")
+      cart.add_observer("observer#{i}b@example.com")
+      cart.add_requester("requester#{i}@example.com")
     end
   end
 end
