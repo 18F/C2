c22f73a10b7ac6154d122f540d287a516d962aef
Aidan Feldman <aidan.feldman@gsa.gov>
2015-03-28 00:19:19 -0400

    more TODOs

--- app/controllers/api/ncr/work_orders_controller.rb
+++ app/controllers/api/ncr/work_orders_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module Ncr
     class WorkOrdersController < ApplicationController
+      # TODO add CORS support
+
       def index
         render json: ::Ncr::WorkOrder.all, root: false
       end
