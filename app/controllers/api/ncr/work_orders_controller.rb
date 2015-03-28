3e5094eeaf28a10eefa5312d559aa2af0087f60e
Aidan Feldman <aidan.feldman@gsa.gov>
2015-03-28 04:36:05 -0400

    simplify API feature flagging

--- app/controllers/api/ncr/work_orders_controller.rb
+++ app/controllers/api/ncr/work_orders_controller.rb
@@ -1,19 +1,8 @@
 module Api
   module Ncr
     class WorkOrdersController < ApplicationController
-      before_action :block_unless_enabled
-
       def index
         render json: ::Ncr::WorkOrder.all, root: false
-      end
-
-
-      private
-
-      def block_unless_enabled
-        unless ENV['API_ENABLED']
-          raise ActionController::RoutingError.new('Not Found')
-        end
       end
     end
   end
