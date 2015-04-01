551cd3e80397bad8b426f2f687f6d18a13ea4249
CM Lubinski <christopher.lubinski@gsa.gov>
2015-04-01 19:11:21 +0000

    Copy over updated_at/created_atw

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327220945) do
+ActiveRecord::Schema.define(version: 20150401170800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
