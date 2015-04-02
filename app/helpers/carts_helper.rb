4f9669233d145621476f40a1da1f947f17a00bd5
CM Lubinski <christopher.lubinski@gsa.gov>
2015-04-02 17:41:59 +0000

    Add approve_reject policy. Use when displaying buttons

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -14,29 +14,4 @@ module CartsHelper
       cart.status.titlecase
     end
   end
-
-  def display_response_actions?(cart, user)
-    return false unless user.approver_of? cart
-
-    parallel_approval_is_pending?(cart, user) ||
-    current_linear_approval?(cart, user)
-  end
-
-  def approval_by(cart, user)
-    cart.approvals.find_by(approvals: { user_id: user.id })
-  end
-
-  def parallel_approval_is_pending?(cart, user)
-    return false unless cart.parallel?
-    if approval = approval_by(cart, user)
-      approval.pending?
-    else
-      false
-    end
-  end
-
-  def current_linear_approval?(cart, user)
-    approval = approval_by(cart, user)
-    cart.linear? && cart.ordered_awaiting_approvals.first == approval
-  end
 end
