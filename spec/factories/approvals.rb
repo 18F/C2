8ef3e4c462162a2aff03656c4ab4247954434607
Aidan Feldman <aidan.feldman@gsa.gov>
2015-04-19 23:46:47 -0400

    remove unused factory

--- spec/factories/approvals.rb
+++ spec/factories/approvals.rb
@@ -4,10 +4,6 @@ FactoryGirl.define do
     user_id 1
     status 'pending'
 
-    factory :approval_with_user do
-      user
-    end
-
     trait :with_cart do
       association :proposal, :with_cart
     end
