f8f2f5fa13ac6b1039de9343ddb85838eb0b24fc
Aidan Feldman <aidan.feldman@gsa.gov>
2015-04-22 02:35:34 -0400

    ensure that delegates can only be assigned to an approver once

--- app/models/approval_delegate.rb
+++ app/models/approval_delegate.rb
@@ -2,6 +2,6 @@ class ApprovalDelegate < ActiveRecord::Base
   belongs_to :assignee, class_name: 'User', foreign_key: 'assignee_id'
   belongs_to :assigner, class_name: 'User', foreign_key: 'assigner_id'
 
-  validates :assignee_id, presence: true
+  validates :assignee_id, presence: true, uniqueness: {scope: :assigner_id}
   validates :assigner_id, presence: true
 end
