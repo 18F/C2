00f41656433168c13ba6a31b500e67f1ad4b108e
Aidan Feldman <aidan.feldman@gsa.gov>
2015-05-14 17:51:48 -0400

    support searching by WorkOrder#project_title

--- lib/search.rb
+++ lib/search.rb
@@ -4,16 +4,21 @@ module Search
     query = <<-SQL
       SELECT pid
       FROM (
+        -- TODO handle other use case models
+        -- TODO handle associations and their properties in a more automated way
         SELECT
-          proposals.id as pid,
-          setweight(to_tsvector(to_char(proposals.id, '999')), 'A')
-        AS document
+          proposals.id AS pid,
+          setweight(to_tsvector(to_char(proposals.id, '999')), 'A') ||
+          setweight(to_tsvector(coalesce(ncr_work_orders.project_title, '')), 'B') AS document
         FROM proposals
-        GROUP BY proposals.id
+        LEFT OUTER JOIN ncr_work_orders ON
+          ncr_work_orders.id = proposals.client_data_id AND
+          proposals.client_data_type = 'Ncr::WorkOrder'
+        GROUP BY proposals.id, ncr_work_orders.id
       ) p_search
       -- TODO sanitize
-      WHERE p_search.document @@ to_tsquery('#{query}')
-      ORDER BY ts_rank(p_search.document, to_tsquery('#{query}')) DESC
+      WHERE p_search.document @@ plainto_tsquery('english', '#{query}')
+      ORDER BY ts_rank(p_search.document, plainto_tsquery('english', '#{query}')) DESC
     SQL
 
     # feels a bit janky
--- spec/lib/search_spec.rb
+++ spec/lib/search_spec.rb
@@ -16,5 +16,17 @@ describe Search do
       results = Search.find_proposals(proposal.id.to_s)
       expect(results).to eq([proposal])
     end
+
+    it "returns an empty list for no matches" do
+      FactoryGirl.create(:proposal)
+      results = Search.find_proposals('asgsfgsfdbsd')
+      expect(results).to eq([])
+    end
+
+    it "returns the Proposal when searching by the WorkOrder#project_title" do
+      work_order = FactoryGirl.create(:ncr_work_order, :with_proposal)
+      results = Search.find_proposals(work_order.project_title)
+      expect(results).to eq([work_order.proposal])
+    end
   end
 end
