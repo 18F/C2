23aaf096a8d4f593ef9e8da9fa2a11b405278a44
Aidan Feldman <aidan.feldman@gsa.gov>
2015-05-15 18:11:43 -0400

    refactor search sql

--- lib/proposal_search.rb
+++ lib/proposal_search.rb
@@ -12,17 +12,19 @@ class ProposalSearch
     #   http://blog.lostpropertyhq.com/postgres-full-text-search-is-good-enough/#ranking
     #
     # Note that all search computation is done at runtime, so may need to use one or more VIEWs or INDEXes down the road to make it more performant.
-    result_set = <<-SQL
-      SELECT pid
-      FROM (
+
+    # TODO sanitize query
+
+    join = <<-SQL
+      INNER JOIN (
         -- TODO handle other use case models
-        -- TODO handle additional properties
         -- TODO handle associations and their properties in a more automated way
         SELECT
           proposals.id AS pid,
           (
             setweight(to_tsvector(to_char(proposals.id, '999')), 'A') ||
             setweight(to_tsvector(coalesce(ncr_work_orders.project_title, '')), 'B')
+            -- TODO handle additional properties
           ) AS document
         FROM proposals
         LEFT OUTER JOIN ncr_work_orders ON
@@ -30,11 +32,17 @@ class ProposalSearch
           proposals.client_data_type = 'Ncr::WorkOrder'
         GROUP BY proposals.id, ncr_work_orders.id
       ) p_search
-      WHERE p_search.document @@ plainto_tsquery(:query)
-      ORDER BY ts_rank(p_search.document, plainto_tsquery(:query)) DESC
+      ON proposals.id = p_search.pid
     SQL
 
-    # feels a bit janky
-    self.relation.where("id IN (#{result_set})", query: query)
+    filter = <<-SQL
+      p_search.document @@ plainto_tsquery('#{query}')
+    SQL
+
+    ordering = <<-SQL
+      ts_rank(p_search.document, plainto_tsquery('#{query}')) DESC
+    SQL
+
+    self.relation.joins(join).where(filter).order(ordering)
   end
 end
