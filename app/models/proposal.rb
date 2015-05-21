cdb63c7aa659ce60b82e8051b5b300bc59ff99b2
CM Lubinski <christopher.lubinski@gsa.gov>
2015-05-21 18:38:46 +0000

    Use a decorator for approval display status

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -175,7 +175,6 @@ class Proposal < ActiveRecord::Base
 
   def restart
     # Note that none of the state machine's history is stored
-    # Further note: this isn't used anywhere
     self.api_tokens.update_all(expires_at: Time.now)
     self.initialize_approvals()
     Dispatcher.deliver_new_proposal_emails(self)
