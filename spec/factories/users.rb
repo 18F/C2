3fddd0b61e52fddc493ae9e85124b94ddd8966f7
Aidan Feldman <aidan.feldman@gsa.gov>
2015-05-29 02:00:33 -0400

    don't fail to setup test database

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email_address, User.count) {|n| "liono#{n}@some-cartoon-show.com" }
+    # don't freak out if the database isn't created yet
+    # http://stackoverflow.com/a/27745684/358804
+    num_users = if ActiveRecord::Base.connection.table_exists?(User.table_name)
+      User.count
+    else
+      0
+    end
+
+    sequence(:email_address, num_users) {|n| "liono#{n}@some-cartoon-show.com" }
     first_name "Liono"
     last_name "Thunder"
 
