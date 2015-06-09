3280042eb419bc67ee619d869025641e821fde93
CM Lubinski <christopher.lubinski@gsa.gov>
2015-06-09 01:24:47 +0000

    Use self for accessible attributes

--- lib/csv_user_importer.rb
+++ lib/csv_user_importer.rb
@@ -5,24 +5,24 @@ class CsvUserImporter
     @csv = CSV.read(csv_path, headers: true)
     @client = client
     headers = @csv.headers()
-    @first_name_col = headers.find {|h| h.downcase.include? "first"}
-    @last_name_col = headers.find {|h| h.downcase.include? "last"}
-    @email_col = headers.find {|h| h.downcase.include? "email"}
+    self.first_name_col = headers.find {|h| h.downcase.include? "first"}
+    self.last_name_col = headers.find {|h| h.downcase.include? "last"}
+    self.email_col = headers.find {|h| h.downcase.include? "email"}
   end
 
   def process_rows
-    if !@first_name_col || !@last_name_col || !@email_col
+    if !self.first_name_col || !self.last_name_col || !self.email_col
       raise "Couldn't determine one or more of first name, last name, email: #{@csv.headers()}"
     end
 
     @csv.each do |row|
-      email = row[@email_col]
+      email = row[self.email_col]
       if email.blank?
         warn "Email is empty: " + row.inspect
       else
         User.for_email(email).update(   # for_email will standardize
-          first_name: row[@first_name_col].try(:titleize),
-          last_name: row[@last_name_col].try(:titleize),
+          first_name: row[self.first_name_col].try(:titleize),
+          last_name: row[self.last_name_col].try(:titleize),
           client_slug: @client
         )
       end
