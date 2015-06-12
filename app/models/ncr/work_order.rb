055f1933bc2f0bffc4aa6304288ca78d6dc5e90e
Nick Bristow <Nick.Bristow@gsa.gov>
2015-06-12 13:36:34 -0400

    formated error messages for amount

--- app/models/ncr/work_order.rb
+++ app/models/ncr/work_order.rb
@@ -26,8 +26,12 @@ module Ncr
 
     # @TODO: use integer number of cents to avoid floating point issues
     validates :amount, numericality: {
+      less_than_or_equal_to: 3000,
+      message: "Amount must be less than or equal to $3,000"
+    }
+    validates :amount, numericality: {
       greater_than_or_equal_to: 0,
-      less_than_or_equal_to: 3000
+      message: "Amount must be greater than or equal to $0"
     }
     validates :expense_type, inclusion: {in: EXPENSE_TYPES}, presence: true
     validates :vendor, presence: true
