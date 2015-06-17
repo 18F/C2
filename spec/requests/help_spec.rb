3c36fc280a8c7ad4b51ad06d880588ece4249799
Aidan Feldman <aidan.feldman@gsa.gov>
2015-06-17 18:07:10 -0400

    validate that markdown was rendered properly on help page

--- spec/requests/help_spec.rb
+++ spec/requests/help_spec.rb
@@ -1,8 +1,15 @@
 describe 'GET /help' do
   it "displays successfully" do
     get '/help'
+
     expect(response.status).to eq(200)
+
     doc = Capybara.string(body)
     expect(doc).to have_content('FAQ')
+
+    inset = doc.find('.markdown')
+    link = inset.find_link('the homepage')
+    expect(link).to_not be_nil
+    expect(link[:href]).to eq('/')
   end
 end
