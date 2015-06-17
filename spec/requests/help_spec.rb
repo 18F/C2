6c5e7cd9839b1a0f1ded385e38513520e3a69dd8
Aidan Feldman <aidan.feldman@gsa.gov>
2015-06-17 17:57:35 -0400

    use capybara to validate help page

--- spec/requests/help_spec.rb
+++ spec/requests/help_spec.rb
@@ -2,6 +2,7 @@ describe 'GET /help' do
   it "displays successfully" do
     get '/help'
     expect(response.status).to eq(200)
-    expect(response.body).to include('FAQ')
+    doc = Capybara.string(body)
+    expect(doc).to have_content('FAQ')
   end
 end
