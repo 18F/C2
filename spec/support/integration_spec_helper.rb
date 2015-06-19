57712c724afa8c3575afa71741a2c0865bc3d99e
Raphy Villas <raphael.villas@gmail.com>
2015-06-19 18:16:29 -0400

    [#94312822] Set procurement environment variables back to initial values in specs

--- spec/support/integration_spec_helper.rb
+++ spec/support/integration_spec_helper.rb
@@ -21,10 +21,13 @@ module IntegrationSpecHelper
   end
 
   def with_18f_procurement_env_variables(setup_vars=nil)
+    old_approver_email = ENV['GSA18F_APPROVER_EMAIL']
+    old_purchaser_email = ENV['GSA18F_PURCHASER_EMAIL']
+
     ENV['GSA18F_APPROVER_EMAIL'] = 'test_approver@some-dot-gov.gov'
     ENV['GSA18F_PURCHASER_EMAIL'] = 'test_purchaser@some-dot-gov.gov'
     yield
-    ENV['GSA18F_APPROVER_EMAIL'] = nil
-    ENV['GSA18F_PURCHASER_EMAIL'] = nil
+    ENV['GSA18F_APPROVER_EMAIL'] = old_approver_email
+    ENV['GSA18F_PURCHASER_EMAIL'] = old_purchaser_email
   end
 end
