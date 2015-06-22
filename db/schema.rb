28a2804fb0ed5a6b4564a7a99a8bc586d4de2caf
Aidan Feldman <aidan.feldman@gsa.gov>
2015-06-22 14:37:33 -0400

    use #deliver_now for feedback mailer

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20150618193310) do
   enable_extension "plpgsql"
 
   create_table "api_tokens", force: :cascade do |t|
-    t.string   "access_token", limit: 255
+    t.string   "access_token"
     t.datetime "expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -33,11 +33,11 @@ ActiveRecord::Schema.define(version: 20150618193310) do
   end
 
   create_table "approval_groups", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "cart_id"
-    t.string   "flow",       limit: 255
+    t.string   "flow"
   end
 
   create_table "approval_groups_users", id: false, force: :cascade do |t|
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20150618193310) do
 
   create_table "approvals", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "status",      limit: 255
+    t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "position"
@@ -55,8 +55,8 @@ ActiveRecord::Schema.define(version: 20150618193310) do
   end
 
   create_table "attachments", force: :cascade do |t|
-    t.string   "file_file_name",    limit: 255
-    t.string   "file_content_type", limit: 255
+    t.string   "file_file_name"
+    t.string   "file_content_type"
     t.integer  "file_file_size"
     t.datetime "file_updated_at"
     t.integer  "proposal_id"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 20150618193310) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "external_id"
@@ -85,33 +85,33 @@ ActiveRecord::Schema.define(version: 20150618193310) do
   add_index "comments", ["proposal_id"], name: "index_comments_on_proposal_id", using: :btree
 
   create_table "gsa18f_procurements", force: :cascade do |t|
-    t.string   "office",                       limit: 255
+    t.string   "office"
     t.text     "justification"
-    t.string   "link_to_product",              limit: 255
+    t.string   "link_to_product"
     t.integer  "quantity"
     t.datetime "date_requested"
-    t.string   "additional_info",              limit: 255
+    t.string   "additional_info"
     t.decimal  "cost_per_unit"
     t.text     "product_name_and_description"
     t.boolean  "recurring"
-    t.string   "recurring_interval",           limit: 255
+    t.string   "recurring_interval"
     t.integer  "recurring_length"
-    t.string   "urgency",                      limit: 255
+    t.string   "urgency"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "ncr_work_orders", force: :cascade do |t|
     t.decimal  "amount"
-    t.string   "expense_type",    limit: 255
-    t.string   "vendor",          limit: 255
+    t.string   "expense_type"
+    t.string   "vendor"
     t.boolean  "not_to_exceed"
-    t.string   "building_number", limit: 255
+    t.string   "building_number"
     t.boolean  "emergency"
-    t.string   "rwa_number",      limit: 255
-    t.string   "org_code",        limit: 255
-    t.string   "code",            limit: 255
-    t.string   "project_title",   limit: 255
+    t.string   "rwa_number"
+    t.string   "org_code"
+    t.string   "code"
+    t.string   "project_title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -129,36 +129,36 @@ ActiveRecord::Schema.define(version: 20150618193310) do
     t.text    "property"
     t.text    "value"
     t.integer "hasproperties_id"
-    t.string  "hasproperties_type", limit: 255
+    t.string  "hasproperties_type"
   end
 
   create_table "proposals", force: :cascade do |t|
-    t.string   "status",           limit: 255
-    t.string   "flow",             limit: 255
+    t.string   "status"
+    t.string   "flow"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "client_data_id"
-    t.string   "client_data_type", limit: 255
+    t.string   "client_data_type"
     t.integer  "requester_id"
     t.string   "public_id"
   end
 
-  add_index "proposals", ["client_data_id", "client_data_type"], name: "index_proposals_on_client_data_id_and_client_data_type", using: :btree
+  add_index "proposals", ["client_data_type", "client_data_id"], name: "index_proposals_on_client_data_type_and_client_data_id", using: :btree
 
   create_table "user_roles", force: :cascade do |t|
     t.integer "approval_group_id"
     t.integer "user_id"
-    t.string  "role",              limit: 255
+    t.string  "role"
     t.integer "position"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email_address", limit: 255
-    t.string   "first_name",    limit: 255
-    t.string   "last_name",     limit: 255
+    t.string   "email_address"
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "client_slug",   limit: 255
+    t.string   "client_slug"
   end
 
 end
