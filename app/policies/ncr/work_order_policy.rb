a6433391b1837ef4985dbba16b59adc5562c5b42
Aidan Feldman <aidan.feldman@gsa.gov>
2015-06-26 01:36:23 -0400

    refactor GSA restriction checking

--- app/policies/ncr/work_order_policy.rb
+++ app/policies/ncr/work_order_policy.rb
@@ -5,8 +5,20 @@ module Ncr
       @work_order = record
     end
 
+    def restricted?
+      ENV['RESTRICT_ACCESS'] == 'true'
+    end
+
+    def gsa_email?
+      @user.email_address.end_with?('@gsa.gov')
+    end
+
     def gsa!
-      check(@user.email_address.end_with?('@gsa.gov'), "You must be logged in with a GSA email address to create")
+      check(self.gsa_email?, "You must be logged in with a GSA email address to create")
+    end
+
+    def gsa_if_restricted!
+      !self.restricted? || self.gsa!
     end
 
     def can_edit!
@@ -15,7 +27,7 @@ module Ncr
     alias_method :can_update!, :can_edit!
 
     def can_create!
-      super && (ENV['RESTRICT_ACCESS'] != 'true' || self.gsa!)
+      super && self.gsa_if_restricted!
     end
     alias_method :can_new!, :can_create!
   end
