5cc3e0be7e672cd0be773433199ab6739a225760
CM Lubinski <christopher.lubinski@gsa.gov>
2015-06-28 23:35:35 +0000

    Remove observers from comments

--- config/initializers/observers.rb
+++ config/initializers/observers.rb
@@ -1,6 +1,3 @@
-ActiveSupport::Notifications.subscribe('Comment.create') {|_, _, _, _, comment|
-  Dispatcher.on_comment_created(comment)
-}
 ActiveSupport::Notifications.subscribe('Ncr::WorkOrder.update') {|_, _, _, _, work_order|
   Dispatcher.on_proposal_update(work_order.proposal)
 }
