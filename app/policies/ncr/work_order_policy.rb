f17dece26449ae55fb8f5a127ec42725f6773917
Raphy Villas <raphael.villas@gmail.com>
2015-07-05 19:59:41 -0400

    Pull #cancelled check into #can_edit?

--- app/policies/ncr/work_order_policy.rb
+++ app/policies/ncr/work_order_policy.rb
@@ -7,9 +7,14 @@ module Ncr
       @work_order = record
     end
 
+    def not_cancelled!
+      check(!@work_order.cancelled?, "The proposal is cancelled.")
+    end
+
     def can_edit!
       check(self.requester? || self.approver? || self.observer?, "You must be the requester, approver, or observer to edit")
     end
+
     alias_method :can_update!, :can_edit!
 
     def can_create!
