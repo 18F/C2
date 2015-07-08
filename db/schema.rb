ef0e14caeec07ae21bb5308ebed4b032268ca53f
CM Lubinski <christopher.lubinski@gsa.gov>
2015-07-08 21:37:15 +0000

    Data migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706144642) do
+ActiveRecord::Schema.define(version: 20150707214004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
