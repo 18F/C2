5b85ab4db7da5a1a1e526ac6a1364afaa00ff6e5
Annalee Flower Horne <leeflowerh@gmail.com>
2015-07-20 16:50:25 -0400

    moving sandbox warning banner to a partial with its own stylesheet.

--- spec/features/sandbox_warning_spec.rb
+++ spec/features/sandbox_warning_spec.rb
@@ -11,20 +11,16 @@ describe "display sandbox warning" do
   end
 
   context 'hides sandbox warning in production' do
-    before do
-      ENV["DISABLE_SANDBOX_WARNING"] = "true"
-    end
-    after do
-      ENV["DISABLE_SANDBOX_WARNING"] = nil
-    end
-    it "on homepage" do
-      visit root_path
-      expect(page).not_to have_content("This sandbox site is for testing and training purposes only")
-    end
+    with_feature 'DISABLE_SANDBOX_WARNING' do
+      it "on homepage" do
+        visit root_path
+        expect(page).not_to have_content("This sandbox site is for testing and training purposes only")
+      end
 
-    it "on internal pages" do
-      visit proposals_path
-      expect(page).not_to have_content("This sandbox site is for testing and training purposes only")
+      it "on internal pages" do
+        visit proposals_path
+        expect(page).not_to have_content("This sandbox site is for testing and training purposes only")
+      end
     end
   end
 end
