91dff2d73eab0f9e50e0bccce748a32fc98156a3
CM Lubinski <christopher.lubinski@gsa.gov>
2015-07-21 21:27:54 +0000

    Tests for approved_at. Make approval factory more robust

--- spec/models/approval_spec.rb
+++ spec/models/approval_spec.rb
@@ -20,4 +20,21 @@ describe Approval do
       expect(approval.api_token).to eq(nil)
     end
   end
+
+  describe '#approved_at' do
+    it 'is nil when pending' do
+      expect(approval.approved_at).to be_nil
+    end
+
+    it 'is nil when actionable' do
+      approval.make_actionable!
+      expect(approval.approved_at).to be_nil
+    end
+
+    it 'is set when approved' do
+      approval.make_actionable!
+      approval.approve!
+      expect(approval.approved_at).not_to be_nil
+    end
+  end
 end
