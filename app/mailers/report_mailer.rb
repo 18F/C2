e102358901a149c08ef7c439fa291243fac0759f
Raphy Villas <raphael.villas@gmail.com>
2015-07-23 23:06:58 -0400

    Adding a bit more info

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -7,15 +7,18 @@ class ReportMailer < ActionMailer::Base
                                 .where("created_at > ?",1.week.ago).count
     @total_unapproved = Proposal.pending.where(client_data_type: "Ncr::WorkOrder").count
 
-    @ba60_proposals = Proposal.where(status:'approved').where("created_at > ?",1.week.ago).where(client_data_type: "Ncr::WorkOrder").map(&:client_data).select{|d| d.expense_type == "BA60"};nil
-    @ba61_proposals = Proposal.where(status:'approved').where("created_at > ?",1.week.ago).where(client_data_type: "Ncr::WorkOrder").map(&:client_data).select{|d| d.expense_type == "BA61"}
-    @ba80_proposals = Proposal.where(status:'approved').where("created_at > ?",1.week.ago).where(client_data_type: "Ncr::WorkOrder").map(&:client_data).select{|d| d.expense_type == "BA80"};nil
+    @ba60_proposals = budget_proposals("BA60", 1.week.ago)
+    @ba61_proposals = budget_proposals("BA61", 1.week.ago)
+    @ba80_proposals = budget_proposals("BA80", 1.week.ago)
 
-    @proposals_pending_approving_official = Proposal.where(client_data_type: "Ncr::WorkOrder").where(status: "pending").select{|p| p.approvals.pluck(:status)[0] == 'actionable'};nil
-    @proposals_pending_budget = Proposal.where(client_data_type: "Ncr::WorkOrder").where(status: "pending").select{|p| p.approvals.pluck(:status).last == 'actionable'}.sort{|a,b|a.client_data.expense_type <=> b.client_data.expense_type};nil
+    @proposals_pending_approving_official = Proposal.where(client_data_type: "Ncr::WorkOrder")
+                                                    .where(status: "pending")
+                                                    .select{ |p| p.approvals.pluck(:status)[0] == 'actionable' }
 
-    # headers['In-Reply-To'] = @proposal.email_msg_id
-    # headers['References'] = @proposal.email_msg_id
+    @proposals_pending_budget = Proposal.where(client_data_type: "Ncr::WorkOrder")
+                                        .where(status: "pending")
+                                        .select{ |p| p.approvals.pluck(:status).last == 'actionable' }
+                                        .sort{ |a,b|a.client_data.expense_type <=> b.client_data.expense_type }
 
     mail(
       to: to_email,
@@ -24,6 +27,14 @@ class ReportMailer < ActionMailer::Base
       template_name: 'budget_report_email'
     )
 
+  end
+
+private
+
+  def budget_proposals(type, timespan)
+    Proposal.approved.where(client_data_type: "Ncr::WorkOrder")
+                     .where("created_at > ?", timespan)
+                     .map(&:client_data).select{|d| d.expense_type == type}
   end
 
 end
