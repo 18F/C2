93429973aaabe1908ad066a241263315ced6c880
Nick Bristow <Nick.Bristow@gsa.gov>
2015-07-31 16:10:03 -0400

    Updated dispatcher to notify observers for work order changes

--- spec/views/communicart_mailer/_email_reply.html.erb_spec.rb
+++ spec/views/communicart_mailer/_email_reply.html.erb_spec.rb
@@ -2,7 +2,8 @@ describe "communicart_mailer/_email_reply.html.erb" do
   it "renders 'Send a Comment' link with approve button" do
     approval = FactoryGirl.create(:approval)
     approval.create_api_token!
-    render partial: "communicart_mailer/email_reply", locals: {show_approval_actions: true, approval: approval}
+    proposal = approval.proposal
+    render partial: "communicart_mailer/email_reply", locals: {show_approval_actions: true, approval: approval, proposal: proposal}
 
     expect(rendered).to include "Approve"
     expect(rendered).to include "Or Send a Comment"
@@ -11,7 +12,8 @@ describe "communicart_mailer/_email_reply.html.erb" do
   it "renders 'View This Request' link without approve button" do
     approval = FactoryGirl.create(:approval)
     approval.create_api_token!
-    render partial: "communicart_mailer/email_reply", locals: {show_approval_actions: false, approval: approval}
+    proposal = approval.proposal
+    render partial: "communicart_mailer/email_reply", locals: {show_approval_actions: false, approval: approval, proposal: proposal}
 
     expect(rendered).to_not include "Approve"
     expect(rendered).to_not include "Or Send a Comment"
