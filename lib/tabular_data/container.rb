4da502db6c114bb1de3d38cfd1acb225cd4f6b0d
CM Lubinski <christopher.lubinski@gsa.gov>
2015-07-31 17:53:18 +0000

    Link the headers for sorting

--- lib/tabular_data/container.rb
+++ lib/tabular_data/container.rb
@@ -23,6 +23,33 @@ module TabularData
       @query.order(@sort)
     end
 
+    def set_state_from_params(params)
+      config = self.param_config(params)
+      if config.has_key? :sort
+        self.set_sort(config[:sort])
+      end
+      self
+    end
+
+    def query_params_for_sort(params, col)
+      config = self.param_config(params)
+      if config[:sort] == col.name   # flip to descending
+        params.deep_merge(tables: {@name => {sort: '-' + col.name}})
+      else
+        params.deep_merge(tables: {@name => {sort: col.name}})
+      end
+    end
+
+    def self.config_for_client(container_name, client_name)
+      container_yaml = YAML.load_file("#{Rails.root}/config/tables/#{container_name}.yml")
+      if container_yaml.has_key?(client_name)
+        container_yaml[client_name].deep_symbolize_keys
+      else
+        container_yaml["default"].deep_symbolize_keys
+      end
+    end
+
+    protected
     def set_sort(field)
       @sort = nil
       dir = field.start_with?('-') ? :desc : :asc
@@ -35,22 +62,9 @@ module TabularData
       end
     end
 
-    def set_state_from_params(params)
-      params = params.permit(tables: {@name => [:sort]})
-      config = params.fetch(:tables, {}).fetch(@name, {})
-      if config.has_key? :sort
-        self.set_sort(config[:sort])
-      end
-      self
-    end
-
-    def self.config_for_client(container_name, client_name)
-      container_yaml = YAML.load_file("#{Rails.root}/config/tables/#{container_name}.yml")
-      if container_yaml.has_key?(client_name)
-        container_yaml[client_name].deep_symbolize_keys
-      else
-        container_yaml["default"].deep_symbolize_keys
-      end
+    def param_config(params)
+      relevant = ActionController::Parameters.new(params).permit(tables: {@name => [:sort]})
+      relevant.fetch(:tables, {}).fetch(@name, {})
     end
   end
 end
