73d918d330c8f2f3655de1bea66e11c95e3a79f3
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-07 00:10:04 -0600

    minimal refactoring of ReportMailer to a class

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,43 +1,32 @@
 class ReportMailer < ActionMailer::Base
-
   add_template_helper ReportHelper
 
   def budget_status
     to_email = ENV.fetch('BUDGET_REPORT_RECIPIENT')
 
-    # TODO move all of this logic to a class
+    @total_last_week = Ncr::Reporter.total_last_week
+    @total_unapproved = Ncr::Reporter.total_unapproved
 
-    @total_last_week  = Proposal.where(client_data_type: "Ncr::WorkOrder")
-                                .where("created_at > ?",1.week.ago).count
-    @total_unapproved = Proposal.pending.where(client_data_type: "Ncr::WorkOrder").count
+    @ba60_proposals = Ncr::Reporter.ba60_proposals
+    @ba61_proposals = Ncr::Reporter.ba61_proposals
+    @ba80_proposals = Ncr::Reporter.ba80_proposals
 
-    @ba60_proposals = budget_proposals("BA60", 1.week.ago)
-    @ba61_proposals = budget_proposals("BA61", 1.week.ago)
-    @ba80_proposals = budget_proposals("BA80", 1.week.ago)
+    @proposals_pending_approving_official = Ncr::Reporter.proposals_pending_approving_official
+    @proposals_pending_budget = Ncr::Reporter.proposals_pending_budget
 
-    @proposals_pending_approving_official = Proposal.where(client_data_type: "Ncr::WorkOrder")
-                                                    .where(status: "pending")
-                                                    .select{ |p| p.approvals.pluck(:status)[0] == 'actionable' }
-
-    @proposals_pending_budget = Proposal.where(client_data_type: "Ncr::WorkOrder")
-                                        .where(status: "pending")
-                                        .select{ |p| p.approvals.pluck(:status).last == 'actionable' }
-                                        .sort{ |a,b|a.client_data.expense_type <=> b.client_data.expense_type }
+    date = Time.now.in_time_zone('Eastern Time (US & Canada)').strftime("%a %m/%d/%y")
 
     mail(
       to: to_email,
-      subject: "C2: Daily Budget report for #{Time.now.in_time_zone('Eastern Time (US & Canada)').strftime("%a %m/%d/%y")}",
-      from: 'communicart.sender@gsa.gov',
-      template_name: 'budget_report_email'
+      subject: "C2: Daily Budget report for #{date}",
+      from: sender_email
     )
   end
 
-private
 
-  def budget_proposals(type, timespan)
-    Proposal.approved.where(client_data_type: "Ncr::WorkOrder")
-                     .where("created_at > ?", timespan)
-                     .map(&:client_data).select{|d| d.expense_type == type}
+  private
+
+  def sender_email
+    ENV['NOTIFICATION_FROM_EMAIL'] || 'noreply@some.gov'
   end
-
 end
