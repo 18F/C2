769ab6f4aae30d3b0438d193d958f4528a0e1e94
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-07 00:56:01 -0600

    set up environment variable for ReportMailer in specs

--- spec/mailers/report_mailer_spec.rb
+++ spec/mailers/report_mailer_spec.rb
@@ -1,9 +1,11 @@
 describe ReportMailer do
   describe '.budget_status' do
-    it "works with no data" do
-      expect {
-        ReportMailer.budget_status.deliver_now
-      }.to_not raise_error
+    with_env_var('BUDGET_REPORT_RECIPIENT', 'budget@gsa.gov') do
+      it "works with no data" do
+        expect {
+          ReportMailer.budget_status.deliver_now
+        }.to_not raise_error
+      end
     end
   end
 end
