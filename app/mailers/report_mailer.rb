c00c7c799a3746d326fad0d321651cda58b8686b
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-07 00:11:46 -0600

    move report variable setting out of the mailer

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,17 +3,6 @@ class ReportMailer < ActionMailer::Base
 
   def budget_status
     to_email = ENV.fetch('BUDGET_REPORT_RECIPIENT')
-
-    @total_last_week = Ncr::Reporter.total_last_week
-    @total_unapproved = Ncr::Reporter.total_unapproved
-
-    @ba60_proposals = Ncr::Reporter.ba60_proposals
-    @ba61_proposals = Ncr::Reporter.ba61_proposals
-    @ba80_proposals = Ncr::Reporter.ba80_proposals
-
-    @proposals_pending_approving_official = Ncr::Reporter.proposals_pending_approving_official
-    @proposals_pending_budget = Ncr::Reporter.proposals_pending_budget
-
     date = Time.now.in_time_zone('Eastern Time (US & Canada)').strftime("%a %m/%d/%y")
 
     mail(
