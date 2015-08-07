72a0abb0239d95ae4e0b2dc042925743d383a41a
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-06 18:28:38 -0600

    have all environments run clockwork
    want them to be as uniform as possible, even if the non-production
    environments dont actually send the mailer

--- config/clock.rb
+++ config/clock.rb
@@ -4,15 +4,13 @@ ENV['NEW_RELIC_DISPATCHER'] ||= 'clockwork'
 
 require 'newrelic_rpm'
 require 'clockwork'
-require_relative '../lib/server_env'
+require_relative 'boot'
+require_relative 'environment'
 
 puts "Clockwork loaded."
 
-if ServerEnv.instance_index == 0
-  require_relative 'boot'
-  require_relative 'environment'
-
-  module Clockwork
+module Clockwork
+  if ENV['BUDGET_REPORT_RECIPIENT'] && ServerEnv.instance_index == 0
     # TODO feature-flag this (more important when we have multiple tasks that we want run in all environments)
     every(1.week, 'report_mailer.budget', at: 'Monday 03:30', thread: true) do
       puts "SENDING TEST REPORT..."
