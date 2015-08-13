8ff37eb232f2b2c0dd3c5a43c8020a21726fb3db
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-13 17:47:00 -0400

    split up Query::Proposals.approvals_for

--- lib/query/proposals.rb
+++ lib/query/proposals.rb
@@ -10,6 +10,12 @@ module Query
       proposals[:requester_id].eq(user.id)
     end
 
+    def self.approvals_with_delegates
+      approvals.project(Arel.star).
+        join(delegates, Arel::Nodes::OuterJoin).
+        on(delegates[:assigner_id].eq(approvals[:user_id]))
+    end
+
     def self.with_approver_or_delegate(user)
       Arel::Nodes::Exists.new(
         self.approvals_for(user)
@@ -48,16 +54,30 @@ module Query
       Proposal.arel_table
     end
 
+    def self.with_matching_proposal
+      approvals[:proposal_id].eq(proposals[:id])
+    end
+
+    def self.non_pending
+      approvals[:status].not_eq('pending')
+    end
+
+    def self.where_approver(user)
+      approvals[:user_id].eq(user.id)
+    end
+
+    def self.where_delegate(user)
+      delegates[:assignee_id].eq(user.id)
+    end
+
     ## subselects to be used alongside the proposals table ##
     # Subselects are used instead of left joins to avoid an explicit duplication-removal step.
 
     def self.approvals_for(user)
-      approvals.project(Arel.star).join(delegates, Arel::Nodes::OuterJoin).on(
-        delegates[:assigner_id].eq(approvals[:user_id])
-      ).where(
-        approvals[:proposal_id].eq(proposals[:id]).and(
-          approvals[:status].not_eq('pending').and(
-            approvals[:user_id].eq(user.id).or(delegates[:assignee_id].eq(user.id))
+      self.approvals_with_delegates.where(
+        self.with_matching_proposal.and(
+          self.non_pending.and(
+            self.where_approver(user).or(self.where_delegate(user))
           )
         )
       ).ast
