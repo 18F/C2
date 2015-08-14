f417340788881b1560dedf25190e92f6239e8093
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-14 19:34:33 -0400

    fix Organization data loading

--- lib/ncr/organization.rb
+++ lib/ncr/organization.rb
@@ -5,9 +5,9 @@ module Ncr
 
     attr_accessor :code, :name
 
-    def initialize(row)
-      self.code = row['organization_cd']
-      self.name = row['organization_nm']
+    def initialize(code:, name:)
+      self.code = code
+      self.name = name
     end
 
     def ==(other)
@@ -31,12 +31,19 @@ module Ncr
       BY_CODE[code]
     end
 
+    def self.from_csv_row(row)
+      self.new(
+        code: row['organization_cd'],
+        name: row['organization_nm']
+      )
+    end
+
     protected
 
     def self.load_csv
       rows = CSV.read("#{Rails.root}/config/data/ncr/org_codes_2015-05-18.csv", headers: true)
       rows.each do |row|
-        org = Ncr::Organization.new(row)
+        org = Ncr::Organization.from_csv_row(row)
         Ncr::Organization::BY_CODE[org.code] = org
       end
     end
@@ -44,7 +51,8 @@ module Ncr
     def self.load_yaml
       data = YAML.load_file(Rails.root.join(*%w(config data ncr ool_org_codes.yml)))
       data.each do |code, name|
-        Ncr::Organization::BY_CODE[code] = name
+        org = Ncr::Organization.new(code: code, name: name)
+        Ncr::Organization::BY_CODE[code] = org
       end
     end
 
