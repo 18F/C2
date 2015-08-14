eca5ae08f245cee6ee8f754ac609e313365069c3
CM Lubinski <christopher.lubinski@gsa.gov>
2015-08-14 21:56:25 +0000

    Check for full name, not just email

--- spec/features/observer_spec.rb
+++ spec/features/observer_spec.rb
@@ -1,35 +1,19 @@
 describe "observers" do
   it "allows observers to be added" do
     work_order = FactoryGirl.create(:ncr_work_order)
+    observer = FactoryGirl.create(:user)
     proposal = work_order.proposal
     login_as(proposal.requester)
 
-    visit "/proposals/#{proposal.id}/observations"
-    fill_in 'Email address', with: 'observer@some-dot-gov.gov'
-    click_on 'Add'
+    visit "/proposals/#{proposal.id}"
+    select observer.email_address, from: 'observation_user_email_address'
+    click_on 'Add a Subscriber'
 
-    expect(page).to have_content("observer@some-dot-gov.gov has been added as an observer")
+    expect(page).to have_content("#{observer.full_name} has been added as an observer")
 
     proposal.reload
-    expect(proposal.observers.map(&:email_address)).to eq(['observer@some-dot-gov.gov'])
+    expect(proposal.observers).to eq [observer]
 
-    expect(email_recipients).to eq(['observer@some-dot-gov.gov'])
-  end
-
-  it "doesn't allow observers to be added if not signed in" do
-    visit '/proposals/1/observations'
-    expect(current_path).to eq('/')
-    expect(page).to have_content("You need to sign in")
-  end
-
-  it "doesn't allow observers to be added if not involved in the Proposal" do
-    proposal = FactoryGirl.create(:proposal)
-    user = FactoryGirl.create(:user)
-    login_as(user)
-
-    visit "/proposals/#{proposal.id}/observations"
-
-    expect(current_path).to eq('/proposals')
-    expect(page).to have_content("not allowed")
+    expect(email_recipients).to eq([observer.email_address])
   end
 end
