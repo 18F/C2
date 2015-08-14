b16fc219b797470a395ab1394526ab2731ac0d6c
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-14 19:24:50 -0400

    move Organization data loading code into methods

--- lib/ncr/organization.rb
+++ lib/ncr/organization.rb
@@ -30,16 +30,25 @@ module Ncr
     def self.find(code)
       BY_CODE[code]
     end
+
+    protected
+
+    def self.load_csv
+      rows = CSV.read("#{Rails.root}/config/data/ncr/org_codes_2015-05-18.csv", headers: true)
+      rows.each do |row|
+        org = Ncr::Organization.new(row)
+        Ncr::Organization::BY_CODE[org.code] = org
+      end
+    end
+
+    def self.load_yaml
+      data = YAML.load_file(Rails.root.join(*%w(config data ncr ool_org_codes.yml)))
+      data.each do |code, name|
+        Ncr::Organization::BY_CODE[code] = name
+      end
+    end
+
+    self.load_csv
+    self.load_yaml
   end
-end
-
-csv_rows = CSV.read("#{Rails.root}/config/data/ncr/org_codes_2015-05-18.csv", headers: true)
-csv_rows.each do |row|
-  org = Ncr::Organization.new(row)
-  Ncr::Organization::BY_CODE[org.code] = org
-end
-
-yaml_data = YAML.load_file(Rails.root.join(*%w(config data ncr ool_org_codes.yml)))
-yaml_data.each do |code, name|
-  Ncr::Organization::BY_CODE[code] = name
 end
