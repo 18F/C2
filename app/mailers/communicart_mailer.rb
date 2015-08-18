1d408d762280c103fd201c95a35d980e21ce9ff5
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-17 21:39:43 -0400

    refactor the mailer subject mailing to multiple methods

--- app/mailers/communicart_mailer.rb
+++ app/mailers/communicart_mailer.rb
@@ -143,24 +143,36 @@ class CommunicartMailer < ActionMailer::Base
     )
   end
 
-  def proposal_subject(proposal)
+  def proposal_subject_i18n_key(proposal)
+    if proposal.client_data
+      # We'll look up by the client_data's class name
+      proposal.client_data.class.name.underscore
+    else
+      # Default (no client_data): look up by "proposal"
+      :proposal
+    end
+  end
+
+  def proposal_subject_params(proposal)
     params = proposal.as_json
     # todo: replace with public_id once #98376564 is fixed
     params[:public_identifier] = proposal.public_identifier
     # Add in requester params
     proposal.requester.as_json.each { |k, v| params["requester_" + k] = v }
     if proposal.client_data
-      # We'll look up by the client_data's class name
-      i18n_key = proposal.client_data.class.name.underscore
       # Add in client_data params
       params.merge!(proposal.client_data.as_json)
-    else
-      # Default (no client_data): look up by "proposal"
-      i18n_key = :proposal
     end
     # Add search path, and default lookup key for I18n
     params.merge!(scope: [:mail, :subject], default: :proposal)
-    I18n.t i18n_key, params.symbolize_keys
+
+    params
+  end
+
+  def proposal_subject(proposal)
+    i18n_key = proposal_subject_i18n_key(proposal)
+    params = proposal_subject_params(proposal)
+    I18n.t(i18n_key, params.symbolize_keys)
   end
 
   def observation_added_from(observation)
