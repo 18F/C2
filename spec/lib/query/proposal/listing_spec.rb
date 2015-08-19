b259e64f092315b86f89ff0463a7d45c5caa4394
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-19 13:02:00 -0400

    test all relevant index methods in the Listing spec

--- spec/lib/query/proposal/listing_spec.rb
+++ spec/lib/query/proposal/listing_spec.rb
@@ -2,29 +2,33 @@ describe Query::Proposal::Listing do
   let(:params) { ActionController::Parameters.new }
   let(:user) { FactoryGirl.create(:user) }
 
-  describe '#pending' do
-    it "only returns that user's Proposals when they are an app admin" do
-      FactoryGirl.create(:proposal)
-      proposal = FactoryGirl.create(:proposal, requester: user)
+  [:pending, :approved, :cancelled].each do |status|
+    describe "##{status}" do
+      it "only returns that user's Proposals when they are an app admin" do
+        FactoryGirl.create(:proposal, status: status)
+        proposal = FactoryGirl.create(:proposal, requester: user, status: status)
 
-      with_env_var('ADMIN_EMAILS', user.email_address) do
-        listing = Query::Proposal::Listing.new(user, params)
-        expect(listing.pending.rows).to eq([proposal])
-      end
-    end
-
-    context "with an arbitrary client" do
-      before do
-        user.update_attribute(:client_slug, 'ncr')
-      end
-
-      it "only displays that user's Proposals when they are a client admin" do
-        proposal = FactoryGirl.create(:proposal, requester: user)
-        FactoryGirl.create(:ncr_work_order)
-
-        with_env_var('CLIENT_ADMIN_EMAILS', user.email_address) do
+        with_env_var('ADMIN_EMAILS', user.email_address) do
           listing = Query::Proposal::Listing.new(user, params)
-          expect(listing.pending.rows).to eq([proposal])
+          expect(listing.send(status).rows).to eq([proposal])
+        end
+      end
+
+      context "with an arbitrary client" do
+        before do
+          user.update_attribute(:client_slug, 'ncr')
+        end
+
+        it "only displays that user's Proposals when they are a client admin" do
+          proposal = FactoryGirl.create(:proposal, requester: user, status: status)
+
+          other_proposal = FactoryGirl.create(:proposal, status: status)
+          FactoryGirl.create(:ncr_work_order, proposal: other_proposal)
+
+          with_env_var('CLIENT_ADMIN_EMAILS', user.email_address) do
+            listing = Query::Proposal::Listing.new(user, params)
+            expect(listing.send(status).rows).to eq([proposal])
+          end
         end
       end
     end
