4b175e5f21f37cb2509e34ba3c07ce2906f2e397
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-19 12:58:50 -0400

    only display Proposals a user is involved with on /proposals

---
+++ spec/lib/query/proposal/listing_spec.rb
@@ -0,0 +1,32 @@
+describe Query::Proposal::Listing do
+  let(:params) { ActionController::Parameters.new }
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe '#pending' do
+    it "only returns that user's Proposals when they are an app admin" do
+      FactoryGirl.create(:proposal)
+      proposal = FactoryGirl.create(:proposal, requester: user)
+
+      with_env_var('ADMIN_EMAILS', user.email_address) do
+        listing = Query::Proposal::Listing.new(user, params)
+        expect(listing.pending.rows).to eq([proposal])
+      end
+    end
+
+    context "with an arbitrary client" do
+      before do
+        user.update_attribute(:client_slug, 'ncr')
+      end
+
+      it "only displays that user's Proposals when they are a client admin" do
+        proposal = FactoryGirl.create(:proposal, requester: user)
+        FactoryGirl.create(:ncr_work_order)
+
+        with_env_var('CLIENT_ADMIN_EMAILS', user.email_address) do
+          listing = Query::Proposal::Listing.new(user, params)
+          expect(listing.pending.rows).to eq([proposal])
+        end
+      end
+    end
+  end
+end
