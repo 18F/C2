8aa8a163471243b45f7f95d1e4492f92d0d8a6fe
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-25 16:54:57 -0400

    clean up SubscriberList

--- lib/subscriber_list.rb
+++ lib/subscriber_list.rb
@@ -7,12 +7,12 @@ class SubscriberList
 
   # Returns triplets of (user, role name, observation)
   def triples
-    requesters, approvers, others = self.partitioned_roles
+    requesters, approvers, observers = self.partitioned_roles
     requesters = requesters.map { |r| [r.user, "Requester", nil] }
     approvers = approvers.map { |r| [r.user, "Approver", nil] }
-    others = others.map { |r| [r.user, nil, self.proposal.observations.find_by(user: r.user)] }
+    observers = observers.map { |r| [r.user, nil, self.observation_for(r.user)] }
 
-    requesters + approvers + others
+    requesters + approvers + observers
   end
 
   # sorted by how they should be displayed
@@ -31,5 +31,9 @@ class SubscriberList
     observers = roles.select(&:observer?)
 
     [requesters, approvers, observers]
+  end
+
+  def observation_for(user)
+    self.proposal.observations.find_by(user: user)
   end
 end
