ad7b484cbda0daf44ffc03c7eb7b8d33e2e4e3cb
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-27 15:59:48 -0400

    refactor Query::Proposal::Versions

--- lib/query/proposal/versions.rb
+++ lib/query/proposal/versions.rb
@@ -1,12 +1,25 @@
 module Query
   module Proposal
     module Versions
-      def self.container(proposal)
-        config = TabularData::Container.config_for_client('versions', 'default')
-        container = TabularData::Container.new(:versions, config)
+      def self.relation(proposal)
         # TODO include versions from the related models
-        container.alter_query { |rel| rel.where(item_id: proposal.id, item_type: 'Proposal') }
-        container
+        C2Version.where(item_id: proposal.id, item_type: 'Proposal')
+      end
+
+      def self.container(proposal)
+        result = self.base_container
+        result.alter_query { |rel| rel.merge(self.relation(proposal)) }
+        result
+      end
+
+      protected
+
+      def self.container_config
+        TabularData::Container.config_for_client('versions', 'default')
+      end
+
+      def self.base_container
+        TabularData::Container.new(:versions, self.container_config)
       end
     end
   end
