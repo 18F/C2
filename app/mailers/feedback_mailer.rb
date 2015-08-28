61cee4b735e5a0c7ad7cc41948c25bc128ed57e8
Aidan Feldman <aidan.feldman@gsa.gov>
2015-08-28 15:27:09 -0400

    send feedback emails 'from' the submitter

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,12 +2,12 @@ class FeedbackMailer < ApplicationMailer
   def feedback(sending_user, form_values)
     form_strings = form_values.map { |key, val| "#{key}: #{val}" }
     message = form_strings.join("\n")
+    from = sending_user.try(:email_address) || self.default_sender_email
     mail(
       to: self.class.support_email,
       subject: 'Feedback submission',
-      from: self.default_sender_email,
+      from: from,
       body: message,
-      cc: sending_user.try(:email_address)
     )
   end
 
