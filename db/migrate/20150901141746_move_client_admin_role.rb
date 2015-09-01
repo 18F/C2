aa6708d389f34f8e5011aaadf3f19a5d544796f7
Peter Karman <peter.karman@gsa.gov>
2015-09-01 15:04:01 -0500

    allow for case where ENV does not match db records

--- db/migrate/20150901141746_move_client_admin_role.rb
+++ db/migrate/20150901141746_move_client_admin_role.rb
@@ -3,7 +3,7 @@ class MoveClientAdminRole < ActiveRecord::Migration
     role = Role.find_or_create_by(name: 'client_admin')
 
     User.client_admin_emails.each do |email_address|
-      user = User.find_by_email_address(email_address)
+      user = User.find_by_email_address(email_address) or next
       user.add_role(role)
       user.save!
     end
