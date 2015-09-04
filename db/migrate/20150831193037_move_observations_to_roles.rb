f5dcf235a623d837b16e48c1e634ee33b32a414a
Aidan Feldman <aidan.feldman@gsa.gov>
2015-09-04 13:42:00 -0400

    update observation migration so that the tuples don't need to be parsed

--- db/migrate/20150831193037_move_observations_to_roles.rb
+++ db/migrate/20150831193037_move_observations_to_roles.rb
@@ -1,16 +1,13 @@
 class MoveObservationsToRoles < ActiveRecord::Migration
-
   def change
     role = Role.find_or_create_by(name: 'observer')
 
-    # We have to use raw sql because we've subclassed the observation class
-    pgres = ProposalRole.connection.execute("SELECT DISTINCT(proposal_id, user_id) FROM observations WHERE user_id IN (SELECT id FROM users)")
+    # We have to use raw sql because we've subclassed the observation class. Also, use `GROUP_BY` to find DISTINCT pairs.
+    pgres = ProposalRole.connection.execute("SELECT proposal_id, user_id FROM observations WHERE user_id IN (SELECT id FROM users) GROUP BY proposal_id, user_id")
     pgres.each_row do |row|
-      matches = row.first.match(/\A\((\d+),(\d+)\)\z/)
-      ProposalRole.create(proposal_id: matches[1], user_id: matches[2], role_id: role.id)
+      ProposalRole.create(proposal_id: row['proposal_id'], user_id: row['user_id'], role_id: role.id)
     end
 
     drop_table :observations
-
   end
 end
