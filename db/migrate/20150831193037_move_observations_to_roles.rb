eee88309c4226be0fe68b3753b2c5fd4124a6215
Peter Karman <peter.karman@gsa.gov>
2015-09-04 10:06:48 -0500

    try to avoid sql constraint errors when migrating from observations to proposal_roles

--- db/migrate/20150831193037_move_observations_to_roles.rb
+++ db/migrate/20150831193037_move_observations_to_roles.rb
@@ -5,8 +5,18 @@ class MoveObservationsToRoles < ActiveRecord::Migration
 
     # We have to use raw sql because we've subclassed the observation class
     pgres = ProposalRole.connection.execute("SELECT proposal_id, user_id from observations")
+    seen_proles = {}
     pgres.each_row do |row|
+      # if we have no user (dangling FK) then skip it
+      user = User.find(row.second) or next
+
+      # track to avoid duplicates
+      k = [row.first.to_s, row.second.to_s, role.id.to_s].join(';')
+      next if seen_proles[k]
+
+      # create new record, marking it as done afterwards
       prole = ProposalRole.create(proposal_id: row.first, user_id: row.second, role_id: role.id)
+      seen_proles[k] = true
     end
 
     drop_table :observations
