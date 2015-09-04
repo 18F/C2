dbefae8d1fc14d08ab05d68cb52c6294fe2c0f38
Peter Karman <peter.karman@gsa.gov>
2015-09-04 11:10:47 -0500

    rewrite to use SQL to eliminate duplicates and orphans

--- db/migrate/20150831193037_move_observations_to_roles.rb
+++ db/migrate/20150831193037_move_observations_to_roles.rb
@@ -4,19 +4,9 @@ class MoveObservationsToRoles < ActiveRecord::Migration
     role = Role.find_or_create_by(name: 'observer')
 
     # We have to use raw sql because we've subclassed the observation class
-    pgres = ProposalRole.connection.execute("SELECT proposal_id, user_id from observations")
-    seen_proles = {}
+    pgres = ProposalRole.connection.execute("SELECT DISTINCT(proposal_id, user_id) FROM observations WHERE user_id IN (SELECT id FROM users)")
     pgres.each_row do |row|
-      # if we have no user (dangling FK) then skip it
-      user = User.find(row.second) or next
-
-      # track to avoid duplicates
-      k = [row.first.to_s, row.second.to_s, role.id.to_s].join(';')
-      next if seen_proles[k]
-
-      # create new record, marking it as done afterwards
       prole = ProposalRole.create(proposal_id: row.first, user_id: row.second, role_id: role.id)
-      seen_proles[k] = true
     end
 
     drop_table :observations
