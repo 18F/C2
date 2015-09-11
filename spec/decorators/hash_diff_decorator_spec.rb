d059ead3bd777ee104b6e891f3c8f36d4f874630
Aidan Feldman <aidan.feldman@gsa.gov>
2015-09-10 20:08:56 -0400

    add specs for formatting of add and removal events

--- spec/decorators/hash_diff_decorator_spec.rb
+++ spec/decorators/hash_diff_decorator_spec.rb
@@ -4,5 +4,15 @@ describe HashDiffDecorator do
       output = HashDiffDecorator.html_for(['+', 'foo', 'bar'])
       expect(output).to eq("<code>foo</code> was set to <code>bar</code>.")
     end
+
+    it "renders modification events" do
+      output = HashDiffDecorator.html_for(['~', 'foo', 'bar', 'baz'])
+      expect(output).to eq("<code>foo</code> was changed from <code>&quot;bar&quot;</code> to <code>&quot;baz&quot;</code>")
+    end
+
+    it "renders removal events" do
+      output = HashDiffDecorator.html_for(['-', 'foo'])
+      expect(output).to eq("<code>foo</code> was removed.")
+    end
   end
 end
