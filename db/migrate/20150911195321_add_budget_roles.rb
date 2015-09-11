12d8cf98927b286279a159f88a682470f1253c87
Peter Karman <peter.karman@gsa.gov>
2015-09-11 14:57:52 -0500

    migrate budget env vars to roles

---
+++ db/migrate/20150911195321_add_budget_roles.rb
@@ -0,0 +1,24 @@
+class AddBudgetRoles < ActiveRecord::Migration
+  def change
+    User.with_email_role_slug!(
+      ENV['NCR_BA61_TIER1_BUDGET_MAILBOX'] || 'communicart.budget.approver@gmail.com',
+      'BA61_tier1_budget_approver',
+      'ncr'
+    )
+    User.with_email_role_slug!(
+      ENV['NCR_BA61_TIER2_BUDGET_MAILBOX'] || 'communicart.ofm.approver@gmail.com',
+      'BA61_tier2_budget_approver',
+      'ncr'
+    )
+    User.with_email_role_slug!(
+      ENV['NCR_BA80_BUDGET_MAILBOX'] || 'communicart.budget.approver@gmail.com',
+      'BA80_budget_approver',
+      'ncr'
+    )
+    User.with_email_role_slug!(
+      ENV['NCR_OOL_BA80_BUDGET_MAILBOX'] || 'communicart.budget.approver@gmail.com',
+      'OOL_BA80_budget_approver',
+      'ncr'
+    )
+  end
+end
