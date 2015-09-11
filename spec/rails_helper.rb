b51af583fe626aaef09576a4fd98a237f051dfa2
Peter Karman <peter.karman@gsa.gov>
2015-09-11 16:36:02 -0500

    always load db seeds after cleaning db for tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,6 +75,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+    Rails.application.load_seed
   end
 
   config.before(:each) do
