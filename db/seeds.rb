481a87f04d2d3b2b488f5f03ddfd53843fe25cc6
Peter Karman <peter.karman@gsa.gov>
2015-09-11 15:04:32 -0500

    add seeds for all new roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Role.find_or_create_by(name: 'observer')
+Role.find_or_create_by(name: 'client_admin')
+Role.find_or_create_by(name: 'admin')
+
+User.with_email_role_slug!(
+  ENV['NCR_BA61_TIER1_BUDGET_MAILBOX'] || 'communicart.budget.approver@gmail.com',
+  'BA61_tier1_budget_approver',
+  'ncr'
+)
+User.with_email_role_slug!(
+  ENV['NCR_BA61_TIER2_BUDGET_MAILBOX'] || 'communicart.ofm.approver@gmail.com',
+  'BA61_tier2_budget_approver',
+  'ncr'
+)
+User.with_email_role_slug!(
+  ENV['NCR_BA80_BUDGET_MAILBOX'] || 'communicart.budget.approver@gmail.com',
+  'BA80_budget_approver',
+  'ncr'
+)
+User.with_email_role_slug!(
+  ENV['NCR_OOL_BA80_BUDGET_MAILBOX'] || 'communicart.budget.approver@gmail.com',
+  'OOL_BA80_budget_approver',
+  'ncr'
+)
