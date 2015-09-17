b05709bc7aa6a857b5ffd435cbc6f2d2d313b18c
Peter Karman <peter.karman@gsa.gov>
2015-09-17 11:25:37 -0500

    add clarifying comments

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -14,6 +14,9 @@ C2::Application.config.action_mailer.default_url_options ||= {
   port: ENV['DEFAULT_URL_PORT'] || default_port
 }
 
+# indicate that this is not a real request in the email subjects,
+# if we are running in non-production env.
+# NOTE that staging uses Rails.env.production, so cannot rely on that config.
 unless ENV['DISABLE_SANDBOX_WARNING'] == 'true'
   class PrefixEmailSubject
     def self.delivering_email(mail)
