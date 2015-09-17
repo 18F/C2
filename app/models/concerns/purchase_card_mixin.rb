69924a660333573976f294b216d837bd5be1d0c1
Peter Karman <peter.karman@gsa.gov>
2015-09-17 15:01:19 -0500

    whitespace only, codeclimate fix

--- app/models/concerns/purchase_card_mixin.rb
+++ app/models/concerns/purchase_card_mixin.rb
@@ -6,18 +6,18 @@ module PurchaseCardMixin
   included do
     def self.which_fiscal_year(year, month)
       if month >= 10
-        year + 1 
+        year + 1
       else
         year
-      end 
+      end
     end
 
     def self.max_amount
-      fiscals = { 
+      fiscals = {
         2015      => 3000,
         2016      => 3500,
         'default' => 3500,
-      }   
+      }
       now = Time.zone.now
       this_fiscal = self.which_fiscal_year(now.year, now.month)
       fiscals[this_fiscal] || fiscals['default']
@@ -25,11 +25,10 @@ module PurchaseCardMixin
     validates name.constantize.purchase_amount_column_name, numericality: {
       less_than_or_equal_to: name.constantize.max_amount,
       message: "must be less than or equal to #{ActiveSupport::NumberHelper.number_to_currency(name.constantize.max_amount)}"
-    }   
+    }
     validates name.constantize.purchase_amount_column_name, numericality: {
       greater_than_or_equal_to: 1,
       message: "must be greater than or equal to #{ActiveSupport::NumberHelper.number_to_currency(1)}"
     }
   end
-
 end
