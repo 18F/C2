cb69cb0cd3916a56a5b9d67b9c7bc4dd012397d3
Peter Karman <peter.karman@gsa.gov>
2015-09-18 09:24:49 -0500

    no functional change, just refactor to satisfy rubocop

--- lib/ncr/reporter.rb
+++ lib/ncr/reporter.rb
@@ -2,7 +2,7 @@ module Ncr
   module Reporter
     def self.total_last_week
       Proposal.where(client_data_type: "Ncr::WorkOrder")
-              .where("created_at > ?",1.week.ago).count
+        .where("created_at > ?", 1.week.ago).count
     end
 
     def self.total_unapproved
@@ -21,63 +21,74 @@ module Ncr
       budget_proposals("BA80", 1.week.ago)
     end
 
-    # rubocop:disable all
+    def self.proposal_public_url(proposal)
+      Rails.application.routes.url_helpers.url_for(controller: 'proposals', action: 'show', id: proposal.id, host: DEFAULT_URL_HOST)
+    end
+
+    def self.make_csv_row(proposal)
+      [
+        self.proposal_public_url(proposal),
+        proposal.requester.email_address,
+        proposal.client_data.approving_official_email_address,
+        proposal.client_data.cl_number,
+        proposal.client_data.function_code,
+        proposal.client_data.soc_code,
+        proposal.created_at
+      ]
+    end
+
     def self.as_csv(proposals)
       CSV.generate do |csv|
         csv << ['URL', 'Requester', 'Approver', 'CL', 'Function Code', 'Soc Code', 'Created']
         proposals.each do |p|
-          csv << [
-            Rails.application.routes.url_helpers.url_for(controller: 'proposals', action: 'show', id: p.id, host: DEFAULT_URL_HOST),
-            p.requester.email_address,
-            p.client_data.approving_official_email_address,
-            p.client_data.cl_number,
-            p.client_data.function_code,
-            p.client_data.soc_code,
-            p.created_at
-          ]
+          csv << self.make_csv_row(p)
         end
       end
     end
-    # rubocop:enable all
 
     def self.proposals_pending_approving_official
       # TODO convert to SQL
       Proposal.pending
-              .where(client_data_type: 'Ncr::WorkOrder')
-              .select{ |p| p.individual_approvals.pluck(:status)[0] == 'actionable' }
+        .where(client_data_type: 'Ncr::WorkOrder')
+        .select { |p| p.individual_approvals.pluck(:status)[0] == 'actionable' }
     end
 
     def self.proposals_pending_budget
       # TODO convert to SQL
       Proposal.pending
-              .where(client_data_type: 'Ncr::WorkOrder')
-              .select{ |p| p.individual_approvals.pluck(:status).last == 'actionable' }
-              .sort_by { |pr| pr.client_data.expense_type }
+        .where(client_data_type: 'Ncr::WorkOrder')
+        .select { |p| p.individual_approvals.pluck(:status).last == 'actionable' }
+        .sort_by { |pr| pr.client_data.expense_type }
     end
 
     def self.budget_proposals(type, timespan)
       # TODO convert to SQL
       Proposal.approved
-              .where(client_data_type: 'Ncr::WorkOrder')
-              .where('created_at > ?', timespan)
-              .select { |pr| pr.client_data.expense_type == type }
+        .where(client_data_type: 'Ncr::WorkOrder')
+        .where('created_at > ?', timespan)
+        .select { |pr| pr.client_data.expense_type == type }
     end
 
-    # rubocop:disable all
-    def self.proposals_tier_one_pending_sql
+    def self.proposals_tier_one_pending_approver_sql
       tier_one_sql = User.sql_for_role_slug('BA61_tier1_budget_approver', 'ncr')
 
-      approver_sql = <<-SQL.gsub(/^ {8}/, '')
+      <<-SQL.gsub(/^ {8}/, '')
         SELECT a.proposal_id FROM approvals AS a
         WHERE a.status='actionable' AND a.user_id IN (#{tier_one_sql})
       SQL
+    end
 
-      work_order_sql = <<-SQL.gsub(/^ {8}/, '')
+    def self.proposals_tier_one_work_order_sql
+      <<-SQL.gsub(/^ {8}/, '')
         SELECT id FROM ncr_work_orders AS nwo
         WHERE nwo.org_code!='#{Ncr::Organization::WHSC_CODE}'
         AND nwo.expense_type IN ('BA60','BA61')
       SQL
+    end
 
+    def self.proposals_tier_one_pending_sql
+      approver_sql   = self.proposals_tier_one_pending_approver_sql
+      work_order_sql = self.proposals_tier_one_work_order_sql
       <<-SQL.gsub(/^ {8}/, '')
         SELECT * FROM proposals AS p
         WHERE p.status='pending'
@@ -86,7 +97,6 @@ module Ncr
         AND p.id IN (#{approver_sql})
       SQL
     end
-    # rubocop:enable all
 
     def self.proposals_tier_one_pending
       Proposal.find_by_sql(self.proposals_tier_one_pending_sql)
