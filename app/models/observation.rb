9dc54454798db7a8969e7cd558736584ade16fcd
Ian Stanfield <ianstanfield@ians-air.attlocal.net>
2015-09-20 11:47:22 -0700

    Validations on proposal_role, observation, user_role

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -4,6 +4,10 @@ class Observation < ProposalRole
   belongs_to :user
   belongs_to :proposal
 
+  validates :user_id, presence: true
+  validates :proposal_id, presence: true
+
+
   delegate :full_name, :email_address, to: :user, prefix: true
 
   after_initialize :init
