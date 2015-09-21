780dc8f50fb920af5a915e4480bf16fe21237c2c
Peter Karman <peter.karman@gsa.gov>
2015-09-21 17:13:47 -0500

    customize show page to list roles

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -5,21 +5,35 @@ ActiveAdmin.register User do
 #
 # permit_params :list, :of, :attributes, :on, :model
 
-permit_params :first_name, :last_name, :email_address, :client_slug, role_ids: []
+  permit_params :first_name, :last_name, :email_address, :client_slug, role_ids: []
 
-form do |f|
-  f.inputs 'Profile' do
-    f.input :email_address
-    f.input :first_name
-    f.input :last_name
-    f.input :client_slug
-    #f.input :client_slug, :as => :select, :include_blank => true, :collection => Proposal.client_slugs
+  # /:id/edit page
+  form do |f|
+    f.inputs 'Profile' do
+      f.input :email_address
+      f.input :first_name
+      f.input :last_name
+      f.input :client_slug
+      #f.input :client_slug, :as => :select, :include_blank => true, :collection => Proposal.client_slugs
+    end
+    f.inputs 'Roles' do
+      f.input :roles, :as => :select, :collection => Role.all
+    end
+    f.actions
   end
-  f.inputs 'Roles' do
-    f.input :roles, :as => :select, :collection => Role.all
+
+  # /:id page
+  show do
+    attributes_table do
+      row :email_address
+      row :first_name
+      row :last_name
+      row :client_slug
+      row :created_at
+      row :updated_at
+      row ('Roles') { user.roles.map(&:name).join(', ') }
+    end
   end
-  f.actions
-end
 
 #
 # or
