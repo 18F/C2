cfebd6b390cdf98d9bd5410ae9e5d3761e27f551
Peter Karman <peter.karman@gsa.gov>
2015-09-21 13:21:23 -0500

    allow Role assignment from /admin/users/:id/edit page (NOTE requires some pending changes before this works)

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,6 +4,23 @@ ActiveAdmin.register User do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
+
+permit_params :first_name, :last_name, :email_address, :client_slug, role_ids: []
+
+form do |f|
+  f.inputs 'Profile' do
+    f.input :email_address
+    f.input :first_name
+    f.input :last_name
+    f.input :client_slug
+    #f.input :client_slug, :as => :select, :include_blank => true, :collection => Proposal.client_slugs
+  end
+  f.inputs 'Roles' do
+    f.input :roles, :as => :select, :collection => Role.all
+  end
+  f.actions
+end
+
 #
 # or
 #
