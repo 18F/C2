3cac3170070489fdd0ee53d31b97e9a5b1f95b3f
Peter Karman <peter.karman@gsa.gov>
2015-09-22 09:55:49 -0500

    make rubocop happy

--- app/admin/role.rb
+++ app/admin/role.rb
@@ -1,18 +1,7 @@
 ActiveAdmin.register Role do
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
   permit_params :name
 
   # remove filters by association
@@ -39,5 +28,4 @@ ActiveAdmin.register Role do
 
     active_admin_comments
   end
-
 end
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,10 +1,6 @@
 ActiveAdmin.register User do
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   permit_params :first_name, :last_name, :email_address, :client_slug, role_ids: []
 
   # /:id/edit page
@@ -13,10 +9,10 @@ ActiveAdmin.register User do
       f.input :email_address
       f.input :first_name
       f.input :last_name
-      f.input :client_slug, :as => :select, :include_blank => true, :collection => Proposal.client_slugs
+      f.input :client_slug, as: :select, include_blank: true, collection: Proposal.client_slugs
     end
     f.inputs 'Roles' do
-      f.input :roles, :as => :select, :collection => Role.all
+      f.input :roles, as: :select, collection: Role.all
     end
     f.actions
   end
@@ -30,18 +26,7 @@ ActiveAdmin.register User do
       row :client_slug
       row :created_at
       row :updated_at
-      row ('Roles') { user.roles.map(&:name).join(', ') }
+      row('Roles') { user.roles.map(&:name).join(', ') }
     end
   end
-
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
-
 end
