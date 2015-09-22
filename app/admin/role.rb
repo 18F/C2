b4743ca426d38c0465d0a50e81067c159cf815d2
Peter Karman <peter.karman@gsa.gov>
2015-09-22 09:31:10 -0500

    customize /admin/roles page

--- app/admin/role.rb
+++ app/admin/role.rb
@@ -13,5 +13,31 @@ ActiveAdmin.register Role do
 #   permitted
 # end
 
+  permit_params :name
+
+  # remove filters by association
+  filter :name
+
+  # show all users with a role on /admin/roles/:id page
+  show do
+    panel "Role Details" do
+      attributes_table_for role do
+        row :id
+        row :name
+        row :created_at
+        row :updated_at
+      end
+    end
+
+    panel "Users" do
+      table_for role.users do |tbl|
+        tbl.column('Email') { |user| link_to user.email_address, admin_user_path(user) }
+        tbl.column('Name')  { |user| user.full_name }
+        tbl.column('Client') { |user| user.client_slug }
+      end
+    end
+
+    active_admin_comments
+  end
 
 end
