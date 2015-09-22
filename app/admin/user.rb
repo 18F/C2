8e8568f7fb54eb672fbf0cd00689f1852609155a
Peter Karman <peter.karman@gsa.gov>
2015-09-22 09:17:24 -0500

    make client_slug a select dropdown in /admin/users/:id/edit which also seems to solve the User.valid? bug where Proposal.client_slugs is empty

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -13,8 +13,7 @@ ActiveAdmin.register User do
       f.input :email_address
       f.input :first_name
       f.input :last_name
-      f.input :client_slug
-      #f.input :client_slug, :as => :select, :include_blank => true, :collection => Proposal.client_slugs
+      f.input :client_slug, :as => :select, :include_blank => true, :collection => Proposal.client_slugs
     end
     f.inputs 'Roles' do
       f.input :roles, :as => :select, :collection => Role.all
