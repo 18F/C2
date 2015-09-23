10649cb7d21d32324260ef4cf1b932695c69530c
Aidan Feldman <aidan.feldman@gsa.gov>
2015-09-23 15:39:31 -0400

    ensure each new feedback email is in its own thread

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,8 +1,14 @@
 class FeedbackMailer < ApplicationMailer
+  include ConversationThreading
+
   def feedback(sending_user, form_values)
     form_strings = form_values.map { |key, val| "#{key}: #{val}" }
     message = form_strings.join("\n")
     from = sending_user.try(:email_address) || form_values[:email] || self.default_sender_email
+
+    # ensure each new feedback email is in its own thread
+    self.set_thread_id(SecureRandom.hex)
+
     mail(
       to: self.class.support_email,
       subject: 'Feedback submission',
