72d80b996705baa693066323d0e908802a5c5171
Aidan Feldman <aidan.feldman@gsa.gov>
2015-09-23 16:20:43 -0400

    include gmail-specific X-Entity-Ref-ID header for threading emails

--- app/mailers/concerns/conversation_threading.rb
+++ app/mailers/concerns/conversation_threading.rb
@@ -5,5 +5,8 @@ module ConversationThreading
     # http://www.jwz.org/doc/threading.html
     headers['In-Reply-To'] = msg_id
     headers['References'] = msg_id
+    # GMail-specific
+    # http://stackoverflow.com/a/25435722/358804
+    headers['X-Entity-Ref-ID'] = msg_id
   end
 end
