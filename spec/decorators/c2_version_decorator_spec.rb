1cdd3bb32b0c37a1d1d13db1fa11702174889d74
Aidan Feldman <aidan.feldman@gsa.gov>
2015-09-23 01:33:17 -0400

    add specs around different objects passed to the C2VersionDecorator

--- spec/decorators/c2_version_decorator_spec.rb
+++ spec/decorators/c2_version_decorator_spec.rb
@@ -14,5 +14,20 @@ describe C2VersionDecorator do
       output = decorated.to_html
       expect(escape(output)).to eq("&lt;script&gt;alert()&lt;/script&gt; Thunder was added as an approver.")
     end
+
+    it "includes a message for individual approvals" do
+      approval = FactoryGirl.build(:approval)
+      version = double(C2Version, event: 'create', item: approval)
+      decorated = C2VersionDecorator.new(version)
+
+      approver = approval.user
+      expect(decorated.to_html).to eq("#{approver.full_name} was added as an approver.")
+    end
+
+    it "doesn't break for unknown classes" do
+      version = double(C2Version, event: 'create', item: double)
+      decorated = C2VersionDecorator.new(version)
+      expect(decorated.to_html).to eq(nil)
+    end
   end
 end
