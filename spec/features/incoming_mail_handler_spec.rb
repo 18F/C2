3df6f1e9951c4892d3064f31fab8451d2e6eeb07
Peter Karman <peter.karman@gsa.gov>
2015-09-24 21:36:04 -0500

    move mandrill json into a fixture file

--- spec/features/incoming_mail_handler_spec.rb
+++ spec/features/incoming_mail_handler_spec.rb
@@ -2,6 +2,7 @@ describe "Handles incoming email" do
   let(:proposal) { FactoryGirl.create(:proposal, :with_parallel_approvers) }
   let(:approval) { proposal.individual_approvals.first }
   let(:mail) { CommunicartMailer.actions_for_approver(approval) }
+  let(:mandrill_inbound_noapp) { File.read(RSpec.configuration.fixture_path + '/mandrill_inbound_noapp.json') }
 
   def mandrill_payload_from_message(mail_msg)
     headers = {}
@@ -24,62 +25,8 @@ describe "Handles incoming email" do
   end
 
   it "should drop non-app email on the floor" do
-    # this raw json string here as much for example documentation as for test.
-    mandrill_event = <<-END.gsub(/^ {6}/, '')
-      [
-         {
-            "event" : "inbound",
-            "msg" : {
-               "subject" : "test via example server",
-               "template" : null,
-               "tags" : [],
-               "spf" : {
-                  "detail" : "",
-                  "result" : "none"
-               },
-               "from_email" : "someone@example.com",
-               "dkim" : {
-                  "valid" : false,
-                  "signed" : false
-               },
-               "email" : "tester@c2.18f.gov",
-               "sender" : null,
-               "text" : "hello world",
-               "raw_msg" : "Received: from example.com (unknown [93.184.216.34])\\n\\tby ip-xx-xx-xx (Postfix) with ESMTPS id 3EB7C2C06B9\\n\\tfor <tester@c2.18f.gov>; Wed, 23 Sep 2015 21:22:46 +0000 (UTC)\\nReceived: by example.com (Postfix, from userid 500)\\n\\tid 67DDF3E841; Wed, 23 Sep 2015 16:22:45 -0500 (CDT)\\nTo: tester@c2.18f.gov\\nSubject: test via example server\\nMessage-Id: <20150923212245.67DDF3E841@example.com>\\nDate: Wed, 23 Sep 2015 16:22:45 -0500 (CDT)\\nFrom: someone@example.com\\n\\nhello world",
-               "headers" : {
-                  "Received" : [
-                     "from example.com (unknown [93.184.216.34]) by ip-xx-xx-xx (Postfix) with ESMTPS id 3EB7C2C06B9 for <tester@c2.18f.gov>; Wed, 23 Sep 2015 21:22:46 +0000 (UTC)",
-                     "by example.com (Postfix, from userid 500) id 67DDF3E841; Wed, 23 Sep 2015 16:22:45 -0500 (CDT)"
-                  ],
-                  "From" : "someone@example.com",
-                  "Message-Id" : "<20150923212245.67DDF3E841@peknet.com>",
-                  "To" : "tester@c2.18f.gov",
-                  "Subject" : "test via example server",
-                  "Date" : "Wed, 23 Sep 2015 16:22:45 -0500 (CDT)"
-               },
-               "to" : [
-                  [
-                     "tester@c2.18f.gov",
-                     null
-                  ]
-               ],
-               "spam_report" : {
-                  "matched_rules" : [
-                     {
-                        "score" : 1.3,
-                        "name" : "RDNS_NONE",
-                        "description" : "Delivered to internal network by a host with no rDNS"
-                     }
-                  ],
-                  "score" : 1.3
-               }
-            },
-            "ts" : 1443043366
-         }
-      ]
-    END
     handler = IncomingMail::Handler.new
-    resp = handler.handle(JSON.parse(mandrill_event))
+    resp = handler.handle(JSON.parse(mandrill_inbound_noapp))
     expect(resp.action).to eq(IncomingMail::Response::DROPPED)
   end
 
