8749bbb191ef7fe08dec6dac0f85b54043b809f5
Jessie A. Young <jessica.young@gsa.gov>
2015-10-06 17:32:18 -0400

    Remove Turnip
    * having turnip + capybara represents an overlap in frameworks
    * seem to have many more capybara specs, so should favor that
    * use cases covered by turnip appear to be covered by other specs
    * this will speed up our test suite - yay!

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,27 +2,17 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'steps/user_steps'
-require 'steps/approval_steps'
-
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
-Dir.glob('./spec/steps/**/*_steps.rb') { |f| load f, true }
-
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/support/fixtures"
   config.use_transactional_fixtures = false
-  config.raise_error_for_unimplemented_steps = true
   config.infer_spec_type_from_file_location!
 
-  config.include ApprovalSteps, type: :feature
   config.include ControllerSpecHelper, type: :controller
   config.include EnvironmentSpecHelper
   config.include FeatureSpecHelper, type: :feature
   config.include RequestSpecHelper, type: :request
-  config.include UserSteps, type: :feature
 
   [:feature, :request].each do |type|
     config.include IntegrationSpecHelper, type: type
