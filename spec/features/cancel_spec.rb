c70e938281fc0ceca221c889cff3ec99b9229718
Jessie A. Young <jessica.young@gsa.gov>
2015-10-06 16:24:32 -0400

    Speed up 3 slowest specs
    * Did an `rspec` profile and edited the 3 slowest specs
    * Made single quotes more consistent while I was there
    * Replaced a "sleep" with Timecop
    * Inlined `let` statements since those can get hairy

--- spec/features/cancel_spec.rb
+++ spec/features/cancel_spec.rb
@@ -1,80 +1,95 @@
-describe "Canceling a request" do
-  let!(:client_data) { FactoryGirl.create(:ncr_work_order, :with_approvers) }
-  let!(:proposal) { client_data.proposal }
-
-  it "shows a cancel link for the requester" do
+describe 'Canceling a request' do
+  it 'shows a cancel link for the requester' do
+    proposal = FactoryGirl.create(:proposal)
     login_as(proposal.requester)
+
     visit proposal_path(proposal)
-    expect(page).to have_content("Cancel my request")
+
+    expect(page).to have_content('Cancel my request')
   end
 
-  it "does not show a cancel link for non-requesters" do
+  it 'does not show a cancel link for non-requesters' do
+    proposal = FactoryGirl.create(:proposal, :with_approver)
     login_as(proposal.approvers.first)
+
     visit proposal_path(proposal)
-    expect(page).to_not have_content("Cancel my request")
+
+    expect(page).to_not have_content('Cancel my request')
   end
 
-  it "prompts the requester for a reason" do
+  it 'prompts the requester for a reason' do
+    proposal = FactoryGirl.create(:proposal)
     login_as(proposal.requester)
+
     visit proposal_path(proposal)
     click_on('Cancel my request')
+
     expect(current_path).to eq("/proposals/#{proposal.id}/cancel_form")
   end
 
   context 'email' do
-    # TODO this shouldn't actually be needed
-    around(:each) { ActionMailer::Base.deliveries.clear }
-
-    it "send emails when cancellation is complete" do
+    it 'send emails when cancellation is complete' do
+      ActionMailer::Base.deliveries.clear
+      proposal = FactoryGirl.create(:proposal)
       expect(deliveries.length).to eq(0)
 
       login_as(proposal.requester)
       visit proposal_path(proposal)
       click_on('Cancel my request')
-      fill_in "reason_input", with: "This is a good reason for the cancellation."
+      fill_in 'reason_input', with: 'This is a good reason for the cancellation.'
       click_on('Yes, cancel this request')
 
-      expect(deliveries.length).to eq(2)
+      expect(deliveries.length).to eq(1)
     end
   end
 
-  context "entering in a reason cancellation" do
-    it "successfully saves comments, changes the request status" do
+  context 'entering in a reason cancellation' do
+    it 'successfully saves comments, changes the request status' do
+      proposal = FactoryGirl.create(:proposal)
       login_as(proposal.requester)
 
       visit proposal_path(proposal)
       click_on('Cancel my request')
-      fill_in "reason_input", with: "This is a good reason for the cancellation."
+      fill_in 'reason_input', with: 'This is a good reason for the cancellation.'
       click_on('Yes, cancel this request')
 
       expect(current_path).to eq("/proposals/#{proposal.id}")
-      expect(page).to have_content("Your request has been cancelled")
-      expect(proposal.reload.status).to eq("cancelled")
-      expect(proposal.reload.comments.last.comment_text).to eq("Request cancelled with comments: This is a good reason for the cancellation.")
+      expect(page).to have_content('Your request has been cancelled')
+      expect(proposal.reload.status).to eq('cancelled')
+      expect(proposal.reload.comments.last.comment_text).to eq('Request cancelled with comments: This is a good reason for the cancellation.')
     end
 
-    it "displays an error if the reason is blank" do
+    it 'displays an error if the reason is blank' do
+      proposal = FactoryGirl.create(:proposal)
       login_as(proposal.requester)
+
       visit proposal_path(proposal)
       click_on('Cancel my request')
-      fill_in "reason_input", with: ""
+      fill_in "reason_input", with: ''
       click_on('Yes, cancel this request')
-      expect(page).to have_content("A reason for cancellation is required. Please indicate why this request needs to be cancelled.")
+
+      expect(page).to have_content('A reason for cancellation is required. Please indicate why this request needs to be cancelled.')
     end
   end
 
-  context "Cancel landing page" do
-    it "succesfully opens the page for a requester" do
+  context 'Cancel landing page' do
+    it 'succesfully opens the page for a requester' do
+      proposal = FactoryGirl.create(:proposal)
       login_as(proposal.requester)
+
       visit cancel_form_proposal_path(proposal)
+
       expect(page).to have_content("Cancellation: #{proposal.name}")
       expect(current_path).to eq("/proposals/#{proposal.id}/cancel_form")
     end
 
-    it "redirects for non-requesters" do
+    it 'redirects for non-requesters' do
+      proposal = FactoryGirl.create(:proposal, :with_approver)
       login_as(proposal.approvers.first)
+
       visit cancel_form_proposal_path(proposal)
-      expect(page).to have_content("You are not the requester")
+
+      expect(page).to have_content('You are not the requester')
       expect(current_path).to eq("/proposals/#{proposal.id}")
     end
   end
--- spec/features/version_check_spec.rb
+++ spec/features/version_check_spec.rb
@@ -1,16 +1,19 @@
-describe "Version check" do
-  let(:proposal) { FactoryGirl.create(:proposal, :with_parallel_approvers) }
-
-  it "occurs if the proposal is modified in after seeing the profile page" do
+describe 'Version check' do
+  it 'occurs if the proposal is modified in after seeing the profile page' do
+    proposal = FactoryGirl.create(:proposal, :with_parallel_approvers)
     login_as(proposal.approvers.first)
-    visit "/proposals/#{proposal.id}"
+    time = Time.current
 
-    sleep 1.second  # wait to get a new update time
-    proposal.touch
+    Timecop.freeze(time) do
+      visit proposal_path(proposal)
 
-    click_on 'Approve'
+      Timecop.travel(time + 1.minute) do
+        proposal.touch
+        click_on 'Approve'
+      end
+    end
 
-    expect(page).to have_content("This request has recently changed.")
-    expect(current_path).to eq("/proposals/#{proposal.id}")
+    expect(page).to have_content('This request has recently changed.')
+    expect(current_path).to eq(proposal_path(proposal))
   end
 end
