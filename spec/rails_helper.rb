5ba04cff5a522081448149fb3b38055e7045af67
Jessie A. Young <jessica.young@gsa.gov>
2015-10-07 11:26:39 -0400

    Add capybara specs for scenarios covered by Turnip

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/support/fixtures"
   config.use_transactional_fixtures = false
