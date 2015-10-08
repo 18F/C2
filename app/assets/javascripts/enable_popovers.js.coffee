fc9357dd22a9cd8900ec17576bbeb2c3c63bd2e8
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2015-10-08 15:28:14 -0700

    Added popover helper, moved some others to UiHelper

--- app/assets/javascripts/enable_popovers.js.coffee
+++ app/assets/javascripts/enable_popovers.js.coffee
@@ -1,2 +1,7 @@
+# Enables Bootstrap's JS popovers:
+# http://getbootstrap.com/javascript/#popovers
+# To format the data attributes for popover usage, see
+# UiHelper::popover_data_attrs()
+
 $ ->
   $('[data-toggle="popover"]').popover()
---
+++ app/helpers/ui_helper.rb
@@ -0,0 +1,43 @@
+module UiHelper
+  # Variation of https://git.io/ugBzaQ
+  def humanized_options_for_select(options)
+    options = options.map do |val|
+      [val.humanize, val]
+    end
+    options_for_select(options)
+  end
+
+  def bootstrap_alert_class(key)
+    suffix = case key.to_sym
+      when :notice
+        'info'
+      when :error
+        'danger'
+      else
+        key
+      end
+
+    "bg-#{suffix}"
+  end
+
+  def flash_message(val)
+    if val.is_a?(Enumerable)
+      val.join('. ')
+    else
+      val
+    end
+  end
+
+  def flash_list
+    flash.map do |key, val|
+      [key, flash_message(val)]
+    end
+  end
+
+  def popover_data_attrs(key)
+    { toggle: 'popover', trigger: 'focus', html: true, placement: 'top',
+      title: I18n.t("helpers.popover.#{key}.title"),
+      content: I18n.t("helpers.popover.#{key}.content")
+    }
+  end
+end
