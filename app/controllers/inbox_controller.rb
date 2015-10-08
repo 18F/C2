e80675eefb216713ce5cee03f875d20253e59077
Peter Karman <peter.karman@gsa.gov>
2015-10-08 12:29:26 -0500

    treat FORWARDED email as complete success

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -7,8 +7,8 @@ class InboxController < ApplicationController
     if resp.action == IncomingMail::Response::ERROR
       # a non-200 response means Mandrill will keep trying up to 100x.
       head 500
-    elsif resp.action == IncomingMail::Response::DROPPED
-      head 204 # TODO does mandrill treat this like 200?
+    elsif resp.action == IncomingMail::Response::FORWARDED
+      head 200
     else
       head 200 # default tells mandrill to stop trying to send the event
     end
