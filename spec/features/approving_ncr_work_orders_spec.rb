ca7af5866ecd281a6d18f3b8c4acedfe49568e35
Jessie A. Young <jessieayoung@Jessie-For-President.local>
2015-10-14 17:39:33 -0700

    One feature spec per file for NCR work order specs
    * Was trying to see if * https://www.pivotaltracker.com/story/show/98774038
      was already implemented, but was having trouble navigating the 662
      line spec file
    * Good rule of thumb is to have one featuer spec file per feature
    * This split out will help me actually see what's going on
    * Working on this refactor made it clearer why `before` and `let`
      statements be very problematic: splitting the specs into separate
      files was very hairy because they were nested and some of the parent
      spec's setup was required for nested specs and some wasn't. Figured it
      out through trial and error but would still be nice to move toward
      including all of the setup for each spec within the test itself (which
      can still mean putting shared logic outside of the specs, but as a
      method that needs to be called rather than a `before` or `let`)

---
+++ spec/features/approving_ncr_work_orders_spec.rb
@@ -0,0 +1,30 @@
+describe "approving a work order" do
+    let(:work_order)   { create(:ncr_work_order) }
+    let(:ncr_proposal) { work_order.proposal }
+
+  before do
+    Timecop.freeze(10.hours.ago) do
+      work_order.setup_approvals_and_observers('approver@example.com')
+    end
+    login_as(work_order.approvers.first)
+  end
+
+  it "allows an approver to approve work order" do
+    Timecop.freeze do
+      visit "/proposals/#{ncr_proposal.id}"
+      click_on("Approve")
+      expect(current_path).to eq("/proposals/#{ncr_proposal.id}")
+      expect(page).to have_content("You have approved #{work_order.public_identifier}")
+      approval = Proposal.last.individual_approvals.first
+      expect(approval.status).to eq('approved')
+      expect(approval.approved_at.utc.to_s).to eq(Time.now.utc.to_s)
+    end
+  end
+  it "doesn't send multiple emails to approvers who are also observers" do
+    work_order.add_observer(work_order.approvers.first.email_address)
+    visit "/proposals/#{ncr_proposal.id}"
+    click_on("Approve")
+    expect(work_order.proposal.observers.length).to eq(1)
+    expect(deliveries.length).to eq(1)
+  end
+end
---
+++ spec/features/creating_ncr_work_orders_spec.rb
@@ -0,0 +1,312 @@
+feature "creating a work order" do
+  around(:each) do |example|
+    with_env_var('DISABLE_SANDBOX_WARNING', 'true') do
+      example.run
+    end
+  end
+
+  let!(:approver) { create(:user) }
+
+  scenario "requires sign-in" do
+    visit '/ncr/work_orders/new'
+    expect(current_path).to eq('/')
+    expect(page).to have_content("You need to sign in")
+  end
+
+  with_feature 'RESTRICT_ACCESS' do
+    scenario "requires a GSA email address" do
+      user = create(:user, email_address: 'intruder@example.com', client_slug: 'ncr')
+      login_as(user)
+
+      visit '/ncr/work_orders/new'
+
+      expect(page.status_code).to eq(403)
+      expect(page).to have_content("You must be logged in with a GSA email address")
+    end
+  end
+
+  context "when signed in as the requester" do
+    let(:requester) { create(:user, client_slug: 'ncr') }
+    let(:ncr_helper_class) { Class.new { extend Ncr::WorkOrdersHelper } }
+
+    before do
+      login_as(requester)
+    end
+
+    describe "tiered approval uses Role-based Users" do
+      scenario "saves a Proposal with the attributes" do
+        expect(Dispatcher).to receive(:deliver_new_proposal_emails)
+
+        visit '/ncr/work_orders/new'
+        fill_in 'Project title', with: "buying stuff"
+        fill_in 'Description', with: "desc content"
+        choose 'BA80'
+        fill_in 'RWA Number', with: 'F1234567'
+        fill_in 'Vendor', with: 'ACME'
+        fill_in 'Amount', with: 123.45
+        check "I am going to be using direct pay for this transaction"
+        select approver.email_address, from: 'approver_email'
+        fill_in 'Building number', with: Ncr::BUILDING_NUMBERS[0]
+        select Ncr::Organization.all[0], from: 'ncr_work_order_org_code'
+        expect { click_on 'Submit for approval' }.to change { Proposal.count }.from(0).to(1)
+
+        proposal = Proposal.last
+        expect(proposal.public_id).to have_content("FY")
+        expect(page).to have_content("Proposal submitted")
+        expect(current_path).to eq("/proposals/#{proposal.id}")
+
+        expect(proposal.name).to eq("buying stuff")
+        expect(proposal.flow).to eq('linear')
+        work_order = proposal.client_data
+        expect(work_order.client).to eq('ncr')
+        expect(work_order.expense_type).to eq('BA80')
+        expect(work_order.vendor).to eq('ACME')
+        expect(work_order.amount).to eq(123.45)
+        expect(work_order.direct_pay).to eq(true)
+        expect(work_order.building_number).to eq(Ncr::BUILDING_NUMBERS[0])
+        expect(work_order.org_code).to eq(Ncr::Organization.all[0].to_s)
+        expect(work_order.description).to eq('desc content')
+        expect(proposal.requester).to eq(requester)
+        expect(proposal.approvers.map(&:email_address)).to eq(
+          [approver.email_address, Ncr::WorkOrder.ba61_tier1_budget_mailbox])
+      end
+
+      scenario "saves a BA60 Proposal with the attributes" do
+        expect(Dispatcher).to receive(:deliver_new_proposal_emails)
+
+        visit '/ncr/work_orders/new'
+        fill_in 'Project title', with: "blue shells"
+        fill_in 'Description', with: "desc content"
+        choose 'BA60'
+        fill_in 'Vendor', with: 'Yoshi'
+        fill_in 'Amount', with: 123.45
+        select approver.email_address, from: "Approving official's email address"
+        fill_in 'Building number', with: Ncr::BUILDING_NUMBERS[0]
+        select Ncr::Organization.all[0], from: 'ncr_work_order_org_code'
+        expect { click_on 'Submit for approval' }.to change { Proposal.count }.from(0).to(1)
+
+        proposal = Proposal.last
+        work_order = proposal.client_data
+        expect(work_order.expense_type).to eq('BA60')
+        expect(proposal.approvers.map(&:email_address)).to eq [
+          approver.email_address,
+          Ncr::WorkOrder.ba61_tier1_budget_mailbox,
+          Ncr::WorkOrder.ba61_tier2_budget_mailbox
+        ]
+      end
+
+      scenario "shows the radio button" do
+        visit '/ncr/work_orders/new'
+        expect(page).to have_content('BA60')
+        expect(page).to have_content('BA61')
+        expect(page).to have_content('BA80')
+      end
+    end
+
+    scenario "shows hint text for amount field", :js do
+      visit '/ncr/work_orders/new'
+      focus_field 'ncr_work_order_amount'
+      expect(page).to have_content('$3,500 for supplies')
+      expect(page).to have_content('$2,500 for services')
+      expect(page).to have_content('$2,000 for construction')
+    end
+
+    scenario "defaults to no approver if there was no previous request" do
+      visit '/ncr/work_orders/new'
+      expect(find_field("Approving official's email address").value).to eq('')
+    end
+
+    scenario "defaults to the approver from the last request" do
+      proposal = create(:proposal, :with_serial_approvers, requester: requester)
+      visit '/ncr/work_orders/new'
+      expect(find_field("Approving official's email address").value).to eq(
+        proposal.approvers.first.email_address)
+    end
+
+    scenario "requires a project_title" do
+      visit '/ncr/work_orders/new'
+      expect { click_on 'Submit for approval' }.to_not change { Proposal.count }
+      expect(page).to have_content("Project title can't be blank")
+    end
+
+    scenario "doesn't show the budget fields" do
+      visit '/ncr/work_orders/new'
+      expect(page).to_not have_field('CL number')
+      expect(page).to_not have_field('Function code')
+      expect(page).to_not have_field('SOC code')
+    end
+
+    scenario "doesn't save when the amount is too high" do
+      visit '/ncr/work_orders/new'
+      fill_in 'Project title', with: "buying stuff"
+      choose 'BA80'
+      fill_in 'Vendor', with: 'ACME'
+      fill_in 'Amount', with: 10_000
+
+      expect { click_on 'Submit for approval' }.to_not change { Proposal.count }
+      expect(current_path).to eq('/ncr/work_orders')
+      expect(page).to have_content("Amount must be less than or equal to $")
+      # keeps the form values
+      expect(find_field('Amount').value).to eq('10000')
+    end
+
+    scenario "preserve form values on submission error" do
+      # make sure we have an existing work order so vendor dropdown is populated.
+      work_order = create(:ncr_work_order, :with_approvers)
+
+      expect(Proposal.count).to eq(1)
+      expect(ncr_helper_class.vendor_options).to eq([work_order.vendor])
+
+      visit '/ncr/work_orders/new'
+      fill_in 'Project title', with: "buying stuff"
+      choose 'BA80'
+      fill_in 'Vendor', with: 'ACME'
+      fill_in 'Amount', with: 10_000
+
+      expect { click_on 'Submit for approval' }.to_not change { Proposal.count }
+
+      # options do not change unless we pass in new explicitly, which the form should.
+      expect(ncr_helper_class.vendor_options('zzbar')).to eq([work_order.vendor, 'zzbar'])
+
+      # keeps the form values
+      expect(find_field('Amount').value).to eq('10000')
+      expect(find_field('Vendor').value).to eq('ACME')
+      # since vendor is populated from local json array, make sure the array contains the submitted string,
+      # even though db does not yet contain it.
+      expect(JSON.parse(find_field('Vendor')['data-initial'])).to eq(['ACME', work_order.vendor])
+    end
+
+    scenario "includes has overwritten field names" do
+      visit '/ncr/work_orders/new'
+      fill_in 'Project title', with: "buying stuff"
+      choose 'BA80'
+      fill_in 'RWA Number', with: 'B9876543'
+      fill_in 'Vendor', with: 'ACME'
+      fill_in 'Amount', with: 123.45
+      select approver.email_address, from: 'approver_email'
+      fill_in 'Building number', with: Ncr::BUILDING_NUMBERS[0]
+      select Ncr::Organization.all[0], from: 'ncr_work_order_org_code'
+      click_on 'Submit for approval'
+      expect(current_path).to eq("/proposals/#{Proposal.last.id}")
+      expect(page).to have_content("RWA Number")
+    end
+
+    scenario "hides fields based on expense", :js do
+      visit '/ncr/work_orders/new'
+      expect(page).to have_no_field("RWA Number")
+      expect(page).to have_no_field("Work Order")
+      expect(page).to have_no_field("emergency")
+      choose 'BA61'
+      expect(page).to have_no_field("RWA Number")
+      expect(page).to have_no_field("Work Order")
+      expect(page).to have_field("emergency")
+      expect(find_field("emergency", visible: false)).to be_visible
+      choose 'BA80'
+      expect(page).to have_field("RWA Number")
+      expect(page).to have_field("Work Order")
+      expect(page).to have_no_field("emergency")
+      expect(find_field("RWA Number")).to be_visible
+    end
+
+    scenario "allows attachments to be added during intake without JS" do
+      visit '/ncr/work_orders/new'
+      expect(page).to have_content("Attachments")
+      expect(page).not_to have_selector(".js-am-minus")
+      expect(page).not_to have_selector(".js-am-plus")
+      expect(page).to have_selector("input[type=file]", count: 10)
+    end
+
+    scenario "allows attachments to be added during intake with JS", :js do
+      visit '/ncr/work_orders/new'
+      expect(page).to have_content("Attachments")
+      first_minus = find(".js-am-minus")
+      first_plus = find(".js-am-plus")
+      expect(first_minus).to be_visible
+      expect(first_plus).to be_visible
+      expect(first_minus).to be_disabled
+      expect(find("input[type=file]")[:name]).to eq("attachments[]")
+      first_plus.click # Adds one row
+      expect(page).to have_selector(".js-am-minus", count: 2)
+      expect(page).to have_selector(".js-am-plus", count: 2)
+      expect(page).to have_selector("input[type=file]", count: 2)
+    end
+
+    scenario "includes an initial list of buildings", :js do
+      visit '/ncr/work_orders/new'
+      option = Ncr::BUILDING_NUMBERS.sample
+
+      expect(page).not_to have_selector(".option[data-value='#{option}']")
+
+      find("input[aria-label='Building number']").native.send_keys(option)
+      expect(page).to have_selector("div.option[data-value='#{option}']")
+    end
+
+    scenario "does not include custom buildings initially", :js do
+      visit '/ncr/work_orders/new'
+      find("input[aria-label='Building number']").native.send_keys("BillDing")
+      expect(page).not_to have_selector("div.option[data-value='BillDing']")
+    end
+
+    scenario "includes previously entered buildings, too", :js do
+      create(:ncr_work_order, building_number: "BillDing")
+      visit '/ncr/work_orders/new'
+      find("input[aria-label='Building number']").native.send_keys("BillDing")
+      expect(page).to have_selector("div.option[data-value='BillDing']")
+    end
+
+    context "selected common values on proposal page" do
+      before do
+        visit '/ncr/work_orders/new'
+
+        fill_in 'Project title', with: "buying stuff"
+        fill_in 'Vendor', with: 'ACME'
+        fill_in 'Amount', with: 123.45
+        select approver.email_address, from: 'approver_email'
+        fill_in 'Building number', with: Ncr::BUILDING_NUMBERS[0]
+        select Ncr::Organization.all[0], from: 'ncr_work_order_org_code'
+      end
+
+      scenario "approves emergencies" do
+        choose 'BA61'
+        check "This request was an emergency and I received a verbal Notice to Proceed (NTP)"
+        expect { click_on 'Submit for approval' }.to change { Proposal.count }.from(0).to(1)
+
+        proposal = Proposal.last
+        expect(page).to have_content("Proposal submitted")
+        expect(current_path).to eq("/proposals/#{proposal.id}")
+        expect(page).to have_content("0 of 0 approved")
+
+        expect(proposal.client_data.emergency).to eq(true)
+        expect(proposal.approved?).to eq(true)
+        expect(proposal.approvers).to be_empty
+        expect(proposal.client_data.decorate.current_approver_email_address).to eq(Ncr::WorkOrderDecorator::EMERGENCY_APPROVER_EMAIL)
+        expect(proposal.client_data.decorate.final_approver_email_address).to eq(Ncr::WorkOrderDecorator::EMERGENCY_APPROVER_EMAIL)
+      end
+
+      scenario "does not set emergencies if form type changes" do
+        choose 'BA61'
+        check "This request was an emergency and I received a verbal Notice to Proceed (NTP)"
+        choose 'BA80'
+        fill_in 'RWA Number', with: 'R9876543'
+        expect { click_on 'Submit for approval' }.to change { Proposal.count }.from(0).to(1)
+
+        proposal = Proposal.last
+        expect(page).to have_content("Proposal submitted")
+        expect(current_path).to eq("/proposals/#{proposal.id}")
+
+        expect(proposal.client_data.emergency).to eq(false)
+        expect(proposal.approved?).to eq(false)
+      end
+    end
+
+    it "does not disable the emergency field" do
+      visit '/ncr/work_orders/new'
+      expect(find_field('emergency')).not_to be_disabled
+    end
+  end
+
+  def focus_field(field_id)
+    execute_script "document.getElementById('#{field_id}').scrollIntoView()"
+    execute_script "$('##{field_id}').focus()"
+  end
+end
