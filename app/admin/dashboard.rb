d63f665aac2b2f3bd6b82dc2be6f9767f456fd37
Peter Karman <peter.karman@gsa.gov>
2015-10-26 12:38:50 -0500

    add DelayedJob class and expose delayed_jobs via /admin UI

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,10 +2,22 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+    section "Background Jobs" do
+      now = Time.now.getgm
+      ul do
+        li do
+          jobs = DelayedJob.where('failed_at is not null').count(:id)
+          link_to "#{jobs} failing jobs", admin_jobs_path(q: {failed_at_is_not_null: true}), style: 'color: red'
+        end
+        li do
+          jobs = DelayedJob.where('run_at <= ?', now).count(:id)
+          link_to "#{jobs} late jobs", admin_jobs_path(q: {run_at_lte: now.to_s(:db)}), style: 'color: hsl(40, 100%, 40%)'
+        end
+        li do
+          jobs = DelayedJob.where('run_at >= ?', now).count(:id)
+          link_to "#{jobs} scheduled jobs", admin_jobs_path(q: {run_at_gte: now.to_s(:db)}), style: 'color: green'
+        end
       end
     end
 
---
+++ app/models/delayed_job.rb
@@ -0,0 +1,3 @@
+class DelayedJob < ActiveRecord::Base
+
+end
