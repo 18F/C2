c47dfba23ec224f7c19ffc50c884113bffcd80f6
Aidan Feldman <aidan.feldman@gsa.gov>
2015-10-26 19:30:13 -0400

    remove hard-coding of number of seed users

--- spec/features/roles_conversion_spec.rb
+++ spec/features/roles_conversion_spec.rb
@@ -1,16 +1,16 @@
 describe RolesConversion do
   describe "#ncr_budget_approvers" do
     it "should convert NCR budget approvers" do
-      expect(User.count).to eq(4) # via db/seeds
-      RolesConversion.new.ncr_budget_approvers
-      expect(User.count).to eq(4) # no change (idempotent)
+      expect {
+        RolesConversion.new.ncr_budget_approvers
+      }.to_not change { User.count }
     end
 
     it "should be idempotent based on client_slug+role" do
       with_env_var('NCR_BA61_TIER1_BUDGET_MAILBOX', 'someoneelse@example.com') do
-        expect(User.count).to eq(4) # via db/seeds
-        RolesConversion.new.ncr_budget_approvers
-        expect(User.count).to eq(4) # no change (idempotent)
+        expect {
+          RolesConversion.new.ncr_budget_approvers
+        }.to_not change { User.count }
       end
     end
   end
