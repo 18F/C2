9663140927bf0b22bce7026c439666b6e4ab084b
Peter Karman <peter.karman@gsa.gov>
2015-10-26 13:52:53 -0500

    add test for idempotent slug+role d24b0bae6cf152ebaa24aabb7a20a2b81cd8fa94

--- spec/features/roles_conversion_spec.rb
+++ spec/features/roles_conversion_spec.rb
@@ -5,5 +5,13 @@ describe RolesConversion do
       RolesConversion.new.ncr_budget_approvers
       expect(User.count).to eq(4) # no change (idempotent)
     end
+
+    it "should be idempotent based on client_slug+role" do
+      with_env_var('NCR_BA61_TIER1_BUDGET_MAILBOX', 'someoneelse@example.com') do
+        expect(User.count).to eq(4) # via db/seeds
+        RolesConversion.new.ncr_budget_approvers
+        expect(User.count).to eq(4) # no change (idempotent)
+      end
+    end
   end
 end
