f51c6364670568362458ea159feea4fd40b17c07
Aidan Feldman <aidan.feldman@gsa.gov>
2015-11-02 14:37:22 -0500

    remove use of instance variables in WorkOrderUpdater

--- lib/ncr/work_order_updater.rb
+++ lib/ncr/work_order_updater.rb
@@ -1,39 +1,39 @@
 module Ncr
   class WorkOrderUpdater
-    attr_reader :flash
+    attr_reader :flash, :model_changing, :work_order
 
     def initialize(work_order:, flash:, model_changing:)
-      @model_instance = work_order
-      @model_changing = model_changing
       @flash = flash
+      @model_changing = !!model_changing
+      @work_order = work_order
     end
 
     def current_user
-      @model_instance.modifier
+      work_order.modifier
     end
 
     def proposal
-      @model_instance.proposal
+      work_order.proposal
     end
 
     def amount_increased?
-      changes = @model_instance.previous_changes
-      changes.key?('amount') && (@model_instance.amount > changes['amount'].first)
+      changes = work_order.previous_changes
+      changes.key?('amount') && (work_order.amount > changes['amount'].first)
     end
 
     def budget_codes_changed?
-      changes = @model_instance.previous_changes
+      changes = work_order.previous_changes
       Ncr::WorkOrder.budget_code_fields.any? do |field|
         changes.key?(field.to_s)
       end
     end
 
     def budget_approver?
-      @model_instance.budget_approvers.include?(current_user)
+      work_order.budget_approvers.include?(current_user)
     end
 
     def requires_budget_reapproval?
-      @model_instance.approved? && (
+      work_order.approved? && (
         self.amount_increased? || (
           self.budget_codes_changed? &&
           !self.budget_approver?
@@ -43,16 +43,16 @@ module Ncr
 
     def reapprove_if_necessary
       if requires_budget_reapproval?
-        @model_instance.restart_budget_approvals
+        work_order.restart_budget_approvals
         flash[:success] = "Successfully modified! This request now needs to be re-approved by budget."
       end
     end
 
     def after_update
-      if @model_changing
-        @model_instance.setup_approvals_and_observers
+      if model_changing
+        work_order.setup_approvals_and_observers
         reapprove_if_necessary
-        Dispatcher.on_proposal_update(proposal, @model_instance.modifier)
+        Dispatcher.on_proposal_update(proposal, work_order.modifier)
       end
     end
   end
