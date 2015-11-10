36619c0a28d85490453a80697c946f5906cc35e9
Peter Karman <peter.karman@gsa.gov>
2015-11-10 09:54:56 -0600

    satisfy rubocop

--- lib/vacuum.rb
+++ lib/vacuum.rb
@@ -1,15 +1,10 @@
 class Vacuum
   attr_reader :verbose, :fiscal_year_start
 
-  def initialize(dtim, ok_to_act=false, verbose=false)
+  def initialize(dtim, ok_to_act = false, verbose = false)
     @fiscal_year_start = self.class.fiscal_year_start(dtim)
     @ok_to_act = ok_to_act
     @verbose = verbose
-  end
-
-  def self.fiscal_year_start(dtim)
-    year = (dtim.month >= 10) ? dtim.year : dtim.year - 1
-    DateTime.parse("#{year.to_s}-10-01")
   end
 
   def old_proposals
@@ -24,27 +19,35 @@ class Vacuum
 
   def proposal(proposal)
     if !proposal.requester
-      @verbose and puts "#{proposal.id} <= no Requester defined"
-      if @ok_to_act
-        proposal.destroy
-      end 
+      handle_no_requester(proposal)
     else
-      @verbose and puts "#{proposal.public_id} -> #{proposal.requester.email_address}"
-      if @ok_to_act
-        send_reminder_email(proposal)
-        proposal.cancel!
-      end 
+      handle_cancellation(proposal)
     end
   end
 
   private
 
   def self.fiscal_year_start(dtim)
-    year = (dtim.month >= 10) ? dtim.year : dtim.year - 1 
-    DateTime.parse("#{year.to_s}-10-01")
+    year = (dtim.month >= 10) ? dtim.year : dtim.year - 1
+    Time.zone.parse("#{year}-10-01")
   end
 
   def send_reminder_email(proposal)
     CommunicartMailer.proposal_reminder(proposal).deliver_later
+  end
+
+  def handle_no_requester(proposal)
+    @verbose and STDERR.puts "#{proposal.id} <= no Requester defined"
+    if @ok_to_act
+      proposal.destroy
+    end
+  end
+
+  def handle_cancellation(proposal)
+    @verbose and STDERR.puts "#{proposal.public_id} -> #{proposal.requester.email_address}"
+    if @ok_to_act
+      send_reminder_email(proposal)
+      proposal.cancel!
+    end
   end
 end
