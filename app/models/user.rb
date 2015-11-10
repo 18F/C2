2badd7a3a7f769e2e45dc0065de12a15e3721952
Peter Karman <peter.karman@gsa.gov>
2015-11-10 15:19:05 -0600

    do not overwrite first/last with oauth payload unless they are locally blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -107,8 +107,11 @@ class User < ActiveRecord::Base
   def self.from_oauth_hash(auth_hash)
     user_data = auth_hash.extra.raw_info.to_hash
     user = self.for_email(user_data['email'])
-    if user_data['first_name'].present? && user_data['last_name'].present?
-      user.update_attributes(first_name: user_data['first_name'], last_name: user_data['last_name'])
+    if user_data['first_name'].present? && first_name.blank?
+      user.update_attributes(first_name: user_data['first_name'])
+    end
+    if user_data['last_name'].present? && last_name.blank?
+      user.update_attributes(last_name: user_data['last_name'])
     end
     user
   end
