9ca38dc01bf02ec593dc97faae8f32bb631fae62
Peter Karman <peter.karman@gsa.gov>
2015-11-10 14:43:18 -0600

    add form labels, feature test for /me editing

---
+++ spec/features/edit_me_spec.rb
@@ -0,0 +1,11 @@
+describe "User can manage their own profile" do
+  it "allows editing of name" do
+    user = create(:user)
+    login_as(user)
+    visit "/me"
+    fill_in :first_name, with: "Some"
+    fill_in :last_name, with: "Body"
+    click_button "Update profile"
+    expect(page).to have_content("Welcome, Some Body <#{user.email_address}>")
+  end
+end
