c8e77b3cf9ca3ee24e5bffa575ec845f9ac67c19
Peter Karman <peter.karman@gsa.gov>
2015-11-16 14:44:59 -0600

    make actions explicit whitelist rather than blacklist

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  actions :all, except: [:destroy]
+  actions :index, :show, :create, :edit, :update
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
