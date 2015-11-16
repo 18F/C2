f28c25ade32eb12a8e1d8a2df7f2c219136d8ac1
Peter Karman <peter.karman@gsa.gov>
2015-11-16 11:53:04 -0600

    newer hash syntax

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  actions :all, :except => [:destroy]
+  actions :all, except: [:destroy]
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
