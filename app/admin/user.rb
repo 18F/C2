00e49e1e4facf0e854780789ffa910d34d1bdbab
Peter Karman <peter.karman@gsa.gov>
2015-11-16 11:44:37 -0600

    disable Delete User action in /admin/users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register User do
+  actions :all, :except => [:destroy]
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   permit_params :active, :first_name, :last_name, :email_address, :client_slug, role_ids: []
