63f137432b56f5fbfbdb5941a93552f00086cfca
Jessie A Young <jessica.young@gsa.gov>
2015-11-17 15:44:06 -0800

    [Delivers #107212902] Add delegates via admin
    * Admin can add delegates
    * Admin can also delete delegates
    * To test, visit '/admin/approval_delegates'
    * Story: https://www.pivotaltracker.com/story/show/107212902

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  actions :index, :show, :create, :edit, :update
+  actions :index, :show, :new, :create, :edit, :update
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
--- app/models/approval_delegate.rb
+++ app/models/approval_delegate.rb
@@ -1,9 +1,19 @@
 class ApprovalDelegate < ActiveRecord::Base
-  has_paper_trail class_name: 'C2Version'
+  has_paper_trail class_name: "C2Version"
 
-  belongs_to :assignee, class_name: 'User', foreign_key: 'assignee_id'
-  belongs_to :assigner, class_name: 'User', foreign_key: 'assigner_id'
+  belongs_to :assignee, class_name: "User", foreign_key: "assignee_id"
+  belongs_to :assigner, class_name: "User", foreign_key: "assigner_id"
 
-  validates :assignee_id, presence: true, uniqueness: {scope: :assigner_id}
-  validates :assigner_id, presence: true
+  validates :assigner, presence: true
+  validates :assignee, presence: true
+  validates :assignee_id, uniqueness: { scope: :assigner_id }
+  validate :assigner_and_assignee_are_different_users
+
+  private
+
+  def assigner_and_assignee_are_different_users
+    if assigner == assignee
+      errors.add(:assingee, "cannot be same user as assigner")
+    end
+  end
 end
