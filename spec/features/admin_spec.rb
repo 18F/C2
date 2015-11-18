fe811db8ab1d944465723a25742e26aa8a4bfcb9
Jessie A Young <jessica.young@gsa.gov>
2015-11-18 10:15:42 -0800

    Add feature spec for ApprovalDelegate config

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -8,4 +8,17 @@ describe "admin" do
 
     expect(page).to_not have_content("Delete User")
   end
+
+  it "does not allow editing of approval delegates" do
+    user = create(:user)
+    user.add_role("admin")
+    other_user = create(:user)
+    approval_delegate = create(:approval_delegate, assigner: user, assignee: other_user)
+
+    login_as(user)
+    visit edit_admin_user_path(user)
+    visit admin_approval_delegate_path(approval_delegate)
+
+    expect(page).not_to have_content("Edit Approval Delegate")
+  end
 end
