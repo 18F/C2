29077e93f6b11aafb8fe25e079966b3a791cf9cb
Peter Karman <peter.karman@gsa.gov>
2015-12-02 16:07:06 -0600

    rubocop, you task master

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -4,7 +4,7 @@ class SummaryController < ApplicationController
   def index
     client_namespace = current_user.client_slug.titleize
     @client_summarizer = ClientSummarizer.new(
-      client_namespace: client_namespace, 
+      client_namespace: client_namespace,
       fiscal_year: params[:fiscal_year]
     )
     @summary = @client_summarizer.run
@@ -13,16 +13,16 @@ class SummaryController < ApplicationController
   private
 
   def authorize
-    if !has_adminish_role? || !has_client_slug?
+    if !adminish_role? || !client_slug?
       render "authorization_error", status: 403
     end
   end
 
-  def has_adminish_role?
+  def adminish_role?
     current_user.admin? || current_user.client_admin?
   end
 
-  def has_client_slug? 
+  def client_slug?
     current_user.client_slug.present?
   end
 end
