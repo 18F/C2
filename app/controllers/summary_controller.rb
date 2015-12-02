b861557502c28b84f9019b5586728a62c75e48b9
Peter Karman <peter.karman@gsa.gov>
2015-12-02 10:53:15 -0600

    add tests for /summary

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -1,19 +1,28 @@
 class SummaryController < ApplicationController
-  def authz_check
-    unless current_user.admin? or current_user.client_admin?
-      fail "Must be admin to view summary"
-    end
-    unless current_user.client_slug.present?
-      fail "Must have client_slug set to view summary"
-    end
-  end
+  before_action :authorize
 
   def index
     client_namespace = current_user.client_slug.titleize
     @client_summarizer = ClientSummarizer.new(
       client_namespace: client_namespace, 
       fiscal_year: params[:fiscal_year]
-    )
+    )   
     @summary = @client_summarizer.run
+  end
+
+  private
+
+  def authorize
+    if !has_adminish_role || !has_client_slug
+      render "authorization_error", status: 403
+    end
+  end
+
+  def has_adminish_role
+    current_user.admin? || current_user.client_admin?
+  end
+
+  def has_client_slug 
+    current_user.client_slug.present?
   end
 end
---
+++ spec/controllers/summary_controller_spec.rb
@@ -0,0 +1,29 @@
+describe SummaryController do
+  describe "#index" do
+    it "requires client_admin or admin role, with client_slug" do
+      user = create(:user)
+      login_as(user)
+      get :index
+      expect(response.status).to eq(403)
+    end
+
+    it "requires client_slug" do
+      admin_user = create(:user, :admin)
+      login_as(admin_user)
+      get :index
+      expect(response.status).to eq(403)
+    end
+
+    it "requires admin or client_admin role" do
+      admin_user = create(:user, :admin, client_slug: "ncr")
+      login_as(admin_user)
+      get :index
+      expect(response.status).to eq(200)
+
+      client_admin_user = create(:user, :client_admin, client_slug: "ncr")
+      login_as(client_admin_user)
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+end
