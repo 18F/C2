46ef1167c471aebbca8e76929387a5d434d77697
Peter Karman <peter.karman@gsa.gov>
2015-12-02 10:54:38 -0600

    whitespace and rubyish method names

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -6,23 +6,23 @@ class SummaryController < ApplicationController
     @client_summarizer = ClientSummarizer.new(
       client_namespace: client_namespace, 
       fiscal_year: params[:fiscal_year]
-    )   
+    )
     @summary = @client_summarizer.run
   end
 
   private
 
   def authorize
-    if !has_adminish_role || !has_client_slug
+    if !has_adminish_role? || !has_client_slug?
       render "authorization_error", status: 403
     end
   end
 
-  def has_adminish_role
+  def has_adminish_role?
     current_user.admin? || current_user.client_admin?
   end
 
-  def has_client_slug 
+  def has_client_slug? 
     current_user.client_slug.present?
   end
 end
