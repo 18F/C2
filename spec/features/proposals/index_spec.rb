f5a68f215823e8ff28544e16acb1b34fc31da34e
Jessie A. Young <jessie.a.young@gmail.com>
2015-12-03 09:32:49 -0800

    Fix failing spec

--- spec/features/proposals/index_spec.rb
+++ spec/features/proposals/index_spec.rb
@@ -2,17 +2,18 @@ feature "Proposals index" do
   include ProposalTableSpecHelper
 
   scenario "filters pending proposals according to current_user" do
+    user = create(:user)
     _reviewable_proposals = create_list(:proposal, 2, observer: user)
     _pending_proposals = create_list(:proposal, 2, :with_approver, approver_user: user)
     _cancelled = create_list(:proposal, 2, status: "cancelled", observer: user)
 
     login_as(user)
     visit proposals_path
+    tables = page.all('.tabular-data')
 
-    expect(tables.size).to eq(3)
-    expect(reviewable_proposals_section.text).to have_content('Please review')
-    expect(pending_proposals_section.text).to have_content('Waiting for review')
-    expect(cancelled_proposals_section.text).to have_content('Cancelled')
+    expect(tables[0].text).to have_content('Please review')
+    expect(tables[1].text).to have_content('Waiting for review')
+    expect(tables[2].text).to have_content('Cancelled')
   end
 
   scenario "defaults to sorted by created date" do
