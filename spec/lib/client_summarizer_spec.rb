5a2459ded744ac44ed62097389666af923abcf39
Peter Karman <peter.karman@gsa.gov>
2015-12-03 10:03:55 -0600

    add test for gsa18f procurement

--- spec/lib/client_summarizer_spec.rb
+++ spec/lib/client_summarizer_spec.rb
@@ -9,4 +9,15 @@ describe ClientSummarizer do
       expect(summary.subtotal(:pending)).to eq(123)
     end
   end
+
+  describe "Gsa18f::Procurement" do
+    it "builds summary" do
+      procurement = create(:gsa18f_procurement, cost_per_unit: 18.50, quantity: 20)
+      summarizer = ClientSummarizer.new(client_namespace: "Gsa18f")
+      summary = summarizer.run
+      expect(summary.total).to eq(370)
+      expect(summary.status(:pending)).to eq(1)
+      expect(summary.subtotal(:pending)).to eq(370)
+    end
+  end
 end
