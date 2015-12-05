4ec414a38b18cec1d87f1c2562da0c6ec5cc2541
Henry Dotson <l3iodeez@gmail.com>
2015-12-05 14:49:45 -0500

    Reorder fields in api test.

--- spec/requests/api/ncr/work_orders_spec.rb
+++ spec/requests/api/ncr/work_orders_spec.rb
@@ -28,7 +28,7 @@ describe 'NCR Work Orders API' do
             "name" => work_order.name,
             "not_to_exceed" => work_order.not_to_exceed,
             "observers" => observers,
-            "org_code" =>  work_order.org_code,
+            "organization_code" => work_order.organization_code,
             "proposal" => {
               "created_at" => time_to_json(proposal.created_at),
               "id" => proposal.id,
