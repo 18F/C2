17b8c9787a743a96ecdc842c5cb993242c03ffdf
Peter Karman <peter.karman@gsa.gov>
2015-12-15 09:59:11 -0600

    rename dir rather than creating symlink, so CI will cache correctly

--- build-elasticsearch.sh
+++ build-elasticsearch.sh
@@ -9,6 +9,5 @@ TAR_FILE=elasticsearch-$ES_VERSION.tar.gz
 if [ ! -e elasticsearch/bin/elasticsearch ]; then
   wget http://download.elasticsearch.org/elasticsearch/elasticsearch/$TAR_FILE
   tar xzf $TAR_FILE
-  rm -f elasticsearch
-  ln -s elasticsearch-$ES_VERSION elasticsearch
+  mv elasticsearch-$ES_VERSION elasticsearch
 fi
