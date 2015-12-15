792eba62fcf606bfddfe80f5e8b247302575c66b
Peter Karman <peter.karman@gsa.gov>
2015-12-14 22:19:02 -0600

    add missing model from migration

---
+++ app/models/test/client_request.rb
@@ -0,0 +1,21 @@
+module Test
+  def self.table_name_prefix
+    'test_'
+  end
+
+  class ClientRequest < ActiveRecord::Base
+
+    # must define before include PurchaseCardMixin
+    def self.purchase_amount_column_name
+      :amount
+    end
+
+    include ClientDataMixin
+    include PurchaseCardMixin
+
+    def editable?
+      true
+    end
+
+  end
+end
