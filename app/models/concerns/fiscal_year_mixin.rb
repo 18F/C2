5db1167ffa56b9780b853932a303009aa8043bf1
Jessie A Young <jessica.young@gsa.gov>
2015-12-15 16:30:48 -0800

    Move `fiscal_year` method into mixin
    * Part of moving NCR-specific logic out of Ncr::WorkOrder
    * Also removes duplication
    
    https://trello.com/c/smBumRFX

--- app/models/concerns/fiscal_year_mixin.rb
+++ app/models/concerns/fiscal_year_mixin.rb
@@ -1,6 +1,8 @@
 module FiscalYearMixin
   extend ActiveSupport::Concern
 
+  FISCAL_YEAR_START_MONTH = 10 # 1-based
+
   included do
     def self.which_fiscal_year(year, month)
       if month >= 10
@@ -8,6 +10,12 @@ module FiscalYearMixin
       else
         year
       end
+    end
+
+    def self.for_fiscal_year(year)
+      start_time = Time.zone.local(year - 1, FISCAL_YEAR_START_MONTH, 1)
+      end_time = start_time + 1.year
+      where(created_at: start_time...end_time)
     end
   end
 end
