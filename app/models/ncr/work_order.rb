1576ea65da295cf1272c592fafb9357fcd1af421
Jessie A Young <jessica.young@gsa.gov>
2015-12-15 16:40:34 -0800

    Remove duplicate mailbox methods from Ncr::WorkOrder
    * Same logic lives in `Ncr::Mailboxes`
    * Part of https://trello.com/c/smBumRFX/38-move-non-ncr-specific-logic-out-of-workorders

--- app/models/ncr/work_order.rb
+++ app/models/ncr/work_order.rb
@@ -56,37 +56,11 @@ module Ncr
 
     def self.all_system_approver_emails
       [
-        self.ba61_tier1_budget_mailbox,
-        self.ba61_tier2_budget_mailbox,
-        self.ba80_budget_mailbox,
-        self.ool_ba80_budget_mailbox,
+        Ncr::Mailboxes.ba61_tier1_budget,
+        Ncr::Mailboxes.ba61_tier2_budget,
+        Ncr::Mailboxes.ba80_budget,
+        Ncr::Mailboxes.ool_ba80_budget,
       ]
-    end
-
-    def self.ba61_tier1_budget_mailbox
-      self.approver_with_role("BA61_tier1_budget_approver")
-    end
-
-    def self.ba61_tier2_budget_mailbox
-      self.approver_with_role("BA61_tier2_budget_approver")
-    end
-
-    def self.ba80_budget_mailbox
-      self.approver_with_role("BA80_budget_approver")
-    end
-
-    def self.ool_ba80_budget_mailbox
-      self.approver_with_role("OOL_BA80_budget_approver")
-    end
-
-    def self.approver_with_role(role_name)
-      users = User.with_role(role_name).where(client_slug: "ncr")
-
-      if users.empty?
-        fail "Missing User with role #{role_name} -- did you run rake db:migrate and rake db:seed?"
-      end
-
-      users.first.email_address
     end
 
     def self.relevant_fields(expense_type)
