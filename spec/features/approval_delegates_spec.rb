53952853ca9c50c0afbd9eb6d0ace133c40c24a6
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2015-12-15 16:37:16 -0800

    Reformatted specs, removed let + before

--- spec/features/approval_delegates_spec.rb
+++ spec/features/approval_delegates_spec.rb
@@ -1,20 +1,22 @@
 describe "Approval Delegates" do
-  let(:proposal) { create(:proposal, :with_approver) }
-
   it "allows delegate to approve a proposal" do
+    proposal = create(:proposal, :with_approver)
     delegate = create(:user)
     approver = proposal.approvers.first
     approver.add_delegate(delegate)
     approver.save!
+
     login_as(delegate)
     visit proposal_path(proposal)
     click_on("Approve")
+
     expect(current_path).to eq(proposal_path(proposal))
     expect(page).to have_content("You have approved #{proposal.public_id}")
     expect(page).to have_content(delegate.full_name)
   end
 
   it "delegates can view work order after approval by different delegate" do
+    proposal = create(:proposal, :with_approver)
     delegate = create(:user)
     delegate_two = create(:user)
     approver = proposal.approvers.first
@@ -25,6 +27,7 @@ describe "Approval Delegates" do
     login_as(delegate)
     visit proposal_path(proposal)
     click_on("Approve")
+
     expect(page).to have_content("You have approved #{proposal.public_id}")
 
     login_as(delegate_two)
--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -1,32 +1,34 @@
 describe "commenting" do
-  let(:proposal) { create(:proposal, :with_parallel_approvers) }
-  let(:user)     { proposal.requester }
-
-  before do
-    login_as(user)
-    visit "/proposals/#{proposal.id}"
-  end
-
   it "saves the comment" do
+    proposal = create_and_visit_proposal
+
     submit_comment
     expect(current_path).to eq("/proposals/#{proposal.id}")
+
     proposal.reload
     expect(proposal.comments.map(&:comment_text)).to eq(['foo'])
   end
 
   it "warns if the comment body is empty" do
+    proposal = create_and_visit_proposal
+
     submit_comment("")
+
     expect(current_path).to eq("/proposals/#{proposal.id}")
     expect(page).to have_content("can't be blank")
   end
 
   it "disables attachments if none is selected", js: true do
+    create_and_visit_proposal
+
     expect(find("#add_a_comment").disabled?).to be(true)
     fill_in "comment[comment_text]", with: "foo"
     expect(find("#add_a_comment").disabled?).to be(false)
   end
 
   it "sends an email" do
+    proposal = create_and_visit_proposal
+
     submit_comment
     expect(email_recipients).to eq([
       proposal.approvers.first.email_address,
@@ -35,21 +37,27 @@ describe "commenting" do
   end
 
   describe "when user is not yet an observer" do
-    let(:approver) { proposal.approvers.first }
-    let(:user)     { create(:user) }
-    before do
+    it "adds current user to the observers list" do
+      proposal = create(:proposal, :with_parallel_approvers)
+      approver = proposal.approvers.first
+      user = create(:user)
       approver.add_delegate(user) # so user can see proposal
       login_as(user)
       visit "/proposals/#{proposal.id}"
-    end
 
-    it "adds current user to the observers list" do
       expect(proposal.observers).to_not include(user)
       submit_comment
       proposal.observers(true) # clear cache
       expect(proposal.observers).to include(user)
     end
   end
+end
+
+def create_and_visit_proposal
+  proposal = create(:proposal, :with_parallel_approvers)
+  login_as(proposal.requester)
+  visit "/proposals/#{proposal.id}"
+  proposal
 end
 
 def submit_comment(text = "foo")
