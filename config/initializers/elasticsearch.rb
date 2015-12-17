07da9c7983a37e08320b589e4cde1496fe22b96c
Peter Karman <peter.karman@gsa.gov>
2015-12-17 12:59:12 -0600

    fix symbol-vs-string mistake in setting up url

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -16,11 +16,11 @@ es_client_args = {
 if Rails.env.production?
   vcap = ENV["VCAP_SERVICES"]
   es_config = JSON.parse(vcap)["elasticsearch-new"]
-  es_client_args["url"] = es_config["url"]
+  es_client_args[:url] = es_config["url"]
 elsif Rails.env.test?
-  es_client_args["url"] = "http://localhost:#{(ENV['TEST_CLUSTER_PORT'] || 9250)}"
+  es_client_args[:url] = "http://localhost:#{(ENV['TEST_CLUSTER_PORT'] || 9250)}"
 else
-  es_client_args["url"] = ENV["ES_URL"] || "http://localhost:9200"
+  es_client_args[:url] = ENV["ES_URL"] || "http://localhost:9200"
 end
 
 # optional verbose logging based on env var, regardless of environment.
@@ -35,12 +35,8 @@ if ENV["ES_DEBUG"].to_i > 0
   logger.debug "[#{Time.now.utc.iso8601}] Elasticsearch logging set to DEBUG mode"
 end
 
-# despite documentation to the contrary, this env variable actually seems to control
-# what the underlying Faraday transport client uses, so set it belt+suspenders.
-ENV["ELASTICSEARCH_URL"] = es_client_args["url"]
-
 Elasticsearch::Model.client = Elasticsearch::Client.new(es_client_args)
 
 if ENV["ES_DEBUG"]
-  es_client_args[:logger].debug "[#{Time.now.utc.iso8601}] Using Elasticsearch server #{es_client_args['url']}"
+  es_client_args[:logger].debug "[#{Time.now.utc.iso8601}] Using Elasticsearch server #{es_client_args[:url]}"
 end
