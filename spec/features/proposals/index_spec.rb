6371e87b7bdcf175720e8c3f2818ba8575ee9846
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2015-12-18 14:58:37 -0800

    Change status field in requests table for step type

--- spec/features/proposals/index_spec.rb
+++ spec/features/proposals/index_spec.rb
@@ -20,20 +20,72 @@ feature "Proposals index" do
     user = create(:user)
     proposals = create_list(:proposal, 2, observer: user)
     cancelled = create_list(:proposal, 2, status: "cancelled", observer: user)
+    @page = MyRequestsPage.new
 
     login_as(user)
-    visit proposals_path
+    @page.load
 
-    within(reviewable_proposals_section) do
-      expect(find("th.desc")).to have_content "Submitted"
+    expect(@page.pending.desc_column_header).to have_content "Submitted"
+    expect(@page.cancelled.desc_column_header).to have_content "Submitted"
+
+    expect_order(@page.pending, proposals.reverse)
+    expect_order(@page.cancelled, cancelled.reverse)
+  end
+
+  describe "status field text" do
+    context "when the user is an approver or purchaser" do
+      it "is correct for the user" do
+        user = create(:user)
+        other_user = create(:user)
+        approval_proposal = create_proposal_with_approvers(user, other_user)
+        purchase_proposal = create_proposal_with_approvers(other_user, user)
+        purchase_proposal.individual_steps.first.approve!
+        @page = MyRequestsPage.new
+
+        login_as(user)
+        @page.load
+
+        expect(@page.needing_review.requests[1].public_id_link.text).to eq approval_proposal.public_id
+        expect(@page.needing_review.requests[1].status.text).to eq "Please review"
+        expect(@page.needing_review.requests[0].public_id_link.text).to eq purchase_proposal.public_id
+        expect(@page.needing_review.requests[0].status.text).to eq "Please purchase"
+      end
     end
 
-    expect_order(reviewable_proposals_section, proposals.reverse)
+    context "when the user's request is waiting for approval or purchase" do
+      it "is correct for the user" do
+        user = create(:user)
+        approver = create(:user)
+        purchaser = create(:user)
+        approval_proposal = create_proposal_for_requester_with_approvers(user, approver, purchaser)
+        purchase_proposal = create_proposal_for_requester_with_approvers(user, approver, purchaser)
+        purchase_proposal.individual_steps.first.approve!
+        @page = MyRequestsPage.new
 
-    within(pending_proposals_section) do
-      expect(find("th.desc")).to have_content "Submitted"
+        login_as(user)
+        @page.load
+
+        expect(@page.pending.requests[1].public_id_link.text).to eq approval_proposal.public_id
+        expect(@page.pending.requests[1].status.text).to eq "Waiting for review from: #{approver.full_name}"
+        expect(@page.pending.requests[0].public_id_link.text).to eq purchase_proposal.public_id
+        expect(@page.pending.requests[0].status.text).to eq "Waiting for purchase from: #{purchaser.full_name}"
+      end
     end
+  end
 
-    expect_order(pending_proposals_section, cancelled.reverse)
+  def create_proposal_with_approvers(first_approver, second_approver)
+    proposal = create(:proposal)
+    steps = [
+      create(:approval, user: first_approver),
+      create(:purchase_step, user: second_approver)
+    ]
+    proposal.add_initial_steps(steps)
+    proposal
+  end
+
+  def create_proposal_for_requester_with_approvers(requester, first_approver, second_approver)
+    proposal = create_proposal_with_approvers(first_approver, second_approver)
+    proposal.update(requester: requester)
+    proposal
   end
 end
