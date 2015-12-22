4f5362779247f45fef2d909bb9ba550eec78f458
Peter Karman <peter.karman@gsa.gov>
2015-12-22 12:55:24 -0600

    refactor to disable form on submit

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
   $(".m-search-ui button.search").click(function() {
     var btn = $(this);
-    var termsInput = $('.search-terms');
-    var form = btn.parent().find('form');
-    form.append(termsInput.clone());
+    var form = $('form.adv-search');
     form.submit();
-    termsInput.prop("disabled", true);
+    // IMPORTANT disable *AFTER* submit
+    form.find('fieldset').prop("disabled", true);
     btn.prop("disabled", true);
-    form.find('input').prop("disabled", true);
-    form.find('select').prop("disabled", true);
-    form.find('button').prop("disabled", true);
+  });
+  $('form.adv-search').on("submit", function(e) {
+    var form = $(this);
+    var termsInput = $('.search-terms');
+    if (termsInput.val().length) {
+      var textInput = $('<input type="hidden" name="text">');
+      textInput.val(termsInput.val());
+      form.append(textInput);
+    }
+    termsInput.prop("disabled", true);
+    return true;
   });
   $(".search-terms").keyup(function(e) {
     if (e.keyCode === 13) {
