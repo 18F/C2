fd29c2775576c54588255cf79974aa9643651e5a
Peter Karman <peter.karman@gsa.gov>
2016-02-01 22:15:19 -0600

    add feature test for advanced search

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -24,6 +24,22 @@ describe "searching" do
     expect(page).not_to have_content(proposals.last.name)
   end
 
+  it "provides advanced search", :js do
+    proposals = populate_proposals
+
+    visit proposals_path
+    fill_in "text", with: proposals.first.name
+    dropdown_button = find("button.dropdown-toggle")
+    dropdown_button.trigger("click") # open dropdown
+    fill_in "test_client_request[client_data.amount]", with: proposals.first.client_data.amount
+    dropdown_button.trigger("click") # close dropdown
+    click_button "search-button"
+
+    expect(current_path).to eq query_proposals_path
+    expect(page).to have_content(proposals.first.public_id)
+    expect(page).to have_content("#{proposals.first.name} client_data.amount:(#{proposals.first.client_data.amount})")
+  end
+
   it "populates the search box on the results page", :js do
     visit '/proposals'
     fill_in 'text', with: 'foo'
@@ -40,5 +56,16 @@ describe "searching" do
 
     visit proposals_path
     expect(page).not_to have_button("Search")
+  end
+
+  def populate_proposals
+    proposals = 2.times.map do |i|
+      wo = create(:test_client_request, project_title: "Work Order #{i}")
+      wo.proposal.update(requester: user)
+      wo.proposal.reindex
+      wo.proposal
+    end
+    Proposal.__elasticsearch__.refresh_index!
+    proposals
   end
 end
