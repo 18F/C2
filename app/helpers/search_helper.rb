e2a0ad59a95d5e40d35af8730f2d3d0bf65f14ac
Peter Karman <peter.karman@gsa.gov>
2016-02-02 14:14:35 -0600

    only show Expense Type on adv search if some options are defined; allow for any expense type by default

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -9,6 +9,6 @@ module SearchHelper
   end
 
   def proposal_expense_type_options(client_model, selected_value)
-    options_for_select(client_model.expense_type_options, selected_value)
+    options_for_select(client_model.expense_type_options.unshift(['Any type', '*']), selected_value)
   end
 end
