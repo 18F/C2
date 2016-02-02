6cfa4a611dc1cd44ad238ec06df48b9a11507a0c
Peter Karman <peter.karman@gsa.gov>
2016-02-02 17:08:30 -0600

    zap redundant line

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require "shoulda/matchers"
-require "elasticsearch/extensions/test/cluster"
 ActiveRecord::Migration.maintain_test_schema!
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
