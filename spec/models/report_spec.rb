80f8d75ec343fcb82fc78265f07b643e53e97be9
Peter Karman <peter.karman@gsa.gov>
2016-02-02 21:19:58 -0600

    make Report attrs explicit in spec, rather than relying on Factory defaults

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,21 +1,24 @@
 describe Report do
   it "#client_query" do
-    report = create(:report)
+    query = { test_client_request: { "client_data.amount" => "<123" } }.to_json
+    report = create(:report, query: query)
     expect(report.client_query.to_s).to eq "client_data.amount:<123"
   end
 
   it "#text_query" do
-    report = create(:report)
+    report = create(:report, query: { text: "something" }.to_json)
     expect(report.text_query).to eq "something"
   end
 
   it "#query_string" do
-    report = create(:report)
+    query = { text: "something", test_client_request: { "client_data.amount" => "<123" } }.to_json
+    report = create(:report, query: query)
     expect(report.query_string).to eq "(something) AND (client_data.amount:<123)"
   end
 
   it "#humanized_query" do
-    report = create(:report)
+    query = { humanized: "(something) AND (Amount:<123)" }.to_json
+    report = create(:report, query: query)
     expect(report.humanized_query).to eq "(something) AND (Amount:<123)"
   end
 
