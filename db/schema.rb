fd675dca25ce5b1e55966fbbc499be7178f1e5d6
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-02-04 16:04:51 -0500

    Run migration for visit_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204204024) do
+ActiveRecord::Schema.define(version: 20160204205859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20160204204024) do
     t.integer  "user_id"
     t.integer  "proposal_id"
     t.boolean  "update_comment"
+    t.integer  "visit_id"
   end
 
   add_index "comments", ["proposal_id"], name: "index_comments_on_proposal_id", using: :btree
@@ -155,6 +156,7 @@ ActiveRecord::Schema.define(version: 20160204204024) do
     t.string   "client_data_type", limit: 255
     t.integer  "requester_id"
     t.string   "public_id",        limit: 255
+    t.integer  "visit_id"
   end
 
   add_index "proposals", ["client_data_id", "client_data_type"], name: "index_proposals_on_client_data_id_and_client_data_type", using: :btree
