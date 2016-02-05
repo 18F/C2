09d17cd690e2ba5e71e480bc41599da1c26dffbb
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-02-05 11:08:35 -0500

    Update cop requests again

--- config/initializers/ahoy_email.rb
+++ config/initializers/ahoy_email.rb
@@ -1,10 +1,12 @@
 class EmailSubscriber
-  # Use open/click to run actions on email open/click
+  # Use open to run actions on email open
   def open(event)
     # :message and :controller keys
     ahoy = event[:controller].ahoy
     ahoy.track "Email opened", message_id: event[:message].id
   end
+
+  # Use click to run actions on email click
   def click(event)
     # same keys as above, plus :url
     ahoy = event[:controller].ahoy
