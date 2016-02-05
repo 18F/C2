fa5bcbfd6befe6a2aacaf3b74e0a824759bd40b6
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-02-05 16:46:36 -0500

    Changes for CodeClimate

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,10 +1,12 @@
-class Ahoy::Store < Ahoy::Stores::BaseStore
-  def track_visit(options)
-  end
+module Ahoy
+  class Store < Ahoy::Stores::BaseStore
+    def track_visit(options)
+    end
 
-  def track_event(name, properties, options)
-  end
+    def track_event(name, properties, options)
+    end
 
-  def current_visit
+    def current_visit
+    end
   end
 end
