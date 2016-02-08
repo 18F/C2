f50eb3d2f4f0d0a2d55cb866d2479d90aa0c3549
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-02-08 13:59:55 -0500

    Robocopping the specs

--- spec/controllers/ahoy/visits_controller.rb
+++ spec/controllers/ahoy/visits_controller.rb
@@ -1,4 +1,4 @@
-describe Ahoy::VisitsController, :type => :controller do
+describe Ahoy::VisitsController, type: :controller do
   routes { Ahoy::Engine.routes }
 
   it "creates visits" do
@@ -12,7 +12,7 @@ describe Ahoy::VisitsController, :type => :controller do
   end
 
   it "renders JSON hash with visitor_id and visit_id" do
-    post :create, :format => :json
+    post :create, format: :json
     response_hash = ActiveSupport::JSON.decode(response.body)
     expect(response_hash).to have_key("visit_id")
     expect(response_hash).to have_key("visitor_id")
