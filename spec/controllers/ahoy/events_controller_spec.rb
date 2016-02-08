fdde3a6a91eb834654cfbb7714e099bc1c527a9a
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-02-08 13:59:55 -0500

    Robocopping the specs

--- spec/controllers/ahoy/events_controller_spec.rb
+++ spec/controllers/ahoy/events_controller_spec.rb
@@ -1,12 +1,12 @@
-describe Ahoy::EventsController, :type => :controller do
+describe Ahoy::EventsController, type: :controller do
   routes { Ahoy::Engine.routes }
 
   it "creates event with other attr" do
-    time = (Time.now - 30).utc.change(:usec => 0)
-    post :create, :id => "11111111-2222-3333-4444-555555555555",
-      :name => "$submit",
-      :properties => {"bar" => "baz"},
-      :time => time
+    time = (Time.now - 30).utc.change(usec: 0)
+    post :create, id: "11111111-2222-3333-4444-555555555555",
+      name: "$submit",
+      properties: {"bar" => "baz"},
+      time: time
     event = Ahoy::Event.last
     expect(event.time).to eq(time)
     expect(event.id).to eq("11111111-2222-3333-4444-555555555555")
@@ -16,8 +16,8 @@ describe Ahoy::EventsController, :type => :controller do
 
   it "creates event with passed name via json request" do
     json = [
-      { :name => "JSON1", :properties => { "buzz" => "bazz"} },
-      { :name => "JSON2", :properties => { "fuzz" => "fazz"} }
+      { name: "JSON1", properties: { "buzz" => "bazz"} },
+      { name: "JSON2", properties: { "fuzz" => "fazz"} }
     ].to_json
     post :create, json, :format => :json
     last_two_events = Ahoy::Event.order('time DESC').limit(2)
@@ -26,9 +26,9 @@ describe Ahoy::EventsController, :type => :controller do
   end
 
   it "renders an empty JSON response" do
-    post :create, :id => "11111111-2222-3333-4444-555555555555",
-      :name => "$submit",
-      :properties => {"bar" => "baz"}
+    post :create, id: "11111111-2222-3333-4444-555555555555",
+      name: "$submit",
+      properties: {"bar" => "baz"}
     expect(ActiveSupport::JSON.decode(response.body)).to eq({})
   end
 end
