30d05b815ddaf85fd62c80a23f627efea1ccdec4
Jessie A. Young <jessie.a.young@gmail.com>
2016-02-11 12:45:44 -0800

    Add code to remove orphan client data

--- spec/db/chores/proposal_cleaner_spec.rb
+++ spec/db/chores/proposal_cleaner_spec.rb
@@ -10,5 +10,23 @@ describe ProposalCleaner do
         ProposalCleaner.new.run
       }.to change { Proposal.count }.from(2).to(1)
     end
+
+    it "removes client data without a proposal" do
+      _ncr_with_proposal = create(:ncr_work_order)
+      ncr_without_proposal = build(:ncr_work_order, proposal: nil)
+      ncr_without_proposal.save(validate: false)
+
+      expect {
+        ProposalCleaner.new.run
+      }.to change { Ncr::WorkOrder.count }.from(2).to(1)
+
+      _18f_with_proposal = create(:gsa18f_procurement)
+      gsa_without_proposal = build(:gsa18f_procurement, proposal: nil)
+      gsa_without_proposal.save(validate: false)
+
+      expect {
+        ProposalCleaner.new.run
+      }.to change { Gsa18f::Procurement.count }.from(2).to(1)
+    end
   end
 end
