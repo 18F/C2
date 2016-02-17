d951a7a7a814eab31c148cb40d49aef6fce833c7
Jessie A Young <jessica.young@gsa.gov>
2016-02-17 15:39:07 -0800

    Make code climate happy

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -38,9 +38,7 @@ module MailerHelper
   end
 
   def observer_text(observation, reason = nil)
-    text = t(
-      "mailer.observer_mailer.on_observer_added.body"
-     )
+    text = t("mailer.observer_mailer.on_observer_added.body")
     add_author(text, observation.created_by)
     add_reason(text, reason)
     text + "."
