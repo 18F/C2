fb7d4235716cf33e8cbe6d8a1d493167fd022988
Jessie A Young <jessica.young@gsa.gov>
2016-02-24 15:51:26 -0800

    Update styles for "approver removed" email
    * https://trello.com/c/uWQFAloH/135-email-conversion-removal-as-approver

--- app/mailers/approval_mailer.rb
+++ app/mailers/approval_mailer.rb
@@ -1,5 +1,5 @@
 class ApprovalMailer < ApplicationMailer
-  layout "mailer"
+  layout "basic"
   add_template_helper ValueHelper
 
   def approval_reply_received_email(step)
@@ -11,5 +11,23 @@ class ApprovalMailer < ApplicationMailer
       to_email: proposal.requester.email_address,
       proposal: proposal
     )
+  end
+
+  def approver_removed(to_email, proposal)
+    @proposal = proposal.decorate
+    assign_threading_headers(@proposal)
+
+    mail(
+      to: to_email,
+      subject: subject(@proposal),
+      from: user_email_with_name(@proposal.requester),
+      reply_to: reply_email(@proposal)
+    )
+  end
+
+  private
+
+  def subject(proposal)
+    "Request #{proposal.public_id}: #{proposal.name}"
   end
 end
