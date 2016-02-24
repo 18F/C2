62f0b44150ebd8fe3d491c33225ed7d7e14fbd1a
Jessie A Young <jessica.young@gsa.gov>
2016-02-24 14:49:41 -0800

    Indicate option args for instance methods

--- app/services/observation_creator.rb
+++ app/services/observation_creator.rb
@@ -56,7 +56,7 @@ class ObservationCreator
   end
 
   def send_observation_added_email
-    DispatchFinder.new(proposal).on_observer_added(observation, reason)
+    DispatchFinder.run(proposal).on_observer_added(observation, reason)
   end
 
   def observer_role
