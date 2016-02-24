790d3d213bc93cee65ee61a10d8676376d75f385
Jessie A Young <jessica.young@gsa.gov>
2016-02-24 10:28:31 -0800

    Refactor Dispatcher classes
    * Make them easier to read and understand

--- app/mailers/approval_mailer.rb
+++ app/mailers/approval_mailer.rb
@@ -2,12 +2,12 @@ class ApprovalMailer < ApplicationMailer
   layout "mailer"
   add_template_helper ValueHelper
 
-  def approval_reply_received_email(approval)
-    proposal = approval.proposal.reload
-    @step = approval
+  def approval_reply_received_email(step)
+    proposal = step.proposal.reload
+    @step = step
 
     send_proposal_email(
-      from_email: user_email_with_name(approval.user),
+      from_email: user_email_with_name(step.user),
       to_email: proposal.requester.email_address,
       proposal: proposal
     )
