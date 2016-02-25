c5dc7d77e15f551e106511f8c9b9bfe1823e1135
Peter Karman <peter.karman@gsa.gov>
2016-02-25 11:12:46 -0600

    wrap actions that touch the ES server in es_execute_with_retries

--- spec/features/return_to_spec.rb
+++ spec/features/return_to_spec.rb
@@ -3,37 +3,45 @@ describe "the return_to url option", elasticsearch: true do
 
   it 'defaults to the proposals listing' do
     login_as(create(:user))
-    visit query_proposals_path(text: "test")
-    expect(page).to have_content('Back to main portal')
-    click_on('Back to main portal')
-    expect(current_path).to eq('/proposals')
+    es_execute_with_retries 3 do
+      visit query_proposals_path(text: "test")
+      expect(page).to have_content('Back to main portal')
+      click_on('Back to main portal')
+      expect(current_path).to eq('/proposals')
+    end
   end
 
   let(:return_to) {make_return_to('nnn', '/somewhere_else')}
 
   it 'changes the link when params are correct' do
     login_as(create(:user))
-    visit query_proposals_path(return_to: return_to, text: "test")
-    expect(page).not_to have_content('Back to main portal')
-    expect(page).to have_content(return_to[:name])
-    expect(find_link(return_to[:name])[:href]).to eq(return_to[:path])
+    es_execute_with_retries 3 do
+      visit query_proposals_path(return_to: return_to, text: "test")
+      expect(page).not_to have_content('Back to main portal')
+      expect(page).to have_content(return_to[:name])
+      expect(find_link(return_to[:name])[:href]).to eq(return_to[:path])
+    end
   end
 
   it 'send back to main if not a valid sig (name)' do
     login_as(create(:user))
     different_name = return_to.merge(name: 'other-key-here')
-    visit query_proposals_path(return_to: different_name, text: "test")
-    expect(page).to have_content('Back to main portal')
-    expect(page).not_to have_content(return_to[:name])
-    expect(page).not_to have_content('other-key-here')
+    es_execute_with_retries 3 do
+      visit query_proposals_path(return_to: different_name, text: "test")
+      expect(page).to have_content('Back to main portal')
+      expect(page).not_to have_content(return_to[:name])
+      expect(page).not_to have_content('other-key-here')
+    end
   end
 
   it 'send back to main if not a valid sig (name)' do
     login_as(create(:user))
     different_path = return_to.merge(path: 'other-key-here')
-    visit query_proposals_path(return_to: different_path, text: "test")
-    expect(page).to have_content('Back to main portal')
-    expect(page).not_to have_content(return_to[:name])
+    es_execute_with_retries 3 do
+      visit query_proposals_path(return_to: different_path, text: "test")
+      expect(page).to have_content('Back to main portal')
+      expect(page).not_to have_content(return_to[:name])
+    end
   end
 
   it "persists the original request URL over login and redirects after" do
