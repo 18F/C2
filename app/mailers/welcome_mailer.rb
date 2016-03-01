219231e46dd5bbed5727d52fb4de9c0b3ad7ebe8
Peter Karman <peter.karman@gsa.gov>
2016-03-01 10:30:09 -0600

    zap unneeded vestigal line

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,8 +2,6 @@ class WelcomeMailer < ApplicationMailer
   layout "basic"
 
   def welcome_notification(user)
-    to_email = user.email_address
-
     mail(
       to: email_with_name(user.email_address, user.full_name),
       subject: "Welcome to C2!",
