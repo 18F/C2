d8142475f9c8b9347b4c9c4435a702e7d0ad5060
Peter Karman <peter.karman@gsa.gov>
2016-03-01 10:27:30 -0600

    add welcome mailer

---
+++ spec/mailers/welcome_mailer_spec.rb
@@ -0,0 +1,30 @@
+describe WelcomeMailer do
+  include Rails.application.routes.url_helpers
+
+  describe "#welcome_notification" do
+    it "includes the welcome text" do
+      user = create(:user)
+
+      mail = WelcomeMailer.welcome_notification(user)
+
+      linebreaker = "=\r\n"
+      body_no_newlines = mail.body.encoded.gsub(linebreaker, "")
+
+      expect(body_no_newlines).to include(
+        I18n.t("mailer.welcome_mailer.welcome_notification.header")
+      )
+      expect(body_no_newlines).to include(
+        I18n.t("mailer.welcome_mailer.welcome_notification.para1")
+      )
+      expect(body_no_newlines).to include(
+        I18n.t("mailer.welcome_mailer.welcome_notification.para2", help_url: help_url('index'))
+      )
+      expect(body_no_newlines).to include(
+        I18n.t("mailer.welcome_mailer.welcome_notification.para3", feedback_url: feedback_url)
+      )
+      expect(body_no_newlines).to include(
+        I18n.t("mailer.welcome_mailer.welcome_notification.signature")
+      )
+    end
+  end
+end
