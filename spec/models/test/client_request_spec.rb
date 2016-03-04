7d49218f2dcb89e57df7eab48302f3495c304a65
Peter Karman <peter.karman@gsa.gov>
2016-03-04 12:05:26 -0600

    add test for automatic serialization of associations (failing, depends on #1007)

--- spec/models/test/client_request_spec.rb
+++ spec/models/test/client_request_spec.rb
@@ -8,4 +8,14 @@ describe Test::ClientRequest do
     end
   end
 
+  describe "#as_indexed_json" do
+    it "serializes associations automatically" do
+      client_request = build(:test_client_request)
+
+      indexable = client_request.as_json
+      indexable[:approving_official] = client_request.approving_official.as_json
+
+      expect(client_request.as_indexed_json).to eq(indexable)
+    end
+  end
 end
