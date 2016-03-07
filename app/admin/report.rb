d5e3d36e926df96112a75b6b263b55f5723d9f19
Peter Karman <peter.karman@gsa.gov>
2016-03-07 15:18:29 -0600

    add /admin/scheduled_reports

--- app/admin/report.rb
+++ app/admin/report.rb
@@ -12,4 +12,23 @@ ActiveAdmin.register Report do
     end
     f.actions
   end
+
+  show do
+    attributes_table do
+      row :name
+      row :query
+      row :shared
+      row :user
+      row :created_at
+      row :updated_at
+    end
+
+    panel "Scheduled Subscriptions" do
+      table_for report.subscriptions.order("created_at DESC") do |tbl|
+        tbl.column("Name") { |scheduled_report| link_to scheduled_report.name, admin_scheduled_report_path(scheduled_report) }
+        tbl.column("Owner") { |scheduled_report| scheduled_report.user }
+        tbl.column("Freqency") { |scheduled_report| scheduled_report.frequency }
+      end
+    end
+  end
 end
---
+++ app/admin/scheduled_report.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register ScheduledReport do
+  permit_params :name, :report_id, :frequency, :user_id
+
+  form do |f|
+    f.inputs "Scheduled Report" do
+      f.input :name
+      f.input :report
+      f.input :user
+      f.input :frequency, as: :select, collection: ["daily", "weekly", "monthly"]
+    end   
+    f.actions
+  end
+end
