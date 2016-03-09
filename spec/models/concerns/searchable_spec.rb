1e88d237f7d96c7c4cbfe004dd8d129b68a0571f
Jessie A Young <jessica.young@gsa.gov>
2016-03-09 14:02:50 -0800

    Update elasticsearch gem

--- spec/models/concerns/searchable_spec.rb
+++ spec/models/concerns/searchable_spec.rb
@@ -1,6 +1,8 @@
 describe Searchable do
   describe ".rebuild_index" do
-    it "should rebuild the index" do
+    it "should rebuild the index", elasticsearch: true do
+      create(:proposal)
+
       expect(Proposal.rebuild_index).not_to raise
     end
   end
