1612a855b437c51461508a064ca1a2010ee3cebb
Peter Karman <peter.karman@gsa.gov>
2016-03-09 23:27:51 -0600

    refactor to satisfy rubocop

--- app/models/report.rb
+++ app/models/report.rb
@@ -53,7 +53,7 @@ class Report < ActiveRecord::Base
   end
 
   def subscription_for(some_user)
-    subscriptions.where(user: some_user).first
+    subscriptions.find_by(user: some_user)
   end
 
   def self.sql_for_user(user)
