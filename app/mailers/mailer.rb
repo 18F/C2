6e303027e75fe5c56ded2de64b7dadb79a81d56c
Jessie A Young <jessica.young@gsa.gov>
2016-03-10 16:06:39 -0800

    Remove unused mailer methods
    * Upate specs for incoming mail handler to user active mailer
    * Was not sending emails because comment had no listeners
    * Add actionable approver so email get sent
    * Remove associated view partials
    * Also remove duplicate footer in StepMailers

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,31 +1,4 @@
 class Mailer < ApplicationMailer
-  def actions_for_approver(step, alert_partial = nil)
-    @show_step_actions = true
-    to_email = step.user_email_address
-    proposal = step.proposal
-
-    unless step.api_token
-      step.create_api_token
-    end
-
-    notification_for_subscriber(to_email, proposal, alert_partial, step)
-  end
-
-  def notification_for_subscriber(to_email, proposal, alert_partial = nil, step = nil)
-    @step = step.decorate if step
-    @alert_partial = alert_partial
-    @proposal = proposal.decorate
-    assign_threading_headers(@proposal)
-
-    mail(
-      from: user_email_with_name(proposal.requester),
-      to: to_email,
-      subject: subject(proposal),
-      template_name: "notification_for_subscriber",
-      reply_to: reply_email(@proposal)
-    )
-  end
-
   def resend(msg)
     @_message = Mail.new msg
     # we want to preserve the From name but not the email address, since gsa.gov
@@ -40,5 +13,4 @@ class Mailer < ApplicationMailer
       "X-C2-Original-From" => from_raw
     ) {} # no-op block so template error is avoided (body already in @_message)
   end
-
 end
