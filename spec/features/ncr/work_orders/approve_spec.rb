d87f217f1179671edd9c14edb6e108af844704f3
Jessie A. Young <jessie.a.young@gmail.com>
2016-03-18 17:45:27 -0700

    Send email to requester when final step is completed
    * Was not sending in production because `proposal` was not reloaded
    * Wrote feature specs for each use case to confirm behavior
    * Always good to actually send emails :)
    
    https://trello.com/c/CP1x6c5e

---
+++ spec/features/ncr/work_orders/approve_spec.rb
@@ -0,0 +1,23 @@
+feature "Approve a NCR work order" do
+  include EnvVarSpecHelper
+
+  context "when signed in as the approver" do
+    context "last step is completed" do
+      it "sends one email to the requester" do
+        with_env_var("NO_WELCOME_EMAIL", "true") do
+          work_order = create(:ncr_work_order, :with_approvers)
+          approver = work_order.individual_steps.last.user
+          work_order.individual_steps.first.complete!
+          deliveries.clear
+
+          login_as(approver)
+          visit proposal_path(work_order.proposal)
+          click_on("Approve")
+
+          expect(deliveries.length).to eq(1)
+          expect(deliveries.first.to).to eq([work_order.proposal.requester.email_address])
+        end
+      end
+    end
+  end
+end
