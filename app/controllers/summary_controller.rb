e3d4137253519f042d13e6a14019694a643fcdd1
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2016-03-21 17:12:59 -0700

    Display multiple client summaries for gateway admins

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -2,24 +2,30 @@ class SummaryController < ApplicationController
   before_action :authorize
 
   def index
-    client_namespace = current_user.client_slug.titleize
-    @client_summarizer = ClientSummarizer.new(
-      client_namespace: client_namespace,
-      fiscal_year: params[:fiscal_year]
-    )
-    @summary = @client_summarizer.run
+    if current_user.gateway_admin?
+      client_namespaces = Proposal.client_slugs.map(&:titleize)
+    else
+      client_namespaces = [current_user.client_slug.titleize]
+    end
+
+    @summaries = client_namespaces.map do |cn|
+      ClientSummarizer.new(
+        client_namespace: cn,
+        fiscal_year: params[:fiscal_year]
+      ).run
+    end
   end
 
   private
 
   def authorize
-    if !adminish_role? || !client_slug?
+    if !adminish_role? || (!client_slug? && !current_user.gateway_admin?)
       render "authorization_error", status: 403
     end
   end
 
   def adminish_role?
-    current_user.admin? || current_user.client_admin?
+    current_user.admin? || current_user.client_admin? || current_user.gateway_admin?
   end
 
   def client_slug?
