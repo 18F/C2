322c9a86f861947e8394c3cd1334481791b4b7e1
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2016-03-23 12:35:17 -0700

    Clarify how dashboard query specs exclude canceled records

--- spec/queries/gsa18f/dashboard_query_spec.rb
+++ spec/queries/gsa18f/dashboard_query_spec.rb
@@ -1,23 +1,30 @@
 describe Gsa18f::DashboardQuery do
   describe "#select_all" do
-    it "returns scope Gsa18F records" do
+    it "aggregates scope Gsa18F records" do
       user = create(:user)
       proposal = create(:proposal, requester: user)
-      # canceled proposals should be excluded from the totals
-      proposal2 = create(:proposal, requester: user)
-      proposal2.update(status: "canceled")
-      create(:gsa18f_procurement, cost_per_unit: 10, quantity: 10,  proposal: proposal)
-      create(:gsa18f_procurement, cost_per_unit: 10, quantity: 10,  proposal: proposal2)
+      create(:gsa18f_procurement, cost_per_unit: 10, quantity: 10, proposal: proposal)
       now = Time.current.utc
 
       query = described_class.new(user).select_all
 
       expect(query.to_a).to eq([{
-        "year" => now.year.to_s,
-        "month" => now.month.to_s,
-        "count" => "1",
-        "cost" => "100.0"
-       }])
+                                 "year" => now.year.to_s,
+                                 "month" => now.month.to_s,
+                                 "count" => "1",
+                                 "cost" => "100.0"
+                               }])
+    end
+
+    it "does not aggregate canceled Gsa18F records" do
+      user = create(:user)
+      canceled_proposal = create(:proposal, requester: user)
+      canceled_proposal.update(status: "canceled")
+      create(:gsa18f_procurement, cost_per_unit: 10, quantity: 10, proposal: canceled_proposal)
+
+      query = described_class.new(user).select_all
+
+      expect(query.to_a).to eq([])
     end
   end
 end
--- spec/queries/ncr/dashboard_query_spec.rb
+++ spec/queries/ncr/dashboard_query_spec.rb
@@ -1,23 +1,30 @@
 describe Ncr::DashboardQuery do
   describe "#select_all" do
-    it "returns scoped NCR records" do
+    it "aggregates scoped NCR records" do
       user = create(:user)
       proposal = create(:proposal, requester: user)
-      # canceled proposals should be excluded from the totals
-      proposal2 = create(:proposal, requester: user)
-      proposal2.update(status: "canceled")
       create(:ncr_work_order, amount: 100, proposal: proposal)
-      create(:ncr_work_order, amount: 100, proposal: proposal2)
       now = Time.current.utc
 
       query = described_class.new(user).select_all
 
       expect(query.to_a).to eq([{
-        "year" => now.year.to_s,
-        "month" => now.month.to_s,
-        "count" => "1",
-        "cost" => "100.0"
-       }])
+                                 "year" => now.year.to_s,
+                                 "month" => now.month.to_s,
+                                 "count" => "1",
+                                 "cost" => "100.0"
+                               }])
+    end
+
+    it "does not aggregate canceled NCR records" do
+      user = create(:user)
+      canceled_proposal = create(:proposal, requester: user)
+      canceled_proposal.update(status: "canceled")
+      create(:ncr_work_order, amount: 100, proposal: canceled_proposal)
+
+      query = described_class.new(user).select_all
+
+      expect(query.to_a).to eq([])
     end
   end
 end
