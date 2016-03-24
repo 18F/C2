392b6cbfe9e794a5478ae31fce997251710f59d0
Jessie A. Young <jessie.a.young@gmail.com>
2016-03-24 15:24:19 -0700

    Fix `urgency` display for 18F procurements
    * Was showing as integer rather than string
    * Move logic from model -> decorator
    
    https://trello.com/c/YEbcYtjo/255-18f-urgency-showing-as-integer-not-string

--- app/decorators/gsa18f/procurement_decorator.rb
+++ app/decorators/gsa18f/procurement_decorator.rb
@@ -17,6 +17,22 @@ module Gsa18f
       ]
     end
 
+    def display
+      [
+        [translated_key("product_name_and_description"), object.product_name_and_description],
+        [translated_key("purchase_type"), object.purchase_type],
+        [translated_key("justification"), object.justification],
+        [translated_key("date_requested"), object.date_requested],
+        [translated_key("quantity"), object.quantity],
+        [translated_key("cost_per_unit"), object.cost_per_unit],
+        [translated_key("total_price"), object.total_price],
+        [translated_key("office"), object.office],
+        [translated_key("urgency"), object.urgency_string],
+        [translated_key("link_to_product"), object.link_to_product],
+        [translated_key("additional_info"), object.additional_info]
+      ] + recurring_fields
+    end
+
     def top_email_field
     end
 
@@ -24,6 +40,17 @@ module Gsa18f
 
     def translated_key(key)
       I18n.t("decorators.gsa18f/procurement.#{key}")
+    end
+
+    def recurring_fields
+      if recurring
+        [
+          [translated_key("recurring_interval"), object.recurring_interval],
+          [translated_key("recurring_length"), object.recurring_length]
+        ]
+      else
+        []
+      end
     end
   end
 end
--- spec/models/gsa18f/procurement_fields_spec.rb
+++ spec/models/gsa18f/procurement_fields_spec.rb
@@ -14,26 +14,4 @@ describe Gsa18f::ProcurementFields do
       expect(fields).not_to include(:recurring_length)
     end
   end
-
-  describe "#display" do
-    it "returns relevant fields for the procurement passed in, plus total price" do
-      procurement = build(:gsa18f_procurement)
-
-      fields = Gsa18f::ProcurementFields.new(procurement).display
-
-      expect(fields).to eq([
-        ["Additional info", procurement.additional_info],
-        ["Cost per unit", procurement.cost_per_unit],
-        ["Date requested", procurement.date_requested],
-        ["Justification", procurement.justification],
-        ["Link to product", procurement.link_to_product],
-        ["Office", procurement.office],
-        ["Product name and description", procurement.product_name_and_description],
-        ["Purchase type", procurement.purchase_type],
-        ["Quantity", procurement.quantity],
-        ["Urgency", procurement.urgency],
-        ["Total Price", procurement.total_price]
-      ])
-    end
-  end
 end
