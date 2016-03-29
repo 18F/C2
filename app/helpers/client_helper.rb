356c61f64ceb6cf3bbfc2391e7fff2bd85a3dfa1
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-03-29 16:18:32 -0400

    Set up the steps

--- app/helpers/client_helper.rb
+++ app/helpers/client_helper.rb
@@ -8,12 +8,12 @@ module ClientHelper
     end
   end
 
-  def modify_client_button(proposal)
+  def modify_client_button(proposal, link_text = "Modify Request", link_class = "form-button modify")
     client_data = proposal.client_data
 
     if client_data && client_data.editable?
       url = polymorphic_path(client_data, action: :edit)
-      link_to "Modify Request", url, class: "form-button modify"
+      link_to link_text, url, class: link_class
     else
       ""
     end
