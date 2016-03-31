d18fa166b22a9bfe5ff15dab0170c9f36cea2d46
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2016-03-31 12:01:31 -0700

    Starting on the request details toggle

--- app/assets/javascripts/redesign_details.js
+++ app/assets/javascripts/redesign_details.js
@@ -4,6 +4,7 @@
 
   details_app.blastOff = function(){
     this.setup_status_toggle();
+    this.setup_request_details_toggle();
   }
 
   details_app.setup_status_toggle = function(){
@@ -14,6 +15,19 @@
         $('.status-toggle-all').text('Minimize');
       } else {
         $('.status-toggle-all').text('Show all');
+      }
+    });
+  }
+
+  details_app.setup_request_details_toggle = function() {
+    console.log("setting edit button");
+    $('.request-detail-edit').on('click', function(e) {
+      e.preventDefault();
+      $('.detail-form,.detail-element,.detail-value').toggle();
+      if($('.detail-value').css('display') == 'none') {
+        $('.request-detail-edit').text('View');
+      } else {
+        $('.request-detail-edit').text('Add / Edit');
       }
     });
   }
