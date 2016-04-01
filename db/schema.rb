fda88c5c577de6af9f57a5925708097dcd947ba6
Peter Karman <peter.karman@gsa.gov>
2016-04-01 14:27:54 -0500

    schema version missed in PR for AddProposalsPublicIdLimit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331143230) do
+ActiveRecord::Schema.define(version: 20160331203347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
