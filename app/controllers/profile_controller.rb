87b3813267363664aac6f00a11a6a7c60bc22441
Peter Karman <peter.karman@gsa.gov>
2016-04-04 16:48:51 -0500

    use User::DEFAULT_TIMEZONE as fallback, current session timezone for populating default field value

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -7,7 +7,7 @@ class ProfileController < ApplicationController
     user = current_user
     user.first_name = user_params[:first_name]
     user.last_name = user_params[:last_name]
-    user.timezone = user_params[:timezone] || "Eastern Time (US &amp; Canada)"
+    user.timezone = user_params[:timezone] || User::DEFAULT_TIMEZONE
     user.save!
     flash[:success] = "Your profile is updated!"
     redirect_to :profile
