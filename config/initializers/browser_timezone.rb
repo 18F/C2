35c9d34dae9f7b003670b8b2b9086e8ee52b824d
Peter Karman <peter.karman@gsa.gov>
2016-04-04 09:19:05 -0500

    if user.timezone is defined, prefer it over cookie value

---
+++ config/initializers/browser_timezone.rb
@@ -0,0 +1,17 @@
+module BrowserTimezoneRails
+  module TimezoneControllerSetup
+    private
+
+    def time_zone_to_use
+      if current_user && current_user.timezone
+        current_user.timezone
+      else
+        browser_timezone.presence || Time.zone
+      end
+    end
+
+    def set_time_zone(&action)
+      Time.use_zone(time_zone_to_use, &action)
+    end
+  end
+end
