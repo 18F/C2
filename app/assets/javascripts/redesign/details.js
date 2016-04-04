bb6d38f18ab47ba5323bf942a02381ea57a6d02e
Nick Bristow <Nick.Bristow@gsa.gov>
2016-04-04 18:18:03 -0400

    rebased

--- app/assets/javascripts/redesign/details.js
+++ app/assets/javascripts/redesign/details.js
@@ -1,11 +1,21 @@
+"use strict";
+
+var data = {};
 var detailsApp = detailsApp || {};
 
 detailsApp.blastOff = function(){
   this.setupStatusToggle();
   this.setupRequestDetailsToggle();
+  this.setupEvents();
 }
 
 detailsApp.data = {}
+
+detailsApp.setupEvents = function(){
+  $(window).on('scroll', function(e){
+
+  })
+}
 
 detailsApp.setupStatusToggle = function(){
   $('.status-toggle-all').on('click', function(e){
@@ -20,41 +30,62 @@ detailsApp.setupStatusToggle = function(){
 }
 
 detailsApp.setupRequestDetailsToggle = function() {
-  console.log("setting edit button");
-  $('.request-detail-edit').on('click', function(e) {
+  var $editButton = $(".request-detail-edit-button");
+  $editButton.on("click", function(e) {
     e.preventDefault();
-    $('.detail-form,.detail-element,.detail-value,.detail-view').toggle();
-    if($('.detail-value').css('display') == 'none') {
-      $('.request-detail-edit span').text('View');
+    $('#edit-request-details,#view-request-details').toggle();
+    if($('#view-request-details').css('display') == 'none') {
+      $('span', $editButton).text('View');
     } else {
-      $('.request-detail-edit span').text('Modify');
+      $('span', $editButton).text('Modify');
     }
+    return false;
   });
 }
 
 detailsApp.setupDataObject = function($elem) {
-  
   var self = this;
-  var cardKeys = $elem.find("[data-card-key]");
+  var cardKeys = $elem.find('[data-card-key]');
 
   cardKeys.each( function(index, elem) {
     var elemDataKey = $(elem).data('card-key');
     var elemDataKeyArray = elemDataKey.split('-');
     var elemDataValue = $(elem).data('card-value');
-    // console.log("Value: " + elemDataValue);
     var parent = self.data;
 
     for (var i = 0; i <= elemDataKeyArray.length - 2; i++) {
       var elKey = elemDataKeyArray[i];
-      if(parent[elKey] == undefined){
+      if(parent[elKey] === undefined){
         parent[elKey] = {};
       }
       parent = parent[elKey];
     }
-    // console.log("Parent: ", elemDataKeyArray[elemDataKeyArray.length-1]);
     parent[elemDataKeyArray[elemDataKeyArray.length-1]] = elemDataValue;
   })
-  console.dir(self.data);
+}
+
+detailsApp.updateStaticElements = function($elem) {
+  var self = this;
+  var cardKeys = $elem.find('div[data-card-key]')
+                      .add($elem.find('span[data-card-key]'));
+
+  cardKeys.each(function(index, elem) {
+    var $elem = $(elem);
+    var newValue = self.lookup($elem.data('card-key'));
+    $elem.text(newValue);
+    $elem.data('card-value', newValue);
+  });
+};
+
+// Currently only goes 2 levels deep
+detailsApp.lookup = function(elemDataKey) {
+  var self = this;
+  var elemDataKeyArray = elemDataKey.split("-");
+  var parentKey = elemDataKeyArray[0];
+  var childKey = elemDataKeyArray[1];
+  if (self.data[parentKey] !== undefined) {
+    return self.data[parentKey][childKey];
+  }
 }
 
 
