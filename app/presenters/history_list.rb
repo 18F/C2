c972e40b10efd009572e6b533f9d377f7a306178
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-04-14 15:13:28 -0400

    Add the history list as abstraction from the proposal#history

---
+++ app/presenters/history_list.rb
@@ -0,0 +1,28 @@
+class HistoryList
+  attr_reader :events
+
+  def initialize(proposal)
+    @proposal = proposal
+    @events = make_events(ProposalVersionsQuery.new(proposal).container.query)
+  end
+
+  private
+
+  def filter_versions(versions)
+    versions.reject do |version|
+      version.item_type == "Ncr::WorkOrder" ||
+        version.item_type == "Gsa18f::Procurement" ||
+        version.item_type == "Step" || # TODO: What's creating these?
+        (version.event == "update" &&
+          version.item_type == "Proposal") || # TODO: include once they're properly formatted
+        (version.event == "create" &&
+          !%w(Proposal Attachment Comment Observation Steps::Serial).include?(version.item_type))
+    end
+  end
+
+  def make_events(versions)
+    filter_versions(versions).map do |version|
+      HistoryEvent.new(version)
+    end
+  end
+end
