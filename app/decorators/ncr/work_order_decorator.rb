1e4f1ba0e89d0f55dced54d6cc14265f0c792f9a
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-04-15 17:40:20 -0400

    Set up decorators to run on client slug basis

--- app/decorators/ncr/work_order_decorator.rb
+++ app/decorators/ncr/work_order_decorator.rb
@@ -112,5 +112,57 @@ module Ncr
     def translated_key(key)
       I18n.t("decorators.ncr/work_order.#{key}")
     end
+
+    def detail_fields_config
+      [
+        { 
+          value: ["description"],
+          style: {
+            column: 1,
+          },
+        },
+        { 
+          value: ["_remaining"],
+          style: {
+            column: 1,
+          },
+        },
+        { 
+          value: ["vendor"],
+          style: {
+            column: 1,
+            background: "color-card"
+          },
+        },
+        { 
+          value: ["building_number"],
+          style: {
+            column: 1,
+            background: "color-card"
+          },
+        },
+        { 
+          value: ["cl_number"],
+          style: {
+            column: 1,
+            background: "color-card"
+          },
+        },
+        { 
+          value: ["direct_pay"],
+          style: {
+            column: 2,
+          },
+        },
+        { 
+          value: ["amount"],
+          style: {
+            column: 2,
+            background: "color-card"
+          },
+        }
+      ]
+    end
+
   end
 end
