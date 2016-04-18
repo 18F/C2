0135f4c8be59638e558262671554fdf45da32aa5
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2016-04-18 12:43:30 -0700

    Remove unnecessary HistoryEvent methods, add HistoryList spec

--- spec/presenters/history_list_spec.rb
+++ spec/presenters/history_list_spec.rb
@@ -3,7 +3,7 @@ describe HistoryList do
     it "returns proposal creation as the first event" do
       proposal = create(:proposal)
 
-      history = HistoryList.new(proposal)
+      history = described_class.new(proposal)
       expect(history.events.first.event).to eq("create")
     end
 
@@ -11,9 +11,19 @@ describe HistoryList do
       it "filters client data events out" do
         work_order = create(:ncr_work_order)
         work_order.update(function_code: "blah!")
-        history = HistoryList.new(work_order.proposal)
+        history = described_class.new(work_order.proposal)
 
         expect(history.events.collect(&:item_type)).to_not include("Ncr::WorkOrder")
+      end
+    end
+
+    context "when the history contains step creation" do
+      it "filters the step creation events out" do
+        proposal = create(:proposal, :with_serial_approvers)
+        history = described_class.new(proposal)
+
+        expect(history.events.collect(&:item_type)).to_not include("Steps::Serial")
+        expect(history.events.collect(&:item_type)).to_not include("Steps::Approval")
       end
     end
   end
