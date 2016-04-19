d0beddba775534375d6fd9d0319c684853b43de7
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2016-04-18 18:06:54 -0700

    Show client data updates through HistoryList

--- app/presenters/history_list.rb
+++ app/presenters/history_list.rb
@@ -10,21 +10,20 @@ class HistoryList
 
   def filter_versions(versions)
     versions.reject do |version|
-      version_is_client_data?(version) ||
-        version_is_ignored_type_creation?(version) ||
-        version.item_type == "Step" || # TODO: What's creating these?
-        (version.event == "update" &&
-          version.item_type == "Proposal") # TODO: include once they're properly formatted
+      version_is_ignored_type_creation?(version) ||
+        version_is_proposal_update?(version) ||
+        version.item_type == "Step" # TODO: What's creating these?
     end
-  end
-
-  def version_is_client_data?(version)
-    Proposal.client_model_names.include?(version.item_type)
   end
 
   def version_is_ignored_type_creation?(version)
     version.event == "create" &&
       !%w(Proposal Attachment Comment Observation Steps::Serial).include?(version.item_type)
+  end
+
+  def version_is_proposal_update?(version)
+    (version.event == "update" &&
+      version.item_type == "Proposal")
   end
 
   def make_events(versions)
--- spec/presenters/history_list_spec.rb
+++ spec/presenters/history_list_spec.rb
@@ -7,15 +7,26 @@ describe HistoryList do
       expect(history.events.first.event).to eq("create")
     end
 
-    context "when the history contains client data events" do
-      it "filters client data events out" do
+    context "when the history contains client data creation events" do
+      it "filters client data creation events out" do
         work_order = create(:ncr_work_order)
-        work_order.update(function_code: "blah!")
         history = described_class.new(work_order.proposal)
 
         expect(history.events.collect(&:item_type)).to_not include("Ncr::WorkOrder")
       end
     end
+
+    context "when the history contains client data update events" do
+      it "does not filter client data update events out" do
+        work_order = create(:ncr_work_order)
+        work_order.update(function_code: "PG999")
+        work_order.save!
+        history = described_class.new(work_order.proposal)
+
+        expect(history.events.collect(&:item_type)).to include("Ncr::WorkOrder")
+      end
+    end
+
 
     context "when the history contains step creation" do
       it "filters the step creation events out" do
