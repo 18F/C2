b217eccf73afc30384e16ff8966e9f0f5c74a4ff
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-04-27 12:33:14 -0400

    Set up the glue js

--- app/assets/javascripts/details/app.js
+++ app/assets/javascripts/details/app.js
@@ -1,3 +1,11 @@
+var details = details || {
+  blastOff: function(){
+    var editStateController = new EditStateController('#mode-parent');
+    var notificationBar = new ActionBar('.action-bar-status');
+    var actionBar = new ActionBar('.action-bar-wrapper');
+  }
+};
+
 $(document).ready(function(){
-  // Do something here
+  details.blastOff();
 });
