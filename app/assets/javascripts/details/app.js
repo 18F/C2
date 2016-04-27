57a30a0a4afd2de6ee541b3079428f482cbc283e
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-04-27 17:55:17 -0400

    Set up the foundation

--- app/assets/javascripts/details/app.js
+++ app/assets/javascripts/details/app.js
@@ -1,8 +1,18 @@
 var details = details || {
   blastOff: function(){
+    this.states();
+    this.presenters();
+    this.data();
+  },
+  states: function(){
     var editStateController = new EditStateController('#mode-parent');
+  },
+  presenters: function(){
     var notificationBar = new ActionBar('.action-bar-status');
     var actionBar = new ActionBar('.action-bar-wrapper');
+  },
+  data: function(){
+
   }
 };
 
