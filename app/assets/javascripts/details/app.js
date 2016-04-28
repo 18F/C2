2e0aa9eef4b1a84a835544a5ba80116ea6962af7
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-04-28 11:51:15 -0400

    Test the running

--- app/assets/javascripts/details/app.js
+++ app/assets/javascripts/details/app.js
@@ -1,3 +1,6 @@
-var editState = new EditStateController();
-var notificationBar = new ActionBar('.action-bar-status');
-var actionBar = new ActionBar('.action-bar-wrapper');
+var mode = $('#mode-parent');
+
+var editState = new EditStateController(mode);
+
+// var actionBar = new ActionBar('.action-bar-wrapper');
+// var notificationBar = new ActionBar('.action-bar-status');
