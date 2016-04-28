40654e81e8ba765ecc8dd069245751de4bf09a84
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-04-28 15:32:38 -0400

    Create the code to manage edit mode event

---
+++ app/assets/javascripts/details/state/edit_mode.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var EditStateController = function (el) {
+  this.el = $(el);
+  this._setup();
+  return this;
+}
+
+EditStateController.prototype._setup = function () {
+  this.state = "view";
+  this._event();
+}
+
+EditStateController.prototype._event = function () {
+  this.el.on( "edit-mode:toggle", function( event ) {
+    var mode = $( this );
+    if ( mode.is( ".edit-mode" ) ) {
+      this.state = "edit";
+      this.el.trigger("edit-mode:on");
+    } else {
+      this.state = "view";
+      this.el.trigger("edit-mode:off");
+    }
+  });
+}
+
+EditStateController.prototype.getState = function () {
+  if(this.el.hasClass("edit-mode")){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+EditStateController.prototype.toggleState = function () {
+  if ( this.el.is( ".edit-mode" ) ) {
+    this.state = "view";
+    this.el.addClass('view-mode').removeClass('edit-mode');
+    this.el.trigger("edit-mode:off");
+  } else {
+    this.state = "edit";
+    this.el.addClass('edit-mode').removeClass('view-mode');
+    this.el.trigger("edit-mode:on");
+  }
+  var state = this.state;
+}
