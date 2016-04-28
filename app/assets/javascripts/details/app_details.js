8c5ccaecc66f12457341b5bca536725bb8f3b46a
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-04-28 13:08:20 -0400

    Add the js updates

--- app/assets/javascripts/details/app_details.js
+++ app/assets/javascripts/details/app_details.js
@@ -1,19 +1,30 @@
 var DetailsApp = function (){
   var mode = $('#mode-parent');
-  
-  var stateEdit = new EditStateController(mode);
 
-  var setup = function(){
-    this.mode = mode;
-    this.stateEdit = stateEdit;
-  }
-}
+  var stateEdit = new EditStateController(mode);
+  var actionBar = new ActionBar('.action-bar-wrapper');
   
+  var blastOff = function(){
+    this.mode = mode;
+    this.state = {
+      edit: stateEdit
+    }
+    this.views = {
+      actionBar: actionBar
+    }
+    return this;
+  }
+
+  return blastOff();
+}
+
+var detailsApp;
+
 $(document).ready(function(){
+  console.log('detailsApp init');
   detailsApp = new DetailsApp();
 })
 
 
-// var actionBar = new ActionBar('.action-bar-wrapper');
 // var notificationBar = new ActionBar('.action-bar-status');
 
