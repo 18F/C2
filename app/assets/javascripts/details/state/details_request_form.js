c1922c1c6d8c891977f61699b571a7dbf382c9ff
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-04-28 15:27:59 -0400

    Create the guid map on fields

--- app/assets/javascripts/details/state/details_request_form.js
+++ app/assets/javascripts/details/state/details_request_form.js
@@ -8,5 +8,21 @@ var DetailsRequestForm = function (el) {
 }
 
 DetailsRequestForm.prototype._setup = function(){
+  this._createGuid();
   this.data.$el = this.el.find('form');
+}
+
+DetailsRequestForm.prototype._guid = function(){
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + "-" + s4() + "-" + s4();
+};
+
+DetailsRequestForm.prototype._createGuid = function(){
+  this.el.find("form, input, textarea, select, radio").each(function(i, item){
+    $(item).attr("data-field-guid", detailsApp.guid());
+  });
 }
