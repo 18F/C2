24a6be77cf3bbaae1675cd1ca7b58b6a5967ea77
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-04-28 10:26:21 -0400

    Set up the event check

--- app/assets/javascripts/details/app.js
+++ app/assets/javascripts/details/app.js
@@ -1,21 +1,3 @@
-var details = details || {
-  blastOff: function(){
-    this.states();
-    this.presenters();
-    this.data();
-  },
-  states: function(){
-    var editStateController = new EditStateController('#mode-parent');
-  },
-  presenters: function(){
-    var notificationBar = new ActionBar('.action-bar-status');
-    var actionBar = new ActionBar('.action-bar-wrapper');
-  },
-  data: function(){
-
-  }
-};
-
-$(document).ready(function(){
-  details.blastOff();
-});
+var editState = new EditStateController();
+var notificationBar = new ActionBar('.action-bar-status');
+var actionBar = new ActionBar('.action-bar-wrapper');
--- app/assets/javascripts/details/state/edit-mode.js
+++ app/assets/javascripts/details/state/edit-mode.js
@@ -1,27 +1,47 @@
 "use strict";
 
-var EditStateController = function (el) {
-  this.el = $(el);
+var EditStateController = function () {
+  var el = $("#mode-parent");
+  this.el = el;
   this._setup();
   return this;
 }
 
 EditStateController.prototype._setup = function () {
-  this.el.on("editMode:toggle", function( e ){
-    if ( this.el.is(".edit-mode") ){
+  this._event();
+}
+
+EditStateController.prototype._event = function () {
+  this.el.on( "mode:toggle", function( event ) {
+    var mode = $( this );
+    if ( mode.is( ".edit-mode" ) ) {
+      this.state = "edit";
       this.el.trigger("editMode:on");
     } else {
+      this.state = "view";
       this.el.trigger("editMode:off");
     }
   });
 }
 
 EditStateController.prototype.getState = function () {
-  var state;
-  if(this.el.is(".edit-mode")){
-    state = true;
+  if(this.el.hasClass("edit-mode")){
+    return true;
   } else {
-    state = false;
+    return false;
   }
-  return state;
+}
+
+EditStateController.prototype.toggleState = function () {
+  if ( this.el.is( ".edit-mode" ) ) {
+    this.state = "view";
+    this.el.addClass('view-mode').removeClass('edit-mode');
+    this.el.trigger("editMode:off");
+  } else {
+    this.state = "edit";
+    this.el.addClass('edit-mode').removeClass('view-mode');
+    this.el.trigger("editMode:on");
+  }
+  var state = this.state;
+  console.log('Edit mode: ', state);
 }
