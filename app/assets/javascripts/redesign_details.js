55ec72f5527b8b3e4717d8f3a6b33c9a939a5316
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-04-29 18:08:58 -0400

    Save the details app

--- app/assets/javascripts/redesign_details.js
+++ app/assets/javascripts/redesign_details.js
@@ -1,16 +1,25 @@
-$(document).ready(function(){
+(function(){
 
-  $('.status-toggle-all').on('click', function(e){
-    e.preventDefault();
-    $('.status-contracted').toggleClass('status-expanded');
-    if($('.status-contracted').hasClass('status-expanded')){
-      $('.status-toggle-all').text('Minimize');
-    } else {
-      $('.status-toggle-all').text('Show all');
-    }
+  var details_app = details_app || {};
+
+  details_app.blastOff = function(){
+    this.setup_status_toggle();
+  }
+
+  details_app.setup_status_toggle = function(){
+    $('.status-toggle-all').on('click', function(e){
+      e.preventDefault();
+      $('.status-contracted').toggleClass('status-expanded');
+      if($('.status-contracted').hasClass('status-expanded')){
+        $('.status-toggle-all').text('Minimize');
+      } else {
+        $('.status-toggle-all').text('Show all');
+      }
+    });
+  }
+
+  $(document).ready(function(){
+    details_app.blastOff();
   });
 
-});
-
-var details_app = details_app || {};
-
+})();
