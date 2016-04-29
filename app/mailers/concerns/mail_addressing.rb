c302f9389c8478c136b73342cccaa9b961db2191
Peter Karman <peter.karman@gsa.gov>
2016-04-29 16:26:07 -0500

    whitespace only, for rubocop

--- app/mailers/concerns/mail_addressing.rb
+++ app/mailers/concerns/mail_addressing.rb
@@ -6,27 +6,27 @@ module MailAddressing
     address = Mail::Address.new(email)
     address.display_name = name
     address.format
-  end 
+  end
 
   def reply_email(proposal)
     reply_to_email.gsub("@", "+#{proposal.public_id}@")
-  end 
+  end
 
   def reply_to_email
     email_with_name(ENV["NOTIFICATION_REPLY_TO"] || "noreplyto@example.com", "C2")
-  end 
+  end
 
   def sender_email
     email_with_name(ENV["NOTIFICATION_FROM_EMAIL"] || "noreply@example.com", "C2")
-  end 
+  end
 
   def resend_to_email
     email_with_name(ENV["NOTIFICATION_FALLBACK_EMAIL"] || "communicart.sender@gsa.gov", "C2")
-  end 
+  end
 
   def default_sender_email
     sender_email
-  end 
+  end
 
   def user_email_with_name(user)
     email_with_name(sender_email, user.full_name)
