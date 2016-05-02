7fd565d59c969547c05c48563be594a5dc7839c1
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-02 17:52:02 -0400

    Set up the request form files

---
+++ app/assets/javascripts/details/state/details_request_form.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var DetailsRequestFormState = function (el) {
+  this.el = $(el);
+  this.data = {
+    fieldUID: {}  
+  }
+  this._setup();
+  return this;
+}
+
+DetailsRequestFormState.prototype._setup = function(){
+}
