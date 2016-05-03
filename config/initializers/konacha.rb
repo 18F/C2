0f65336d7a96da5b27e9e9bc0d4a288e8852349d
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-03 11:02:14 -0400

    Enable timeouts and debug mode for konacha

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,8 +1,11 @@
 require "tilt/coffee"
 
 if defined?(Konacha)
+  Capybara.register_driver :slow_poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, timeout: 2.minutes, debug: true)
+  end
   Konacha.configure do |config|
     require "capybara/poltergeist"
-    config.driver = :poltergeist
+    config.driver    = :slow_poltergeist
   end
 end
