ee140f486abfb9bb7f4b6dd95e04cd99462c49da
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-03 18:14:58 -0400

    Move content to shell file

---
+++ app/assets/javascripts/details/shell/c2.js
@@ -0,0 +1,37 @@
+var C2;
+C2 = (function() {
+  
+  function C2(){
+    this._blastOff();
+    this._events();
+  }
+
+  C2.prototype._blastOff = function(){
+    this.attachmentCardController = new AttachmentCardController(".card-for-attachments");
+    this.editMode = new EditStateController('#mode-parent');
+    this.formState = new DetailsRequestFormState('#request-details-card');
+    this.actionBar = new ActionBar('.action-bar-wrapper');
+  }
+
+  C2.prototype._events = function(){
+    this.formState.el.on("form:changed", function(){
+      var hasChanged = false;
+      if(hasChanged){
+        editMode.el.trigger("edit-mode:on")
+      } else {
+        editMode.el.trigger("edit-mode:off")
+      }
+    }); 
+
+    this.actionBar.el.on("button:save", function(){
+      var editMode = editMode.getState();
+      if(editMode){
+      } else {
+      }
+    });
+  }
+  
+  return C2;
+
+})();
+window.C2 = C2;
