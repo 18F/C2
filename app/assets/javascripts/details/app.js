12c01c83916a111cfd538f2a0e2b2e9861f28845
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-03 18:15:10 -0400

    Remove the content from app file

--- app/assets/javascripts/details/app.js
+++ app/assets/javascripts/details/app.js
@@ -1,25 +1,3 @@
 $(document).ready(function(){
-  var attachmentCardController = new AttachmentCardController(".card-for-attachments");
-});
-
-var editMode = new EditStateController('#mode-parent');
-var formState = new DetailsRequestFormState('#request-details-card');
-var actionBar = new ActionBar('#request-actions');
-
-formState.el.on("form:changed", function(){
-  var hasChanged = false;
-  if(hasChanged){
-    editMode.el.on("edit-mode:on", function(){
-  } else {
-    editMode.el.on("edit-mode:off", function(){
-  }
-}); 
-
-actionBar.el.on("button:save", function(){
-  var editMode = editMode.getState();
-  if(editMode){
-
-  } else {
-    
-  }
+  var c2 = new C2();
 });
