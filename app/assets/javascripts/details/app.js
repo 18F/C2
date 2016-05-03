125f42c3cf22f638f6f5d55d7bd9ee33a057f536
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-03 16:28:58 -0400

    Add the content form trigger

--- app/assets/javascripts/details/app.js
+++ app/assets/javascripts/details/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
   var attachmentCardController = new AttachmentCardController(".card-for-attachments");
 });
+
+var editMode = new EditStateController('#mode-parent');
+var formState = new DetailsRequestFormState('#request-details-card');
+
+formState.el.on("form:changed", function(){
+  var hasChanged = false;
+  if(hasChanged){
+    editMode.el.on("edit-mode:on", function(){
+  } else {
+    editMode.el.on("edit-mode:off", function(){
+  }
+}); 
