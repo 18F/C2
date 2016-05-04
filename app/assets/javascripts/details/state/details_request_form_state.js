dc346a2ff272caf2dbe5a3c1fe993366460f7f42
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-03 23:53:38 -0400

    Add the guid utility function

--- app/assets/javascripts/details/state/details_request_form_state.js
+++ app/assets/javascripts/details/state/details_request_form_state.js
@@ -13,6 +13,15 @@ DetailsRequestFormState = (function(){
   DetailsRequestFormState.prototype._setup = function(){
   }
 
+  DetailsRequestFormState.prototype.guid = function(){
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + "-" + s4() + "-" + s4();
+  };
+
   return DetailsRequestFormState;
 
 }());
