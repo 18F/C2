e241ae3847d2b439e4808fb99dee84786270c290
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-03 22:57:26 -0400

    Move invocation to inside IIFE

--- app/assets/javascripts/details/shell/c2.js
+++ app/assets/javascripts/details/shell/c2.js
@@ -3,12 +3,18 @@ C2 = (function() {
   
   function C2(){
     this._blastOff();
+    this._events();
   }
 
   C2.prototype._blastOff = function(){
     this.attachmentCardController = new AttachmentCardController(".card-for-attachments");
+    this.editMode = new EditStateController('#mode-parent');
+    this.actionBar = new ActionBar('.action-bar-wrapper');
   }
- 
+
+  C2.prototype._events = function(){
+  }
+  
   return C2;
 
 })();
