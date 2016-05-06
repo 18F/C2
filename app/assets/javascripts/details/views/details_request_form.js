bb03c4213fe0d613f45eef408aa0d551b67472bb
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-06 15:43:52 -0400

    Add the debounce

--- app/assets/javascripts/details/views/details_request_form.js
+++ app/assets/javascripts/details/views/details_request_form.js
@@ -15,8 +15,25 @@ DetailsRequestForm = (function(){
     var self = this;
     this.el.find("input, textarea, select, radio").on("change keypress blur focus keyup", function(e){
       var el = this;
-      self.fieldChanged(e, el)
+      self._debounce.(function(){
+        self.fieldChanged(e, el);
+      }, 100);
     });
+  }
+
+  DetailsRequestForm.prototype._debounce function(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
   }
 
   DetailsRequestForm.prototype.fieldChanged = function(e, el){
