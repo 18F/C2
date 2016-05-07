0258ea86742eeb0f4d6a0292705b7f84f69034b6
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-06 23:40:09 -0400

    Add new structure for shell

--- app/assets/javascripts/details/shell/c2.js
+++ app/assets/javascripts/details/shell/c2.js
@@ -2,19 +2,42 @@ var C2;
 C2 = (function() {
   
   function C2(){
+    this.config = {
+      editMode:       '#mode-parent',
+      formState:      '#request-details-card',
+      detailsForm:    '#request-details-card',
+      detailsSave:    '#request-details-card',
+      attachmentCard: '.card-for-attachments',
+      actionBar:      '.action-bar-wrapper'
+    }
     this._blastOff();
-    this._events();
   }
 
   C2.prototype._blastOff = function(){
-    this.attachmentCardController = new AttachmentCardController(".card-for-attachments");
-    this.editMode = new EditStateController('#mode-parent');
-    this.actionBar = new ActionBar('.action-bar-wrapper');
+    this._setupStates();
+    this._setupViews();
+    this._setupData();
+    this._setupEvents();
   }
 
-  C2.prototype._events = function(){
+  C2.prototype._setupData = function(){
+    var config = this.config;
+  }
+
+  C2.prototype._setupStates = function(){
+    var config = this.config;
+    this.editMode = new EditStateController(config.editMode);
   }
   
+  C2.prototype._setupViews = function(){
+    var config = this.config;
+    this.attachmentCardController = new AttachmentCardController(config.attachmentCard);
+    this.actionBar = new ActionBar(config.actionBar);
+  }
+
+  C2.prototype._setupEvents = function(){
+  }
+
   return C2;
 
 }());
