4ac1257ec230885b6ab487dacea96c1246451d1e
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-11 17:46:18 -0400

    Create the notification

--- app/assets/javascripts/details/views/notifications.js
+++ app/assets/javascripts/details/views/notifications.js
@@ -15,6 +15,36 @@ NotificationBars = (function(){
 
   }
 
+  NotificationBars.prototype.create = function(params){
+    var notice = this._prepare(params);
+    this.postNotification(notice);
+  }
+
+  NotificationBars.prototype.postNotification = function(notice){
+    this.el.find('ul').append(notice);
+  }
+
+  NotificationBars.prototype._prepare = function(params){
+    var type    = (params['type']) ? params['type'] : 'primary';
+    var title   = (params['title']) ? params['title'] : '';
+    var content = (params['content']) ? params['content'] : '';
+    var timeout = (params['timeout']) ? params['timeout'] : false;
+    
+    var notice = '<li class="notice-type-' + type + ' notification-bar-el" data-timeout="' + timeout + '">' +
+      '<div><h4>' + title + '</h4><p>' + content + '</p></div>' + 
+    '</li>';
+    
+    return notice
+  }
+
+  NotificationBars.prototype.clearAll = function(){
+  }
+
+  // Use the ID to select which to clear
+  NotificationBars.prototype.clearOne = function(id){
+    
+  }
+
   return NotificationBars;
 
 }());
