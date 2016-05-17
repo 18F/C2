63585feb875f4311042f000ea3065a28db9bfd2f
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-16 23:42:24 -0400

    Save the changes on the form data manipulation

--- app/assets/javascripts/details/state/details_request_form_state.js
+++ app/assets/javascripts/details/state/details_request_form_state.js
@@ -14,31 +14,11 @@ DetailsRequestFormState = (function(){
     this._processFields();
   }
 
-  DetailsRequestFormState.prototype.guid = function(){
-    function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-    }
-    return s4() + "-" + s4() + "-" + s4();
-  };
-
   DetailsRequestFormState.prototype._processFields = function(){
     var self = this;
     this.el.find("form, input, textarea, select, radio").each(function(i, item){
-      self.createGuid(item);
-      self.updateSavedValue(item);
+    
     });
-  }
-
-  DetailsRequestFormState.prototype.createGuid = function(item){
-    var self = this;
-    $(item).attr("data-field-guid", self.guid());
-  }
-
-  DetailsRequestFormState.prototype.updateSavedValue = function(item){
-    var self = this;
-    $(item).attr("data-field-value", $(item).val());
   }
 
   return DetailsRequestFormState;
