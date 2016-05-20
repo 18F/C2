565d69c14614d8693f00240e17c0d4f8088a4137
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-05-20 15:25:01 -0400

    Save the buttons built in

--- app/assets/javascripts/details/shell/c2.js
+++ app/assets/javascripts/details/shell/c2.js
@@ -156,6 +156,7 @@ C2 = (function() {
   C2.prototype.handleSaveError = function(data){
     var response = data['response'];
     for (var i = response.length - 1; i >= 0; i--) {
+      response[i]['timeout'] = 7500;
       this.createNotification(response[i], "", "alert");
     }
   }
