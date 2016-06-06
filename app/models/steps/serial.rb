0ed71fe8af3f4e6a27c8145fbbdbcc0024fb3807
Robb Shecter <robb.shecter@gsa.gov>
2016-06-06 15:07:30 -0700

    Docs

--- app/models/steps/serial.rb
+++ app/models/steps/serial.rb
@@ -24,6 +24,7 @@ module Steps
         event :restart, transitions_to: :pending
       end
 
+      # Does the equivalent of `def complete!`
       state :completed do
         on_entry { notify_parent_completed }
 
