5a066e9e2497c561153e945b4bbb6b4ec81ce668
Robb Shecter <robb.shecter@gsa.gov>
2016-06-09 16:00:08 -0700

    Docs

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,10 +4,11 @@ class Role < ActiveRecord::Base
   has_many :user_roles
   has_many :users, through: :user_roles
 
-
+  # Safely ensure that the system roles in `config/initializers/01_c2.rb`
+  # exist in the database. Rails will run this code when it loads the
+  # `Role` class at bootup.
   def self.ensure_system_roles_exist
     SYSTEM_ROLES.each { |r| Role.find_or_create_by!(name: r) }
   end
   ensure_system_roles_exist
-
 end
