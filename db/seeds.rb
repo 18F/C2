f00bcafdc8cc45fa1eb3c363ccfcaa64c55af391
Robb Shecter <robb.shecter@gsa.gov>
2016-06-10 12:28:39 -0700

    Refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,7 @@ require 'seed_report'
 
 # Any commands in this file should be idempotent
 SeedReport.for_model Role do
-  Role.find_or_create_by!(name: "observer")
-  Role.find_or_create_by!(name: "client_admin")
-  Role.find_or_create_by!(name: "admin")
+  %w(observer client_admin admin).each { |r| Role.find_or_create_by!(name: r) }
 end
 
 # RolesConversion relies upon some ENV variables to set the initial User records. it
