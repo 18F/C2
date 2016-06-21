3a72360875cf68fce7f9c2038b2095fbf8fa6918
Robb Shecter <robb.shecter@gsa.gov>
2016-06-21 00:15:54 -0700

    Fixes brittle spec

--- spec/queries/ncr/unapproved_count_query_spec.rb
+++ spec/queries/ncr/unapproved_count_query_spec.rb
@@ -1,12 +1,14 @@
 describe Ncr::UnapprovedCountQuery do
-  describe ".find" do
+  describe "#find" do
     it "returns the number of pending proposals for NCR" do
+      orig_count = Ncr::UnapprovedCountQuery.new.find
+
       pending_proposal = create(:proposal, status: "pending")
       completed_proposal = create(:proposal, status: "completed")
       create(:ncr_work_order, proposal: pending_proposal)
       create(:ncr_work_order, proposal: completed_proposal)
 
-      expect(Ncr::UnapprovedCountQuery.new.find).to eq 1
+      expect(Ncr::UnapprovedCountQuery.new.find).to eq(orig_count + 1)
     end
   end
 end
