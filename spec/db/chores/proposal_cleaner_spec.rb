f3a8ad2c4ce926e7dee869dddb84b88182cb5d92
Robb Shecter <robb.shecter@gsa.gov>
2016-06-21 17:04:30 -0700

    Refactor to use rspec .by() syntax

--- spec/db/chores/proposal_cleaner_spec.rb
+++ spec/db/chores/proposal_cleaner_spec.rb
@@ -1,34 +1,26 @@
-require "#{Rails.root}/db/chores/proposal_cleaner"
+require Rails.root.join "db/chores/proposal_cleaner"
 
 describe ProposalCleaner do
   describe "#run" do
     it "removes proposals without client data" do
       _client_data_with_proposal = create(:ncr_work_order)
       create(:proposal, client_data: nil)
-      orig_count = Proposal.count
 
-      ProposalCleaner.new.run
-
-      expect(Proposal.count).to eq(orig_count - 1)
+      expect { ProposalCleaner.new.run }.to change { Proposal.count }.by(-1)
     end
 
     it "removes client data without a proposal" do
       _ncr_with_proposal = create(:ncr_work_order)
       ncr_without_proposal = build(:ncr_work_order, proposal: nil)
       ncr_without_proposal.save(validate: false)
-      orig_count = Ncr::WorkOrder.count
 
-      ProposalCleaner.new.run
-
-      expect(Ncr::WorkOrder.count).to eq(orig_count - 1)
+      expect { ProposalCleaner.new.run }.to change { Ncr::WorkOrder.count }.by(-1)
 
       _18f_with_proposal = create(:gsa18f_procurement)
       gsa_without_proposal = build(:gsa18f_procurement, proposal: nil)
       gsa_without_proposal.save(validate: false)
 
-      expect {
-        ProposalCleaner.new.run
-      }.to change { Gsa18f::Procurement.count }.from(2).to(1)
+      expect { ProposalCleaner.new.run }.to change { Gsa18f::Procurement.count }.by(-1)
     end
   end
 end
