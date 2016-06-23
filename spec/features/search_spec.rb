4df0cbc8ba33b34f33a16d931c836e118e67a658
Robb Shecter <robb.shecter@gsa.gov>
2016-06-23 16:27:09 -0700

    A failing spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -4,7 +4,7 @@ describe "searching", elasticsearch: true do
     login_as(proposals.first.requester)
 
     visit proposals_path
-    fill_in 'text', with: proposals.first.public_id
+    fill_in "text", with: proposals.first.public_id
     click_button "search-button"
 
     expect(current_path).to eq query_proposals_path
@@ -25,27 +25,11 @@ describe "searching", elasticsearch: true do
     expect(page).to have_content(I18n.t("errors.features.es.service_unavailable"))
   end
 
-  it "provides advanced search", :js do
-    proposals = populate_proposals
-    login_as(proposals.first.requester)
-
-    visit proposals_path
-    fill_in "text", with: proposals.first.name
-    adv_options = find("a.adv-options")
-    adv_options.trigger("click")
-    fill_in "test_client_request[client_data.amount]", with: proposals.first.client_data.amount
-    click_button "adv-search-button"
-
-    expect(current_path).to eq query_proposals_path
-    expect(page).to have_content(proposals.first.public_id)
-    expect(page).to have_content("(#{proposals.first.name}) AND (Amount:(#{proposals.first.client_data.amount}))")
-  end
-
   it "opens advanced search UI when ?search=true set", :js do
     proposals = populate_proposals
     login_as(proposals.first.requester)
 
-    visit proposals_path({search: true})
+    visit proposals_path(search: true)
     fill_in "test_client_request[client_data.amount]", with: proposals.first.client_data.amount
     click_button "adv-search-button"
 
@@ -58,12 +42,12 @@ describe "searching", elasticsearch: true do
     login_as(user)
 
     visit proposals_path
-    fill_in 'text', with: 'foo'
+    fill_in "text", with: "foo"
     click_button "search-button"
 
-    expect(current_path).to eq('/proposals/query')
-    field = find_field('text')
-    expect(field.value).to eq('foo')
+    expect(current_path).to eq("/proposals/query")
+    field = find_field("text")
+    expect(field.value).to eq("foo")
   end
 
   it "does not show search UI for user without client_slug" do
@@ -85,9 +69,39 @@ describe "searching", elasticsearch: true do
     expect(page).to have_content("Download")
   end
 
+  describe "Advanced Search" do
+    it "provides advanced search", :js do
+      proposals = populate_proposals
+      login_as(proposals.first.requester)
+
+      visit proposals_path
+      fill_in "text", with: proposals.first.name
+      adv_options = find("a.adv-options")
+      adv_options.trigger("click")
+      fill_in "test_client_request[client_data.amount]", with: proposals.first.client_data.amount
+      click_button "adv-search-button"
+
+      expect(current_path).to eq query_proposals_path
+      expect(page).to have_content(proposals.first.public_id)
+      expect(page).to have_content("(#{proposals.first.name}) AND (Amount:(#{proposals.first.client_data.amount}))")
+    end
+
+    it "has an Org Code field", :js do
+      proposals = populate_proposals
+      login_as(proposals.first.requester)
+
+      visit proposals_path
+      fill_in "text", with: proposals.first.name
+      adv_options = find("a.adv-options")
+      adv_options.trigger("click")
+
+      expect(page).to have_content "Org Code"
+    end
+  end
+
   def populate_proposals
     requester = create(:user, client_slug: "test")
-    proposals = 2.times.map do |i|
+    proposals = Array.new(2) do |i|
       wo = create(:test_client_request, project_title: "Work Order #{i}")
       wo.proposal.update(requester: requester)
       wo.proposal.reindex
