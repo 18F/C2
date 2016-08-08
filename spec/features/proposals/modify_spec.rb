acfcc8145d595f6b122bdba5fa1907cd8c13efae
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-08-08 19:35:30 -0400

    Create test for canceling

--- spec/features/proposals/modify_spec.rb
+++ spec/features/proposals/modify_spec.rb
@@ -3,7 +3,6 @@ describe "Modify Proposal Spec" do
     proposal = create_and_visit_proposal_beta
     title_selector = '#ncr_work_order_project_title'
     new_title_text = "New title text"
-    page.save_screenshot('./tmp/screen.png', full: true)
     expect(page).to have_selector(title_selector, visible: false)
     js_activate_modify_proposal
     expect(page).to have_selector(title_selector, visible: true)
@@ -11,6 +10,21 @@ describe "Modify Proposal Spec" do
     within("#proposal-title-wrapper") do
       within(".detail-value") do
         expect(page).to have_content(new_title_text)
+      end
+    end
+  end
+
+  scenario "submit content in form, then cancel submission", js: true do
+    proposal = create_and_visit_proposal_beta
+    title_selector = '#ncr_work_order_project_title'
+    new_title_text = "New title text"
+    expect(page).to have_selector(title_selector, visible: false)
+    js_activate_modify_proposal
+    expect(page).to have_selector(title_selector, visible: true)
+    js_cancel_before_modify_proposal(new_title_text, title_selector)
+    within("#proposal-title-wrapper") do
+      within(".detail-value") do
+        expect(page).not_to have_content(new_title_text)
       end
     end
   end
@@ -27,6 +41,13 @@ describe "Modify Proposal Spec" do
     find(submit).trigger("click")
     find('.save_confirm-modal-content .form-button[data-modal-event="confirm"]').trigger("click")
     wait_for_ajax
+  end
+
+  def js_cancel_before_modify_proposal(text = "foo", selector = 'ncr_work_order[project_title]', submit = ".request-actions .save-button button")
+    fill_in('ncr_work_order_project_title', with: text)
+    find(submit).trigger("click")
+    find('.save_confirm-modal-content .form-button[data-modal-event="cancel"]').trigger("click")
+    find(".request-actions .cancel-button button").trigger("click")
   end
 
   def create_and_visit_proposal_beta
