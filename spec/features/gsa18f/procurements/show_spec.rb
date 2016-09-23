6f4374bf8dfbefa62a5be584b496b5f606804e69
rememberlenny <rememberlenny@gmail.com>
2016-09-22 19:56:45 -0700

    Add a test

--- spec/features/gsa18f/procurements/show_spec.rb
+++ spec/features/gsa18f/procurements/show_spec.rb
@@ -10,4 +10,24 @@ feature "View Gsa18F procurement" do
 
     expect(page).to have_content(procurement.purchase_type)
   end
+
+  scenario "last step is completed" do
+    it "shows the pegasys document number" do
+      purchaser = Gsa18f::Procurement.user_with_role("gsa18f_purchaser")
+      procurement = create(:gsa18f_procurement, :with_steps)
+      proposal = procurement.proposal
+
+      procurement.individual_steps.first.complete!
+      deliveries.clear
+
+      login_as(purchaser)
+      visit proposal_path(proposal)
+      expect(page).not_to have_content("Pegasys Document Number")
+      click_on("Mark as Purchased")
+
+      visit proposal_path(proposal)
+      expect(page).to have_content("Pegasys Document Number")
+
+    end
+  end
 end
