18e257a9dc1e5168bdb1aa63a99d28d2c77a6fff
Nick Bristow <Nick.Bristow@gsa.gov>
2016-09-23 17:59:55 -0400

    fix code climate issues

--- app/decorators/gsa18f/event_decorator.rb
+++ app/decorators/gsa18f/event_decorator.rb
@@ -17,14 +17,13 @@ module Gsa18f
     end
 
     def new_display
-      %w( 
-          duty_station supervisor_id title_of_event 
-          event_provider type_of_event cost_per_unit start_date 
-          end_date purpose justification link instructions 
-          free_event nfs_form travel_required estimated_travel_expenses
-        )
+      %w(
+        duty_station supervisor_id title_of_event
+        event_provider type_of_event cost_per_unit start_date
+        end_date purpose justification link instructions
+        free_event nfs_form travel_required estimated_travel_expenses
+      )
     end
-        
 
     def top_email_field
     end
--- app/models/gsa18f/event_fields.rb
+++ app/models/gsa18f/event_fields.rb
@@ -16,8 +16,8 @@ module Gsa18f
       [
         :duty_station, :supervisor_id, :title_of_event,
         :event_provider, :type_of_event, :cost_per_unit,
-        :start_date, :end_date, :purpose, :justification, 
-        :link, :instructions, :free_event, :nfs_form, 
+        :start_date, :end_date, :purpose, :justification,
+        :link, :instructions, :free_event, :nfs_form,
         :travel_required, :estimated_travel_expenses
       ]
     end
