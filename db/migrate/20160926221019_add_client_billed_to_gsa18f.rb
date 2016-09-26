ebba6af157716b0a484899cd9676688603ff853b
rememberlenny <rememberlenny@gmail.com>
2016-09-26 16:37:16 -0700

    Create migration

--- db/migrate/20160926221019_add_client_billed_to_gsa18f.rb
+++ db/migrate/20160926221019_add_client_billed_to_gsa18f.rb
@@ -1,4 +1,8 @@
-class AddClientBuildToGsa18f < ActiveRecord::Migration
-  def change
+class AddClientBilledToGsa18f < ActiveRecord::Migration
+  def up
+    add_column :gsa18f_procurements, :client_billed, :boolean
+  end
+  def down
+    drop_column :gsa18f_procurements, :client_billed, :boolean
   end
 end
