06a156063899e3a714e1601c068d9b6374398ec1
rememberlenny <rememberlenny@gmail.com>
2016-09-30 13:12:03 -0400

    Remove the nsf form from form field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927192531) do
+ActiveRecord::Schema.define(version: 20160930170849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -158,7 +158,6 @@ ActiveRecord::Schema.define(version: 20160927192531) do
     t.string   "justification"
     t.string   "link"
     t.string   "instructions"
-    t.string   "nfs_form"
     t.decimal  "cost_per_unit"
     t.decimal  "estimated_travel_expenses"
     t.date     "start_date"
@@ -348,14 +347,6 @@ ActiveRecord::Schema.define(version: 20160927192531) do
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
-
-  create_table "test_client_requests", force: :cascade do |t|
-    t.decimal  "amount"
-    t.string   "project_title"
-    t.integer  "approving_official_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "user_delegates", force: :cascade do |t|
     t.integer  "assigner_id"
