e2135e17fb1063d013791af9b4dcceeae10bb49b
rememberlenny <rememberlenny@gmail.com>
2016-10-07 00:57:11 -0400

    Update display field to take a single el

--- app/services/prepare_display_fields.rb
+++ app/services/prepare_display_fields.rb
@@ -1,21 +1,19 @@
 class PrepareDisplayFields
-  def initialize(value, key,  data)
-    @value = value
-    @key = key
-    @data = data
+  def initialize(display_fields)
+    @obj = display_fields
   end
 
   def run
-    if @data[@key].nil?
+    if @obj[:data][@obj[:key]].nil?
       "--"
-    elsif special_keys.include? key
-      Object.const_get(@data.class).send("display_update_" + key, value, key, data)
+    elsif special_keys.include? @obj[:key]
+      Object.const_get(@obj[:data].class).send("display_update_" + @obj[:key], @obj)
     else
-      value
+      @obj[:value]
     end
   end
 
   private
 
-  attr_accessor :value, :key, :client_data_instance
+  attr_accessor :display_fields
 end
