01dcf64104020a3471fea3d9deaff9080549d146
rememberlenny <rememberlenny@gmail.com>
2016-10-17 18:40:35 -0400

    Create the beta condition check as a test

--- spec/features/reports_spec.rb
+++ spec/features/reports_spec.rb
@@ -15,6 +15,25 @@ feature "reports", elasticsearch: true do
     expect(page).to have_content("Saved as report my test report")
   end
 
+  scenario "provides Save as Report button on search results page with beta", js: true do
+    user = create(:user, client_slug: "test")
+    user.roles << Role.find_by!(name: ROLE_BETA_USER)
+    user.roles << Role.find_by!(name: ROLE_BETA_ACTIVE)
+
+    proposals = populate_proposals(user)
+    login_as(user)
+
+    es_execute_with_retries 3 do
+      visit query_proposals_path(text: proposals.first.name)
+    end
+
+    click_on "Save as Report"
+    fill_in "saved-search-name", with: "my test report"
+    click_on "Save"
+
+    expect(page).to have_content("Saved as report my test report")
+  end
+
   def populate_proposals(user)
     proposals = 2.times.map do |i|
       wo = create(:test_client_request, project_title: "Work Order #{i}")
