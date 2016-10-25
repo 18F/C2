5d83cc6e13f973e3765e7025a36fc34efbb666a9
rememberlenny <rememberlenny@gmail.com>
2016-10-25 17:29:31 -0400

    code climate

--- app/services/prepare_proposals_elasticsearch.rb
+++ app/services/prepare_proposals_elasticsearch.rb
@@ -2,12 +2,16 @@ class PrepareProposalsElasticsearch
   def default_indexed
     {
       include: {
-        comments: { include: {
-          user: { methods: [:display_name], only: [:display_name]
-        } } },
-        steps: { include: {
-          completed_by: { methods: [:display_name], only: [:display_name]
-        } } }
+        comments: {
+          include: {
+            user: { methods: [:display_name], only: [:display_name] }
+          }
+        },
+        steps: {
+          include: {
+            completed_by: { methods: [:display_name], only: [:display_name] }
+          }
+        }
       }
     }
   end
