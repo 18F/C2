b282a17c780437e4ab44fddd4d6ac5ec0ae9c13e
rememberlenny <rememberlenny@gmail.com>
2016-10-26 09:52:27 -0400

    Confirm the aces/desc is working

--- spec/features/proposals/index_spec.rb
+++ spec/features/proposals/index_spec.rb
@@ -44,6 +44,24 @@ feature "Proposals index" do
     expect(page).to have_selector('tbody tr', count: 4)
   end
 
+  scenario "order list by descending alhabetical in new index page for pending requests", :js do
+    work_order_ba80 = create(:ba80_ncr_work_order, :with_beta_requester)
+    user = work_order_ba80.requester
+    _reviewable_proposals = create_list(:proposal, 2, :with_approver, observer: user)
+    _pending_proposals = create_list(:proposal, 2, :with_approver, approver_user: user)
+    _canceled = create_list(:proposal, 2, status: "canceled", observer: user)
+
+    login_as(user)
+
+    visit "/proposals"
+
+    expect(page).to have_css("th.th-value-id .table-header")
+    first("th.th-value-id .table-header").click
+    sleep(1)
+
+    expect(first('tbody tr td.public_id a')).to have_content('PUBLIC7')
+  end
+
   scenario "filters new index page for canceled requests", :js do
     work_order_ba80 = create(:ba80_ncr_work_order, :with_beta_requester)
     user = work_order_ba80.requester
