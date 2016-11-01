03d0619b8afc908b69e3eee59eb70b8e5805832c
rememberlenny <rememberlenny@gmail.com>
2016-11-01 12:39:48 -0400

    Pull out more config attribution

--- app/models/concerns/proposal_config.rb
+++ app/models/concerns/proposal_config.rb
@@ -1,29 +1,27 @@
 module ProposalConfig
   extend ActiveSupport::Concern
 
-  included do
-    has_many :steps
-    has_many :approval_steps, class_name: "Steps::Approval"
-    has_many :purchase_steps, class_name: "Steps::Purchase"
-    has_many :completers, through: :steps, source: :completer
-    has_many :api_tokens, through: :steps
-    has_many :attachments, dependent: :destroy
-    has_many :comments, -> { order(created_at: :desc) }, dependent: :destroy
+    included do
+        has_many :steps
+        has_many :approval_steps, class_name: "Steps::Approval"
+        has_many :purchase_steps, class_name: "Steps::Purchase"
+        has_many :completers, through: :steps, source: :completer
 
-    has_many :observations, -> { where("proposal_roles.role_id in (select roles.id from roles where roles.name='#{ROLE_OBSERVER}')") }
-    has_many :observers, through: :observations, source: :user
-    belongs_to :client_data, polymorphic: true, dependent: :destroy
-    belongs_to :requester, class_name: "User"
-
-    delegate :client_slug, to: :client_data, allow_nil: true
-
-    validates :requester_id, presence: true
-    validates :public_id, uniqueness: true, allow_nil: true
-
-    statuses.each do |status|
-      scope status, -> { where(status: status) }
+        has_many :api_tokens, through: :steps
+        has_many :attachments, dependent: :destroy
+        has_many :comments, -> { order(created_at: :desc) }, dependent: :destroy
     end
-    scope :closed, -> { where(status: %w(completed canceled)) }
-    scope :canceled, -> { where(status: "canceled") }
-  end
+
+
+    def add_completed_comment
+        completer = individual_steps.last.completed_by
+        comments.create_without_callback(
+          comment_text: I18n.t(
+            "activerecord.attributes.proposal.user_completed_comment",
+            user: completer.full_name
+          ),
+          update_comment: true,
+          user: completer
+        )
+    end
 end
--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,5 +1,6 @@
 class Proposal < ActiveRecord::Base
   include WorkflowModel
+  include ProposalConfig
   include ValueHelper
   include StepManager
   include Searchable
@@ -35,14 +36,6 @@ class Proposal < ActiveRecord::Base
 
   acts_as_taggable
   visitable # Used to track user visit associated with processed proposal
-
-  has_many :steps
-  has_many :approval_steps, class_name: "Steps::Approval"
-  has_many :purchase_steps, class_name: "Steps::Purchase"
-  has_many :completers, through: :steps, source: :completer
-  has_many :api_tokens, through: :steps
-  has_many :attachments, dependent: :destroy
-  has_many :comments, -> { order(created_at: :desc) }, dependent: :destroy
 
   has_many :observations, -> { where("proposal_roles.role_id in (select roles.id from roles where roles.name='#{ROLE_OBSERVER}')") }
   has_many :observers, through: :observations, source: :user
@@ -197,18 +190,6 @@ class Proposal < ActiveRecord::Base
       fail "#{email} is an approver on this Proposal -- cannot also be Requester"
     end
     set_requester(user)
-  end
-
-  def add_completed_comment
-    completer = individual_steps.last.completed_by
-    comments.create_without_callback(
-      comment_text: I18n.t(
-        "activerecord.attributes.proposal.user_completed_comment",
-        user: completer.full_name
-      ),
-      update_comment: true,
-      user: completer
-    )
   end
 
   def add_restart_comment(user)
