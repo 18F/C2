86718b553988be644d5ce6b09e0842b03f3d617b
Leonard Bogdonoff <rememberlenny@gmail.com>
2016-11-25 12:52:50 -0500

    Remove the myusa references in config scripts

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -7,5 +7,5 @@ if BULLET_ENABLED
   Bullet.enable = true
   Bullet.console = true
   Bullet.rails_logger = true
-  Bullet.stacktrace_includes = ["omniauth-myusa"]
+  Bullet.stacktrace_includes = ["omniauth-cg"]
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,19 +1,9 @@
-MYUSA_KEY    = OauthCredentials.myusa_key
-MYUSA_SECRET = OauthCredentials.myusa_secret
-MYUSA_URL    = ENV["MYUSA_URL"] || "https://alpha.my.usa.gov"
-
 CG_KEY       = OauthCredentials.cg_app_id
 CG_SECRET    = OauthCredentials.cg_app_secret
 CG_URL       = ENV["CG_URL"] || "https://login.cloud.gov"
 CG_TOKEN_URL = ENV["CG_TOKEN_URL"] || "https://uaa.cloud.gov"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :myusa, MYUSA_KEY, MYUSA_SECRET, scope: "profile.email",
-                                            client_options: {
-                                              site: MYUSA_URL,
-                                              token_url: "/oauth/authorize"
-                                            }
-
   provider :cg,
            CG_KEY,
            CG_SECRET,
