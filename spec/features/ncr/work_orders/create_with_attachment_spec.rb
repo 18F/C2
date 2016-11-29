45eb6b6baad62a09d4fbcbd29d63c92d7b0a2944
Nick Bristow <Nick.Bristow@gsa.gov>
2016-11-29 10:46:16 -0500

    fixed attachment spec

--- spec/features/ncr/work_orders/create_with_attachment_spec.rb
+++ spec/features/ncr/work_orders/create_with_attachment_spec.rb
@@ -7,8 +7,7 @@ feature "Create NCR work order with attachment" do
     expect(page).to have_content("Attachments")
     expect(page).not_to have_selector(".js-am-minus")
     expect(page).not_to have_selector(".js-am-plus")
-    
-    expect(page).to have_selector("input[type=\"file\"]", count: 10)
+    expect(page).to have_selector(".attachment-label", count: 10)
   end
 
   scenario "allows attachments to be added during intake with JS", :js do
@@ -23,10 +22,16 @@ feature "Create NCR work order with attachment" do
     expect(first_minus).to be_visible
     expect(first_plus).to be_visible
     expect(first_minus).to be_disabled
+    show_attachment_buttons
     expect(find("input[type=file]")[:name]).to eq("attachments[]")
     first_plus.click # Adds one row
+    show_attachment_buttons
     expect(page).to have_selector(".js-am-minus", count: 2)
     expect(page).to have_selector(".js-am-plus", count: 2)
     expect(page).to have_selector("input[type=file]", count: 2)
   end
+end
+
+def show_attachment_buttons
+    execute_script("$('input[type=file]').show()")
 end
