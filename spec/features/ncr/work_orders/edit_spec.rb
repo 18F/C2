ab6ad969a117e85bee8a94f56e344cffac89f3ee
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-11-29 21:46:09 -0500

    Create a proper proposal based on scratch

--- spec/features/ncr/work_orders/edit_spec.rb
+++ spec/features/ncr/work_orders/edit_spec.rb
@@ -25,6 +25,39 @@ feature "Editing NCR work order" do
       expect(page).to have_css('[data-modal-type="save_confirm"][disabled]')
     end
 
+    scenario "allows requester to change the approving official", :js do
+      approver = create(:user, client_slug: "ncr")
+      organization = create(:ncr_organization)
+      project_title = "buying stuff"
+      requester = create(:user, client_slug: "ncr")
+
+      login_as(requester)
+
+      visit new_ncr_work_order_path
+      fill_in 'Project title', with: project_title
+      fill_in 'Description', with: "desc content"
+      choose 'BA80'
+      fill_in 'RWA#', with: 'F1234567'
+      fill_in_selectized("ncr_work_order_building_number", "Test building")
+      fill_in_selectized("ncr_work_order_vendor", "ACME")
+      fill_in 'Amount', with: 123.45
+      fill_in_selectized("ncr_work_order_approving_official", approver.email_address)
+      fill_in_selectized("ncr_work_order_ncr_organization", organization.code_and_name)
+      click_on "SUBMIT"
+
+      proposal = requester.proposals.last
+
+      visit proposal_path(proposal)
+      
+      save_and_open_screenshot
+
+      find(".card-for-observers .selectize-input input").native.send_keys( approver.email_address ) #fill the input text
+      first(:xpath, ("//div[@data-selectable and contains(., '#{approver.email_address}')]")).click #wait for the input and then click on it
+      
+      expect(proposal.approvers.first.email_address).to eq approver.email_address
+      expect(proposal.individual_steps.first).to be_actionable
+    end
+
     scenario "preserves previously selected values in dropdowns", :js do
       work_order_ba80 = create(:ba80_ncr_work_order, :with_beta_requester)
       work_order_ba80.save!
