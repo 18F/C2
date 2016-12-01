a9624914e2af371990be625d80e54e9a7a05b2a5
Nick Bristow <Nick.Bristow@gsa.gov>
2016-12-01 14:25:09 -0500

    update time zone spec

--- spec/features/proposals/complete_spec.rb
+++ spec/features/proposals/complete_spec.rb
@@ -20,17 +20,17 @@ describe "Completing a proposal" do
     expect(page).to have_content("You have approved #{proposal.public_id}")
   end
 
-  it "responds with error message when step cannot be acted on" do
-    proposal = create(:proposal, :with_serial_approvers)
+  it "responds with error message when step cannot be acted on", :js do
+    proposal = create(:ncr_work_order, :with_approvers).proposal
     first_step = proposal.individual_steps.first
-    first_approver = first_step.user
-
+    first_approver = proposal.approvers.first
+    first_step.update_attributes!(status: "foobar")
+    
     login_as(first_approver)
     visit proposal_path(proposal)
-
-    first_step.update_attributes!(status: "foobar")
-
+    page.save_screenshot('../screen.png', full: true)
     click_on("Approve")
+    
 
     expect(current_path).to eq(proposal_path(proposal))
     expect(page).to have_content(I18n.t("errors.policies.proposal.step_complete"))
